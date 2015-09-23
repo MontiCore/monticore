5390f22f2d0a8327bd87a04940bd59d92e71ad0e
Galina Volkova <volkova@se-rwth.de>
2015-09-23 18:43:37 +0200

    fixed it-test

--- monticore-generator/it/src/main/java/mc/feature/statechart/statechartdsl/_ast/StatechartDSLNodeFactory.java
+++ monticore-generator/it/src/main/java/mc/feature/statechart/statechartdsl/_ast/StatechartDSLNodeFactory.java
@@ -2,19 +2,12 @@ package mc.feature.statechart.statechartdsl._ast;
 
 public class StatechartDSLNodeFactory extends StatechartDSLNodeFactoryTOP {
   
-  private static StatechartDSLNodeFactoryTOP getFactory() {
-    if (factory == null) {
-      factory = new StatechartDSLNodeFactory();
-    }
-    return factory;
-  }
-  
   protected StatechartDSLNodeFactory() {
   }
   
   public static ASTStatechartList createASTStatechartList() {
     if (factoryASTStatechartList == null) {
-      factoryASTStatechartList = getFactory();
+      factoryASTStatechartList = new StatechartDSLNodeFactory();
     }
     return factoryASTStatechartList.doCreateASTStatechartList();
   }
