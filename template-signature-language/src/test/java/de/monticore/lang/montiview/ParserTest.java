ca88e196e0a197ec39f03ac4ee3aac41ab70318f
Pfeiffer <pfeiffer@se-rwth.de>
2016-04-06 15:38:56 +0200

    - minor - one more test

--- template-signature-language/src/test/java/de/monticore/lang/montiview/ParserTest.java
+++ template-signature-language/src/test/java/de/monticore/lang/montiview/ParserTest.java
@@ -5,8 +5,11 @@
  */
 package de.monticore.lang.montiview;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -18,10 +21,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import junit.framework.Assert;
+
 import org.antlr.v4.runtime.RecognitionException;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import de.monticore.lang.templatesignature.templatesignature._ast.ASTParameter;
+import de.monticore.lang.templatesignature.templatesignature._ast.ASTResult;
 import de.monticore.lang.templatesignature.templatesignature._ast.ASTSignature;
 import de.monticore.lang.templatesignature.templatesignature._parser.TemplateSignatureParser;
 import de.se_rwth.commons.logging.Log;
@@ -39,12 +46,49 @@ public class ParserTest {
     Log.enableFailQuick(ENABLE_FAIL_QUICK);
   }
   
-  
   @Test
   public void testSignature() throws RecognitionException, IOException {
     test("si");
   }
   
+  @Test
+  public void testSignatureCorrectness() {
+    File fqnTemplateName = new File("src/test/resources/parser/valid/simpleSignature.si");
+    Optional<ASTSignature> sig = parse(fqnTemplateName);
+    assertTrue(sig.isPresent());
+    ASTSignature signature = sig.get();
+    List<ASTParameter> params = signature.getParameters();
+    assertEquals(params.get(0).getName(), "$ast");
+    assertEquals(params.get(0).getParamType().toString(), "int");
+    Optional<ASTResult> res = signature.getResult();
+    assertTrue(res.isPresent());
+    assertEquals(res.get().getResultType().toString(), "String");
+  }
+  
+  private Optional<ASTSignature> parse(File fqnTemplateName) {
+    FileReader fr;
+    Optional<ASTSignature> signature = Optional.empty();
+    try {
+      fr = new FileReader(fqnTemplateName);
+      System.out.println(fqnTemplateName.getAbsolutePath());
+      BufferedReader br = new BufferedReader(fr);
+      String line = "";
+      line = br.readLine();
+      String signatureString = "";
+      while (line != null) {
+        signatureString += "\n" + line;
+        line = br.readLine();
+      }
+      br.close();
+      
+      TemplateSignatureParser parser = new TemplateSignatureParser();
+      signature = parser.parseString_Signature(signatureString);
+    }
+    catch (IOException e) {
+      e.printStackTrace();
+    }
+    return signature;
+  }
   
   private void test(String fileEnding) throws IOException {
     ParseTest parserTest = new ParseTest("." + fileEnding);
@@ -59,7 +103,7 @@ public class ParserTest {
     Log.info("Count of tested models: " + parserTest.getTestCount(), "ParserTest");
     Log.info("Count of correctly parsed models: "
         + (parserTest.getTestCount() - parserTest.getModelsInError().size()), "ParserTest");
-        
+    
     assertTrue("There were models that could not be parsed", parserTest.getModelsInError()
         .isEmpty());
   }
@@ -102,18 +146,18 @@ public class ParserTest {
         throws IOException {
       if (file.toFile().isFile()
           && (file.toString().toLowerCase().endsWith(fileEnding))) {
-          
+        
         Log.debug("Parsing file " + file.toString(), "ParserTest");
         testCount++;
         
-        Optional<ASTSignature> signature = Optional.empty();        
+        Optional<ASTSignature> signature = Optional.empty();
         TemplateSignatureParser parser = new TemplateSignatureParser();
         try {
           signature = parser.parse(file.toString());
         }
         catch (Exception e) {
-            Log.error("Exception during test", e);
-          }
+          Log.error("Exception during test", e);
+        }
         if ((parser.hasErrors() || !signature.isPresent())) {
           modelsInError.add(file.toString());
           Log.error("There were unexpected parser errors");
