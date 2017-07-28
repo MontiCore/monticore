eb85084b369f182c882bb411a9c4426bfb6890f3
Galina Volkova <volkova@se-rwth.de>
2017-07-28 11:30:43 +0200

    add methods-profile for list-valued ast attributes

---
+++ monticore-generator/src/main/java/de/monticore/codegen/cd2java/ast/AstListMethods.java
@@ -0,0 +1,114 @@
+/*
+ * ******************************************************************************
+ * MontiCore Language Workbench
+ * Copyright (c) 2015, MontiCore, All rights reserved.
+ *
+ * This project is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this project. If not, see <http://www.gnu.org/licenses/>.
+ * ******************************************************************************
+ */
+
+package de.monticore.codegen.cd2java.ast;
+
+/**
+ * Methods-profile for list-valued ast attributes.
+ *
+ * @author Galina Volkova
+ */
+
+public enum AstListMethods {
+  
+  // ----------- java.util.Collection -----------------------------
+  
+  clear("public void clear%s();"),
+  
+  add("public boolean add%s(%s element);"),
+  
+  addAll("public boolean addAll%s(Collection<? extends %s> collection);"),
+  
+  contains("public boolean contains%s(ASTNode element);"),
+  
+  containsAll("public boolean containsAll%s(Collection<ASTNode> collection);"),
+  
+  isEmpty("public boolean isEmpty%s();"),
+  
+  iterator("public Iterator<%s> iterator%s();"),
+  
+  remove("public boolean remove%s(ASTNode element);"),
+  
+  removeAll("public boolean removeAll%s(Collection<ASTNode> collection);"),
+  
+  retainAll("public boolean retainAll%s(Collection<ASTNode> collection);"),
+  
+  size("public int size%s();"),
+  
+  toArray("public ASTNode[] toArray();"),
+  
+  toArraySpecified("public %s[] toArray%s(%s[] array);"),
+  
+  // ----------- java.util.Collection since 1.8 --------------------
+  
+  removeIf("public boolean removeIf%s(Predicate<? super %s> filter);"),
+  
+  spliterator("public Spliterator<%s> spliterator%s();"),
+  
+  stream("public Stream<%s> stream%s());"),
+  
+  parallelStream("public Stream<%s> parallelStream%s();"),
+  
+  // ----------- java.util.Iterable since 1.8 -----------------------
+  
+  forEach("public void forEach%s(Consumer<? super %s> action);"),
+  
+  // ----------- java.util.List -------------------------------------
+  
+  addIndex(" public void add%s(int index, %s element);"),
+  
+  addAllIndex("public boolean addAll%s(int index, Collection<? extends %s> collection);"),
+  
+  get("public %s get%s(int index);"),
+  
+  indexOf("public int indexOf%s(ASTNode element);"),
+  
+  lastIndexOf("public int lastIndexOf%s(ASTNode element);"),
+  
+  equals("public boolean equals(ASTNode o);"),
+  
+  hashCode("public int hashCode()"),
+  
+  listIterator("public ListIterator<%s> listIterator%s();"),
+  
+  listIteratorIndex("public ListIterator<%s> listIterator%s(int index);"),
+  
+  removeIndex("%s remove%s(int index);"),
+  
+  subList("List<%s> subList%s(int start, int end);"),
+  
+  // ----------- java.util.List since 1.8 --------------------------
+  
+  replaceAll("public void replaceAll%s(UnaryOperator<%s> operator);"),
+  
+  sort("public void sort%s(Comparator<? super %s> comparator);");
+  
+  // ---------------------------------------------------------------
+  
+  private String methodDeclaration;
+  
+  private AstListMethods(String header) {
+    this.methodDeclaration = header;
+  }
+  
+  public String getDeclaration() {
+    return methodDeclaration;
+  }
+  
+}
