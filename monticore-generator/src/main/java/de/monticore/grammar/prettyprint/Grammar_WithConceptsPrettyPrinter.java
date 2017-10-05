ae3fa8527b29fea417484da60d3c7c4936768ea5
breuer <breuer@se-rwth.de>
2017-10-05 11:29:31 +0200

    mc

--- monticore-generator/src/main/java/de/monticore/grammar/prettyprint/Grammar_WithConceptsPrettyPrinter.java
+++ monticore-generator/src/main/java/de/monticore/grammar/prettyprint/Grammar_WithConceptsPrettyPrinter.java
@@ -19,7 +19,7 @@
 
 package de.monticore.grammar.prettyprint;
 
-import de.monticore.expressions.mcexpressions._ast.ASTMCExpressionsNode;
+import de.monticore.mcexpressions._ast.ASTMCExpressionsNode;
 import de.monticore.expressions.prettyprint.MCExpressionsPrettyPrinter;
 import de.monticore.grammar.concepts.antlr.antlr._ast.ASTAntlrNode;
 import de.monticore.grammar.grammar._ast.ASTGrammarNode;
@@ -49,7 +49,7 @@ public class Grammar_WithConceptsPrettyPrinter implements Grammar_WithConceptsVi
     visitor.set_de_monticore_grammar_grammar__visitor_GrammarVisitor(new GrammarPrettyPrinter(out));
     visitor.set_de_monticore_java_javadsl__visitor_JavaDSLVisitor(new JavaDSLPrettyPrinter(out));
     visitor.set_de_monticore_literals_literals__visitor_LiteralsVisitor(new LiteralsPrettyPrinterConcreteVisitor(out));
-    visitor.set_de_monticore_expressions_mcexpressions__visitor_MCExpressionsVisitor(new MCExpressionsPrettyPrinter(out));
+    visitor.set_de_monticore_mcexpressions__visitor_MCExpressionsVisitor(new MCExpressionsPrettyPrinter(out));
     visitor.set_de_monticore_types_types__visitor_TypesVisitor(new TypesPrettyPrinterConcreteVisitor(out));
   }
   
