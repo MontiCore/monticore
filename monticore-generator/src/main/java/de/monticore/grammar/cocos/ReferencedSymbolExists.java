a6e30c5a323aff5d91b05b21619b2b70d3fd7e52
pichler <pichler@se-rwth.de>
2018-04-26 09:14:58 +0200

    better coco without visitor

--- monticore-generator/src/main/java/de/monticore/grammar/cocos/ReferencedSymbolExists.java
+++ monticore-generator/src/main/java/de/monticore/grammar/cocos/ReferencedSymbolExists.java
@@ -1,22 +1,33 @@
 package de.monticore.grammar.cocos;
 
-import de.monticore.grammar.grammar._ast.ASTMCGrammar;
-import de.monticore.grammar.grammar._cocos.GrammarASTMCGrammarCoCo;
+import de.monticore.codegen.mc2cd.MCGrammarSymbolTableHelper;
+import de.monticore.grammar.grammar._ast.ASTNonTerminal;
+import de.monticore.grammar.grammar._cocos.GrammarASTNonTerminalCoCo;
 import de.monticore.grammar.symboltable.MCGrammarSymbol;
+import de.se_rwth.commons.logging.Log;
 
-public class ReferencedSymbolExists implements GrammarASTMCGrammarCoCo {
+import java.util.Optional;
+
+public class ReferencedSymbolExists implements GrammarASTNonTerminalCoCo {
 
   public static final String ERROR_CODE = "0xA4037";
 
   public static final String ERROR_MSG_FORMAT = " The production for the referenced symbol %s does not exist as a symbol or not at all.";
 
   @Override
-  public void check(ASTMCGrammar a) {
-    MCGrammarSymbol grammarSymbol = (MCGrammarSymbol) a.getSymbol();
-
-    ReferencedSymbolVisitor visitor = new ReferencedSymbolVisitor(grammarSymbol);
-    visitor.visit(a);
-
+  public void check(ASTNonTerminal node) {
+    Optional<MCGrammarSymbol> grammarSymbol = MCGrammarSymbolTableHelper
+        .getMCGrammarSymbol(node);
+    if (node.isPresentReferencedSymbol()) {
+      String symbol = node.getReferencedSymbol();
+      if (grammarSymbol.get().getProdWithInherited(symbol).isPresent()) {
+        if (grammarSymbol.get().getProdWithInherited(symbol).get().isSymbolDefinition()) {
+          return;
+        }
+      }
+      Log.error(String.format(ERROR_CODE + String.format(ERROR_MSG_FORMAT, symbol),
+          node.get_SourcePositionStart()));
+    }
   }
 }
 
