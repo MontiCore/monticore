f1bda1c1eac0414da76a4a2c88248c7365be6ceb
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2018-05-22 14:00:23 +0100

    builder validate test

--- monticore-generator/it/src/test/java/mc/feature/inheritence/InheritanceBuilderTest.java
+++ monticore-generator/it/src/test/java/mc/feature/inheritence/InheritanceBuilderTest.java
@@ -8,13 +8,13 @@ package mc.feature.inheritence;
 import de.se_rwth.commons.logging.Log;
 import mc.GeneratorIntegrationsTest;
 import mc.feature.inheritence.sup.sub.subgrammar._ast.ASTB;
+import mc.feature.inheritence.sup.sub.subgrammar._ast.ASTBBuilder;
+import mc.feature.inheritence.sup.sub.subgrammar._ast.ASTD;
 import mc.feature.inheritence.sup.sub.subgrammar._ast.SubGrammarMill;
 import mc.feature.inheritence.sup.supergrammar._ast.*;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class InheritanceBuilderTest extends GeneratorIntegrationsTest {
   
@@ -24,20 +24,29 @@ public class InheritanceBuilderTest extends GeneratorIntegrationsTest {
     ASTJ j = SuperGrammarMill.jBuilder().build();
     ASTY y = SuperGrammarMill.yBuilder().build();
     
-    ASTX x = SuperGrammarMill.xBuilder().setJ(j).setY(y).build();
+    ASTXBuilder x = SuperGrammarMill.xBuilder();
+    assertFalse(x.isValid());
+  
+    x.setJ(j).setY(y);
+    assertTrue(x.isValid());
     
     try {
       SuperGrammarMill.xBuilder().build();
       fail("invalid ASTX could be build");
     } catch (IllegalStateException e) {
-      assertEquals(1, Log.getFindings().size());
+      assertEquals(2, Log.getFindings().size());
       Log.getFindings().clear();
     }
   }
   
   @Test
   public void testBuildB() {
-    ASTB b = SubGrammarMill.bBuilder().setD(SubGrammarMill.dBuilder().build()).build();
+    ASTD d = SubGrammarMill.dBuilder().build();
+    ASTBBuilder b = SubGrammarMill.bBuilder();
+    assertFalse(b.isValid());
+    
+    b.setD(d);
+    assertTrue(b.isValid());
     
     try {
       SubGrammarMill.bBuilder().build();
