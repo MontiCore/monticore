b49b98a72821777b42a1ea5ae8849a271180cd69
eikermann <eikermann@se-rwth.de>
2019-01-16 14:08:39 +0100

    Type adapter

--- monticore-grammar/src/test/java/de/monticore/aggregation/Dummy2EMethodResolvingFilter.java
+++ monticore-grammar/src/test/java/de/monticore/aggregation/Dummy2EMethodResolvingFilter.java
@@ -3,13 +3,15 @@ package de.monticore.aggregation;
 import de.monticore.aggregation.blah._symboltable.DummyKind;
 import de.monticore.aggregation.blah._symboltable.DummySymbol;
 import de.monticore.expressions.expressionsbasis._symboltable.EMethodSymbol;
+import de.monticore.symboltable.CommonSymbol;
 import de.monticore.symboltable.Symbol;
+import de.monticore.symboltable.SymbolKind;
 import de.monticore.symboltable.resolving.TransitiveAdaptedResolvingFilter;
 
 public class Dummy2EMethodResolvingFilter extends TransitiveAdaptedResolvingFilter<EMethodSymbol> {
 
-  public Dummy2EMethodResolvingFilter() {
-    super(DummyKind.KIND,EMethodSymbol.class, EMethodSymbol.KIND);
+  public Dummy2EMethodResolvingFilter(SymbolKind kind) {
+    super(kind ,EMethodSymbol.class, EMethodSymbol.KIND);
   }
 
   @Override
