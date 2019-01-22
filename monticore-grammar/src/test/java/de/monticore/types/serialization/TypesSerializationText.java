c354bb95228fac19eee072c231a3de2f60ba3cf5
Butting <butting@se-rwth.de>
2019-01-22 10:04:01 +0100

    initial types serialization

---
+++ monticore-grammar/src/test/java/de/monticore/types/serialization/TypesSerializationText.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2019 RWTH Aachen. All rights reserved.
+ *
+ * http://www.se-rwth.de/
+ */
+package de.monticore.types.serialization;
+
+import org.junit.Test;
+
+/**
+ * TODO: Write me!
+ *
+ * @author  (last commit) $Author$
+ * @version $Revision$,
+ *          $Date$
+ * @since   TODO: add version number
+ *
+ */
+public class TypesSerializationText {
+  
+  
+  @Test
+  public void testSimpleType() {
+    
+  }
+}
