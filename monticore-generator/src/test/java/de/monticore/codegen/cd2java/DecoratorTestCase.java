cad3ebfb0d2fdf955703087d645b3ac6a0a48a39
Brian Sinkovec <brian.sinkovec@rwth-aachen.de>
2019-03-01 13:47:24 +0100

    fixed tests

--- monticore-generator/src/test/java/de/monticore/codegen/cd2java/DecoratorTestCase.java
+++ monticore-generator/src/test/java/de/monticore/codegen/cd2java/DecoratorTestCase.java
@@ -1,6 +1,8 @@
 package de.monticore.codegen.cd2java;
 
 import de.monticore.io.paths.ModelPath;
+import de.monticore.symboltable.GlobalScope;
+import de.monticore.symboltable.ResolvingConfiguration;
 import de.monticore.umlcd4a.CD4AnalysisLanguage;
 import de.monticore.umlcd4a.CD4AnalysisModelLoader;
 import de.monticore.umlcd4a.cd4analysis._ast.ASTCDCompilationUnit;
@@ -8,6 +10,7 @@ import de.monticore.umlcd4a.cd4analysis._parser.CD4AnalysisParser;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Optional;
 
 import static org.junit.Assert.fail;
@@ -30,11 +33,16 @@ public abstract class DecoratorTestCase {
 
   public ASTCDCompilationUnit loadModel(String qualifiedName) {
     CD4AnalysisLanguage cd4AnalysisLanguage = new CD4AnalysisLanguage();
+    ResolvingConfiguration resolvingConfiguration = new ResolvingConfiguration();
+    resolvingConfiguration.addDefaultFilters(cd4AnalysisLanguage.getResolvingFilters());
     CD4AnalysisModelLoader modelLoader = new CD4AnalysisModelLoader(cd4AnalysisLanguage);
     ModelPath modelPath = new ModelPath(Paths.get(MODEL_PATH));
-    Optional<ASTCDCompilationUnit> ast = modelLoader.loadModel(qualifiedName, modelPath);
-    if (!ast.isPresent())
+    GlobalScope globalScope = new GlobalScope(modelPath, cd4AnalysisLanguage);
+    Collection<ASTCDCompilationUnit> ast = modelLoader.loadModelsIntoScope(qualifiedName, modelPath, globalScope, resolvingConfiguration);
+    if (ast.isEmpty())
       fail(String.format("Failed to load model '%s'", qualifiedName));
-    return ast.get();
+    if (ast.size() > 1)
+      fail(String.format("Multiple models loaded model '%s'", qualifiedName));
+    return ast.iterator().next();
   }
 }
