4b2df3945c4d483d4476253fa19bbc320d2f92fb
eikermann <eikermann@se-rwth.de>
2019-04-09 09:42:02 +0200

    EMethod at CallExpression

---
+++ monticore-grammar/src/main/java/de/monticore/expressions/commonexpressions/_ast/ASTCallExpression.java
@@ -0,0 +1,29 @@
+package de.monticore.expressions.commonexpressions._ast;
+
+import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
+import de.monticore.expressions.expressionsbasis._symboltable.EMethodSymbol;
+
+public class ASTCallExpression extends ASTCallExpressionTOP {
+
+  private EMethodSymbol eMethodSymbol;
+
+  protected ASTCallExpression() {
+  }
+
+
+  protected ASTCallExpression(ASTExpression expression, ASTArguments arguments) {
+    setExpression(expression);
+    setArguments(arguments);
+  }
+
+  public EMethodSymbol geteMethodSymbol() {
+    return eMethodSymbol;
+  }
+
+  public void seteMethodSymbol(EMethodSymbol eMethodSymbol) {
+    this.eMethodSymbol = eMethodSymbol;
+  }
+
+
+
+}
