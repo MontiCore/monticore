0a8f0f7a908e6a38bd649d71b55e543d476e6a07
Pfeiffer <pfeiffer@se-rwth.de>
2019-04-25 16:40:43 +0200

    Adapted TCG to new generator infrastructure of MontiCore

--- monticore-templateclassgenerator/src/main/java/de/monticore/generating/templateengine/ExtendedTemplateController.java
+++ monticore-templateclassgenerator/src/main/java/de/monticore/generating/templateengine/ExtendedTemplateController.java
@@ -4,18 +4,13 @@ package de.monticore.generating.templateengine;
 import static com.google.common.base.Preconditions.checkArgument;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 import de.monticore.generating.GeneratorSetup;
-import de.se_rwth.commons.logging.Log;
 
 /**
  * This class adds the signature methods tc.params(...) and tc.result(...) to
  * the existing TemplateController.
- *
  */
 public class ExtendedTemplateController extends TemplateController {
   
@@ -23,11 +18,6 @@ public class ExtendedTemplateController extends TemplateController {
    * Ensures tc.params(...) is only called once
    */
   private boolean parametrized = false;
-  
-  /**
-   * Ensures tc.result(...) is only called once
-   */
-  private boolean resultized = false;
   
   /**
    * Constructor for de.montiarc.generator.codegen.MyTemplateController
@@ -61,125 +51,20 @@ public class ExtendedTemplateController extends TemplateController {
     
     // compares params and args
     for (int i = 0; i < arguments.size(); i++) {
-      Object argument = arguments.get(i);
       String parameter = params[i];
       String paramType = parameter.substring(0, parameter.indexOf(" "));
       paramType = paramType.trim();
-      if (paramType.contains("<")) {
-        paramType = paramType.substring(0, paramType.indexOf("<"));
+      if (paramType.contains(">")) {
+        paramType = paramType.substring(0, paramType.indexOf(">"));
       }
-      String paramName = parameter.substring(parameter.indexOf(" ") + 1);
-      Class argumentClass = argument.getClass();
-      String argumentClassName = argumentClass.getName();
-      
-      // checks whether argument type and param type are equal try with java
-      // library types otherwise
-      if (!paramType.equals(argumentClassName)) {
-        Optional<Class> javaLibraryType = getJavaLibraryType(paramType);
-        boolean isAssignable = false;
-        if (javaLibraryType.isPresent()) {
-          isAssignable = javaLibraryType.get().isAssignableFrom(argumentClass);
-          // if it is not assignable with java library type it might be a number
-          if (!isAssignable) {
-            isAssignable = Number.class.isAssignableFrom(javaLibraryType.get())
-                && Number.class.isAssignableFrom(argumentClass);
-          }
-        }
-        checkArgument(isAssignable, "0xA5301 Template '"
-            + getTemplatename() + "': passed argument type (" +
-            argumentClassName +
-            ") and type of signature parameter (" + paramType + " " + paramName + ") mismatch.");
-      }
-      
-      // Case 1: No Signature -> we have to signature the paramnames
-//      if (names.isEmpty()) {
-        toSignature.add(paramName);
-//      }
-      // TODO MB  signature?
-//      // Case 2: User wrote signature() additionally to params() -> we do not
-//      // need to signature the parameter names, but compare them to the
-//      // parameter names in the params() method.
-//      else {
-//        String argumentName = names.get(i);
-//        checkArgument(argumentName.equals(paramName),
-//            "0xA5300 Template '" + getTemplatename() + "': Parameter name (" +
-//                paramName +
-//                ") and name of parameter in signature (" + argumentName + ") mismatch.");
-//      }
+      String paramName = parameter.substring(parameter.indexOf(paramType) + paramType.length() +1).trim();
+      toSignature.add(paramName);
     }
     
     if (!toSignature.isEmpty()) {
       signature(toSignature);
     }
     parametrized = true;
-  }
-  
-  /**
-   * Loads java library types to the passed {@link paramType}
-   *
-   * @param argumentClass
-   * @return
-   */
-  private Optional<Class> getJavaLibraryType(String paramType) {
-    // maps primitive data types to wrapper classes
-    Map<String, String> primitiveTypes = new HashMap<String, String>();
-    primitiveTypes.put("long", "java.lang.Long");
-    primitiveTypes.put("int", "java.lang.Integer");
-    primitiveTypes.put("short", "java.lang.Short");
-    primitiveTypes.put("double", "java.lang.Double");
-    primitiveTypes.put("float", "java.lang.Float");
-    primitiveTypes.put("byte", "java.lang.Byte");
-    primitiveTypes.put("char", "java.lang.Character");
-    primitiveTypes.put("boolean", "java.lang.Boolean");
-    
-    // 1. tries to load the passed type
-    try {
-      Class c = Class.forName(paramType);
-      return Optional.of(c);
-    }
-    catch (ClassNotFoundException e) {
-//      Log.info("Class " + paramType + " not found!", "ExtendedTemplateController");
-    }
-    
-    // 2. this packages are searched to find the fqn of the passed paramType
-    String[] packagesToSearch = { "java.lang", "java.util" };
-    Optional<Class> fqnLibraryType = Optional.empty();
-    for (String _package : packagesToSearch) {
-      try {
-        Class c = Class.forName(_package + "." + paramType);
-        return Optional.of(c);
-      }
-      catch (ClassNotFoundException e) {
-//        Log.info("Class " + _package + "." + paramType + " not found!", "ExtendedTemplateController");
-      }
-    }
-    
-    // 3. try primitive types
-    if (primitiveTypes.containsKey(paramType)) {
-      Class c;
-      try {
-        c = Class.forName(primitiveTypes.get(paramType));
-        return Optional.of(c);
-        
-      }
-      catch (ClassNotFoundException e) {
-//        Log.info("Class " + primitiveTypes.get(paramType) + " not found!", "ExtendedTemplateController");
-      }
-      
-    }
-    return fqnLibraryType;
-  }
-  
-  /**
-   * Checks whether there are more than one result definitions.
-   * 
-   * @param result
-   */
-  public void result(String result) {
-    checkArgument(!resultized,
-        "0xA5302 Template '" + getTemplatename() + "': tried to invoke result() twice");
-    resultized = true;
-    
   }
   
 }
