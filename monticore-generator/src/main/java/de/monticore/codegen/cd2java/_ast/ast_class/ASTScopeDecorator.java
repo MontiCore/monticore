bf6d51c8d26cf9fa05365560825cf5df82805c3d
pichler <pichler@se-rwth.de>
2019-05-21 12:27:23 +0200

    first try for generating setEnclosingScope2 methods form super interface

--- monticore-generator/src/main/java/de/monticore/codegen/cd2java/_ast/ast_class/ASTScopeDecorator.java
+++ monticore-generator/src/main/java/de/monticore/codegen/cd2java/_ast/ast_class/ASTScopeDecorator.java
@@ -6,6 +6,7 @@ import de.monticore.codegen.mc2cd.MC2CDStereotypes;
 import de.monticore.codegen.mc2cd.TransformationHelper;
 import de.monticore.generating.templateengine.GlobalExtensionManagement;
 import de.monticore.types.types._ast.ASTReferenceType;
+import de.monticore.types.types._ast.ASTSimpleReferenceType;
 import de.monticore.types.types._ast.ASTType;
 import de.monticore.umlcd4a.cd4analysis._ast.ASTCDAttribute;
 import de.monticore.umlcd4a.cd4analysis._ast.ASTCDType;
@@ -13,8 +14,7 @@ import de.monticore.umlcd4a.cd4analysis._ast.ASTCDType;
 import java.util.ArrayList;
 import java.util.List;
 
-import static de.monticore.codegen.cd2java._symboltable.SymbolTableConstants.ENCLOSING_SCOPE;
-import static de.monticore.codegen.cd2java._symboltable.SymbolTableConstants.SPANNED_SCOPE;
+import static de.monticore.codegen.cd2java._symboltable.SymbolTableConstants.*;
 import static de.monticore.codegen.cd2java.factories.CDModifier.PROTECTED;
 
 public class ASTScopeDecorator extends AbstractDecorator<ASTCDType, List<ASTCDAttribute>> {
@@ -41,6 +41,11 @@ public class ASTScopeDecorator extends AbstractDecorator<ASTCDType, List<ASTCDAt
 
     //add methods for super intrefaces because otherwise the class will not compile
     for (ASTReferenceType referenceType : clazz.getInterfaceList()) {
+      if(referenceType instanceof ASTSimpleReferenceType){
+        ASTSimpleReferenceType simpleReferenceType = (ASTSimpleReferenceType) referenceType;
+        simpleReferenceType.getNameList().set(simpleReferenceType.lastIndexOfName("_ast"), SYMBOL_TABLE_PACKGE);
+        simpleReferenceType.getName(simpleReferenceType.sizeNames()-1).replaceFirst("AST", "I");
+      }
       ASTCDAttribute enclosingScope2Attribute = createEnclosingScope2Attribute(referenceType);
       TransformationHelper.addStereotypeValue(enclosingScope2Attribute.getModifier(), MC2CDStereotypes.INHERITED.toString());
       attributeList.add(enclosingScope2Attribute);
