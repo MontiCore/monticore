eee765587f598c4a04a8eb8b0e33d0ce879a8756
pichler <pichler@se-rwth.de>
2019-06-06 15:59:17 +0200

    add symbol2 getter

--- monticore-generator/src/main/java/de/monticore/codegen/mc2cd/transl/SymbolAndScopeTranslation.java
+++ monticore-generator/src/main/java/de/monticore/codegen/mc2cd/transl/SymbolAndScopeTranslation.java
@@ -4,12 +4,12 @@ import de.monticore.codegen.mc2cd.MC2CDStereotypes;
 import de.monticore.codegen.mc2cd.MCGrammarSymbolTableHelper;
 import de.monticore.codegen.mc2cd.TransformationHelper;
 import de.monticore.codegen.symboltable.SymbolTableGenerator;
-import de.monticore.grammar.grammar._ast.ASTClassProd;
-import de.monticore.grammar.grammar._ast.ASTMCGrammar;
-import de.monticore.grammar.grammar._ast.ASTSymbolDefinition;
+import de.monticore.grammar.grammar._ast.*;
 import de.monticore.grammar.symboltable.MCGrammarSymbol;
 import de.monticore.umlcd4a.cd4analysis._ast.ASTCDClass;
 import de.monticore.umlcd4a.cd4analysis._ast.ASTCDCompilationUnit;
+import de.monticore.umlcd4a.cd4analysis._ast.ASTCDInterface;
+import de.monticore.umlcd4a.cd4analysis._ast.ASTCDType;
 import de.monticore.utils.Link;
 
 import java.util.Optional;
@@ -24,31 +24,41 @@ public class SymbolAndScopeTranslation implements
     for (Link<ASTClassProd, ASTCDClass> link : links.getLinks(ASTClassProd.class, ASTCDClass.class)) {
       final ASTClassProd astClassProd = link.source();
       final ASTCDClass astcdClass = link.target();
+      addSymbolAndScopeStereotypes(astClassProd, astcdClass);
+    }
 
-      for (ASTSymbolDefinition symbolDefinition : astClassProd.getSymbolDefinitionList()) {
-        if (symbolDefinition.isGenSymbol()) {
-          final Optional<MCGrammarSymbol> grammarSymbol = MCGrammarSymbolTableHelper
-              .getMCGrammarSymbol(astClassProd);
-          if (symbolDefinition.isPresentSymbolName()
-              && grammarSymbol.isPresent()) {
-            //extra information into stereotype value if a symboltype is already defined in the grammar
-            String symbolName = symbolDefinition.getSymbolName();
-            String qualifiedName = grammarSymbol.get().getFullName().toLowerCase() + "." +
-                SymbolTableGenerator.PACKAGE + "." + symbolName;
-            TransformationHelper.addStereoType(astcdClass,
-                MC2CDStereotypes.SYMBOL.toString(), qualifiedName + SYMBOL);
-          } else {
-            TransformationHelper.addStereoType(astcdClass,
-                MC2CDStereotypes.SYMBOL.toString());
-          }
+    for (Link<ASTInterfaceProd, ASTCDInterface> link : links.getLinks(ASTInterfaceProd.class, ASTCDInterface.class)) {
+      final ASTInterfaceProd astInterfaceProd = link.source();
+      final ASTCDInterface astcdInterface = link.target();
+      addSymbolAndScopeStereotypes(astInterfaceProd, astcdInterface);
+
+    }
+    return links;
+  }
+
+  private void addSymbolAndScopeStereotypes(ASTParserProd grammarProd, ASTCDType cdType) {
+    for (ASTSymbolDefinition symbolDefinition : grammarProd.getSymbolDefinitionList()) {
+      if (symbolDefinition.isGenSymbol()) {
+        final Optional<MCGrammarSymbol> grammarSymbol = MCGrammarSymbolTableHelper
+            .getMCGrammarSymbol(grammarProd);
+        if (symbolDefinition.isPresentSymbolName()
+            && grammarSymbol.isPresent()) {
+          //extra information into stereotype value if a symboltype is already defined in the grammar
+          String symbolName = symbolDefinition.getSymbolName();
+          String qualifiedName = grammarSymbol.get().getFullName().toLowerCase() + "." +
+              SymbolTableGenerator.PACKAGE + "." + symbolName;
+          TransformationHelper.addStereoType(cdType,
+              MC2CDStereotypes.SYMBOL.toString(), qualifiedName + SYMBOL);
         } else {
-          if (symbolDefinition.isGenScope()) {
-            TransformationHelper.addStereoType(astcdClass,
-                MC2CDStereotypes.SCOPE.toString());
-          }
+          TransformationHelper.addStereoType(cdType,
+              MC2CDStereotypes.SYMBOL.toString());
+        }
+      } else {
+        if (symbolDefinition.isGenScope()) {
+          TransformationHelper.addStereoType(cdType,
+              MC2CDStereotypes.SCOPE.toString());
         }
       }
     }
-    return links;
   }
 }
