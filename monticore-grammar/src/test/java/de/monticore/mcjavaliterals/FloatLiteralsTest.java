c00cc381d9e9c7faf5031e8a65edcbb75baa0eb9
ndienstknecht <dienstknecht@se-rwth.de>
2019-07-03 09:28:26 +0200

    added package literals

--- monticore-grammar/src/test/java/de/monticore/mcjavaliterals/FloatLiteralsTest.java
+++ monticore-grammar/src/test/java/de/monticore/mcjavaliterals/FloatLiteralsTest.java
@@ -2,8 +2,8 @@
 
 package de.monticore.mcjavaliterals;
 
-import de.monticore.mcliteralsbasis._ast.ASTLiteral;
-import de.monticore.mcjavaliterals._ast.ASTFloatLiteral;
+import de.monticore.literals.mcliteralsbasis._ast.ASTLiteral;
+import de.monticore.literals.mcjavaliterals._ast.ASTFloatLiteral;
 import de.se_rwth.commons.logging.Log;
 import de.se_rwth.commons.logging.LogStub;
 import org.junit.BeforeClass;
