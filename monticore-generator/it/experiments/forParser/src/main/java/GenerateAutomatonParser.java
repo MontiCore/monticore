df4065a2cac652619ac14f01bb67277a065d8522
Katrin Hölldobler <hoelldobler@se-rwth.de>
2019-08-07 14:40:41 +0200

    fix test

--- monticore-generator/it/experiments/forParser/src/main/java/GenerateAutomatonParser.java
+++ monticore-generator/it/experiments/forParser/src/main/java/GenerateAutomatonParser.java
@@ -1,24 +1,23 @@
 /* (c) Monticore license: https://github.com/MontiCore/monticore */
 import java.io.IOException;
 
+import de.monticore.generating.templateengine.GlobalExtensionManagement;
+import de.monticore.grammar.grammar._symboltable.GrammarSymbolTableCreatorDelegator;
+import de.monticore.grammar.grammar_withconcepts._symboltable.Grammar_WithConceptsGlobalScope;
+import de.monticore.grammar.grammar_withconcepts._symboltable.Grammar_WithConceptsLanguage;
+import de.monticore.grammar.grammar_withconcepts._symboltable.Grammar_WithConceptsSymbolTableCreatorDelegator;
 import de.se_rwth.commons.logging.Log;
 
 import de.monticore.codegen.mc2cd.MCGrammarSymbolTableHelper;
 import de.monticore.codegen.parser.ParserGenerator;
 
 import java.io.File;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import de.monticore.grammar.grammar._ast.ASTMCGrammar;
-import de.monticore.grammar.symboltable.MCGrammarSymbol;
-import de.monticore.grammar.symboltable.MontiCoreGrammarLanguage;
 import de.monticore.io.paths.IterablePath;
 import de.monticore.io.paths.ModelPath;
-import de.monticore.symboltable.GlobalScope;
-import de.monticore.symboltable.Scope;
 
-import de.monticore.grammar.symboltable.MontiCoreGrammarSymbolTableCreator;
 
 import de.monticore.grammar.grammar_withconcepts._parser.Grammar_WithConceptsParser;
 
@@ -37,11 +36,12 @@ public class GenerateAutomatonParser {
       ast = new Grammar_WithConceptsParser()
                       .parseMCGrammar(filename).get();
 	  
-      // Initialize symbol table 
+      // Initialize symbol table
       // (using imported grammars from the model path)
       ModelPath modelPath = new ModelPath(Paths.get("monticore-cli.jar"));
-      MCGrammarSymbolTableHelper.initializeSymbolTable(ast, modelPath);
-      
+      Grammar_WithConceptsLanguage l = new Grammar_WithConceptsLanguage();
+      Grammar_WithConceptsGlobalScope gs = new Grammar_WithConceptsGlobalScope(modelPath, l);
+      new Grammar_WithConceptsSymbolTableCreatorDelegator(gs);
       // Hand coded path
       IterablePath handcodedPath = IterablePath.empty();
       
@@ -49,7 +49,7 @@ public class GenerateAutomatonParser {
       File outputDir = new File("gen");
       
       // Generate the parser
-      ParserGenerator.generateFullParser(ast, handcodedPath, outputDir);
+      ParserGenerator.generateFullParser(new GlobalExtensionManagement(),ast, gs, handcodedPath, outputDir);
     }
     catch (IOException e) {
       // If something happens ... handling necessary
