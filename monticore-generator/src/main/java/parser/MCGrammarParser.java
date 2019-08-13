955ccbbb4b472affd9db872b5f48c4070f070b09
breuer <breuer@se-rwth.de>
2019-08-13 10:00:28 +0200

    mc

--- monticore-generator/src/main/java/parser/MCGrammarParser.java
+++ monticore-generator/src/main/java/parser/MCGrammarParser.java
@@ -2,11 +2,8 @@
 
 package parser;
 
-import com.google.common.io.Files;
 import de.monticore.grammar.grammar._ast.ASTMCGrammar;
 import de.monticore.grammar.grammar_withconcepts._parser.Grammar_WithConceptsParser;
-import de.monticore.grammar.transformation.GrammarTransformer;
-import de.se_rwth.commons.Names;
 import de.se_rwth.commons.logging.Log;
 
 import java.io.IOException;
@@ -28,38 +25,13 @@ public class MCGrammarParser {
   public static Optional<ASTMCGrammar> parse(Path grammarFile) {
     try {
       Grammar_WithConceptsParser parser = new Grammar_WithConceptsParser();
-      java.util.Optional<ASTMCGrammar> ast = parser.parseMCGrammar(grammarFile.toString());
-      String simplePathName = grammarFile.toString();
-      String packageName = Names.getPackageFromPath(Names.getPathFromFilename(simplePathName));
-
-      if (!parser.hasErrors() && ast.isPresent()) {
-        // Checks
-        String simpleFileName = Files.getNameWithoutExtension(grammarFile.toString());
-        String modelName = ast.get().getName();
-        if (!modelName.equals(simpleFileName)) {
-          Log.error("0xA4003 The grammar name " + modelName + " must be identical to the file name "
-              + simpleFileName + " of "
-              + "the grammar (without its file extension).");
-        }
-        
-        if(!packageName.endsWith(Names.getQualifiedName(ast.get().getPackageList()))){
-          Log.error("0xA4004 The package declaration " + Names.getQualifiedName(ast.get().getPackageList()) + " of the grammar must not differ from the "
-              + "package of the grammar file.");
-        }
-        
-        // Transform
-        GrammarTransformer.transform(ast.get());
-      }
-
-      Optional<ASTMCGrammar> result = Optional.empty();
-      if (ast.isPresent()) {
-        result = Optional.of(ast.get());
-      }
-      return result;
+      java.util.Optional<ASTMCGrammar> ast = parser.parse(grammarFile.toString());
+      return ast;
     }
     catch (IOException e) {
-      throw new RuntimeException(e);
+      Log.error("0XA0115 IOException during parsing of " + grammarFile.toString());
     }
+    return Optional.empty();
   }
 
 }
