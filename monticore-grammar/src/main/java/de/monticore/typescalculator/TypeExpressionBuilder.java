13584e1341979b0e5a415e72a39307de132454fe
ndienstknecht <dienstknecht@se-rwth.de>
2019-08-27 14:50:05 +0200

    tests for deepClone and deepEquals

--- monticore-grammar/src/main/java/de/monticore/typescalculator/TypeExpressionBuilder.java
+++ monticore-grammar/src/main/java/de/monticore/typescalculator/TypeExpressionBuilder.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TypeExpressionBuilder {
-  public static TypeExpression buildTypeConstant(String name) {
+  public static TypeConstant buildTypeConstant(String name) {
     List<String> primitiveTypes = Arrays
             .asList("boolean", "byte", "char", "short", "int", "long", "float", "double");
     if (primitiveTypes.contains(name) ) {
@@ -15,18 +15,26 @@ public class TypeExpressionBuilder {
     return null;
   }
 
-  public static TypeExpression buildObjectType(String name, List<TypeExpression> superTypes) {
+  public static ObjectType buildObjectType(String name, List<TypeExpression> superTypes) {
     ObjectType o = new ObjectType();
     o.setName(name);
     o.setSuperTypes(superTypes);
     return o;
   }
 
-  public static TypeExpression buildGenericTypeExpression(String name, List<TypeExpression> superTypes, List<TypeExpression> arguments){
+  public static GenericTypeExpression buildGenericTypeExpression(String name, List<TypeExpression> superTypes, List<TypeExpression> arguments){
     GenericTypeExpression o = new GenericTypeExpression();
     o.setName(name);
     o.setArguments(arguments);
     o.setSuperTypes(superTypes);
     return o;
   }
+
+  public static TypeVariable buildTypeVariable(String name, List<TypeExpression> superTypes) {
+    TypeVariable o = new TypeVariable();
+    o.setName(name);
+    o.setSuperTypes(superTypes);
+    return o;
+  }
+
 }
--- monticore-grammar/src/test/java/de/monticore/typescalculator/TypeExpressionTest.java
+++ monticore-grammar/src/test/java/de/monticore/typescalculator/TypeExpressionTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.Optional;
 
+import static de.monticore.typescalculator.TypeExpressionBuilder.*;
 import static org.junit.Assert.*;
 
 public class TypeExpressionTest {
@@ -22,8 +23,7 @@ public class TypeExpressionTest {
 
   @Test
   public void deepCloneTypeConstantTest() {
-    TypeExpression e = new TypeConstant();
-    e.setName("double");
+    TypeExpression e = buildTypeConstant("double");
     TypeSymbol s = new TypeSymbol("double");
     e.typeSymbol = java.util.Optional.of(s);
     TypeExpression eClone = e.deepClone();
@@ -34,8 +34,7 @@ public class TypeExpressionTest {
 
   @Test
   public void deepCloneObjectTypeTest() {
-    TypeExpression e = new ObjectType();
-    e.setName("String");
+    TypeExpression e = buildObjectType("String", Lists.newArrayList());
     TypeSymbol s = new TypeSymbol("String");
     e.typeSymbol = java.util.Optional.of(s);
     TypeExpression eClone = e.deepClone();
@@ -43,16 +42,13 @@ public class TypeExpressionTest {
     assertDeepEquals(e,eClone);
 
     //example with super-/subclasses
-
-    TypeExpression ex = new ObjectType();
-    ex.setName("ArrayList");
-    TypeSymbol a = new TypeSymbol("ArrayList");
-    TypeExpression exp = new ObjectType();
-    exp.setName("List");
+    TypeExpression exp = buildObjectType("List", Lists.newArrayList());
     TypeSymbol b = new TypeSymbol("List");
-    ex.typeSymbol = Optional.of(a);
     exp.typeSymbol = Optional.of(b);
-    ex.setSuperTypes(Lists.newArrayList(exp));
+
+    TypeExpression ex = buildObjectType("ArrayList", Lists.newArrayList(exp));
+    TypeSymbol a = new TypeSymbol("ArrayList");
+    ex.typeSymbol = Optional.of(a);
 
     TypeExpression exClone = ex.deepClone();
     assertSame(ex,exClone);
@@ -62,6 +58,31 @@ public class TypeExpressionTest {
 
   @Test
   public void deepCloneGenericTypeTest() {
+    TypeExpression expre = buildObjectType("Bar", Lists.newArrayList());
+    TypeSymbol d = new TypeSymbol("Bar");
+    expre.typeSymbol = Optional.of(d);
+
+
+    GenericTypeExpression expr = buildGenericTypeExpression("SuperFoo",Lists.newArrayList(),Lists.newArrayList(expre));
+    TypeSymbol c = new TypeSymbol("SuperFoo");
+    expr.typeSymbol = Optional.of(c);
+
+    TypeExpression exp = buildObjectType("Bar",Lists.newArrayList());
+    TypeSymbol b = new TypeSymbol("Bar");
+    exp.typeSymbol = Optional.of(b);
+
+    GenericTypeExpression ex = buildGenericTypeExpression("Foo",Lists.newArrayList(expr),Lists.newArrayList(exp));
+    TypeSymbol a = new TypeSymbol("Foo");
+    ex.typeSymbol = Optional.of(a);
+
+    GenericTypeExpression exClone = (GenericTypeExpression) ex.deepClone();
+    assertSame(ex,exClone);
+    assertDeepEquals(ex,exClone);
+    assertSame(ex.getArguments().get(0),exClone.getArguments().get(0));
+    assertSame(ex.getSuperTypes().get(0),exClone.getSuperTypes().get(0));
+    assertSame(((GenericTypeExpression)(ex.getSuperTypes().get(0))).getArguments().get(0),((GenericTypeExpression)exClone.getSuperTypes().get(0)).getArguments().get(0));
+
+
     TypeExpression e = new GenericTypeExpression();
     e.setName("List");
     TypeSymbol s = new TypeSymbol("List");
@@ -69,47 +90,5 @@ public class TypeExpressionTest {
     TypeExpression eClone = e.deepClone();
     assertSame(e,eClone);
     assertDeepEquals(e,eClone);
-
-    //example with generics
-
-    GenericTypeExpression ex = new GenericTypeExpression();
-    ex.setName("Foo");
-    TypeSymbol a = new TypeSymbol("Foo");
-    ex.typeSymbol = Optional.of(a);
-
-    TypeExpression exp = new ObjectType();
-    exp.setName("Bar");
-    TypeSymbol b = new TypeSymbol("Bar");
-    exp.typeSymbol = Optional.of(b);
-
-    ex.setArguments(Lists.newArrayList(exp));
-
-    GenericTypeExpression exClone = (GenericTypeExpression) ex.deepClone();
-    assertSame(ex,exClone);
-    assertDeepEquals(ex,exClone);
-    assertSame(ex.getArguments().get(0),exClone.getArguments().get(0));
-
-    //example with generics and super-/subclasses
-
-    GenericTypeExpression expr = new GenericTypeExpression();
-    expr.setName("SuperFoo");
-    TypeSymbol c = new TypeSymbol("SuperFoo");
-    expr.typeSymbol = Optional.of(c);
-
-    TypeExpression expre = new ObjectType();
-    expre.setName("Bar");
-    TypeSymbol d = new TypeSymbol("Bar");
-    expre.typeSymbol = Optional.of(d);
-
-    expr.setArguments(Lists.newArrayList(expre));
-
-    ex.setSuperTypes(Lists.newArrayList(expr));
-
-    exClone = (GenericTypeExpression) ex.deepClone();
-    assertSame(ex,exClone);
-    assertDeepEquals(ex,exClone);
-    assertSame(ex.getArguments().get(0),exClone.getArguments().get(0));
-    assertSame(ex.getSuperTypes().get(0),exClone.getSuperTypes().get(0));
-    assertSame(((GenericTypeExpression)(ex.getSuperTypes().get(0))).getArguments().get(0),((GenericTypeExpression)exClone.getSuperTypes().get(0)).getArguments().get(0));
   }
 }
