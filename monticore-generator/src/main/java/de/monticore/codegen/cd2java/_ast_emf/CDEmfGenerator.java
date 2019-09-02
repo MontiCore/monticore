220f9c85a520add5c470415d44054300204b0181
pichler <pichler@se-rwth.de>
2019-09-02 11:13:45 +0200

    #2378 fix deepEquals for astrule attributes

--- monticore-generator/src/main/java/de/monticore/codegen/cd2java/_ast_emf/CDEmfGenerator.java
+++ monticore-generator/src/main/java/de/monticore/codegen/cd2java/_ast_emf/CDEmfGenerator.java
@@ -1,7 +1,6 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.codegen.cd2java._ast_emf;
 
-import de.monticore.cd.cd4analysis._ast.ASTCDAttribute;
 import de.monticore.cd.cd4analysis._ast.ASTCDDefinition;
 import de.monticore.cd.cd4analysis._ast.ASTCDInterface;
 import de.monticore.codegen.cd2java.CDGenerator;
@@ -9,8 +8,6 @@ import de.monticore.codegen.cd2java.CoreTemplates;
 import de.monticore.generating.GeneratorSetup;
 
 import java.nio.file.Path;
-import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 
 import static de.monticore.codegen.cd2java._ast_emf.EmfConstants.PACKAGE_SUFFIX;
@@ -31,21 +28,6 @@ public class CDEmfGenerator extends CDGenerator {
     this.originalDefinition = originalDefinition;
   }
 
-  private List<ASTCDAttribute> makeCDAttributesUniqueByName(List<ASTCDAttribute> attrs) {
-    HashSet<String> names = new HashSet<>();
-    LinkedList<ASTCDAttribute> newList = new LinkedList<>();
-
-    for(ASTCDAttribute a : attrs) {
-      if(!names.contains(a.getName())) {
-        names.add(a.getName());
-        newList.add(a);
-      }
-    }
-
-    return newList;
-
-  }
-
   @Override
   protected void generateCDInterfaces(String packageAsPath, List<ASTCDInterface> astcdInterfaceList) {
     for (ASTCDInterface cdInterface : astcdInterfaceList) {
@@ -54,11 +36,6 @@ public class CDEmfGenerator extends CDGenerator {
         //have to use different template for EmfPackage interface because interface has inner interface
         //which cannot be described in CD4A
         //otherwise do not hide logic in templates
-
-        List<ASTCDAttribute> oldList = cdInterface.getCDAttributeList();
-        List<ASTCDAttribute> newList = this.makeCDAttributesUniqueByName(oldList);
-        cdInterface.setCDAttributeList(newList);
-
         this.generatorEngine.generate("_ast_emf.emf_package.EmfPackage", filePath, cdInterface, cdInterface,
             getOriginalDefinition());
       } else {
