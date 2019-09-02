c033ae8730e7d753eafcf2fee41287b4daaffec7
Bernhard Rumpe <rumpe@se-rwth.de>
2019-09-02 20:00:03 +0200

    BR from q

--- monticore-grammar/src/test/java/de/monticore/types2/SynthesizeSymTypeFromMCCollectionTypesTest.java
+++ monticore-grammar/src/test/java/de/monticore/types2/SynthesizeSymTypeFromMCCollectionTypesTest.java
@@ -3,65 +3,53 @@ package de.monticore.types2;
 
 import com.google.common.collect.Lists;
 import de.monticore.types.mcbasictypes._ast.*;
-import de.monticore.types.mcbasictypestest._ast.MCBasicTypesTestMill;
 import de.monticore.types.mcbasictypestest._parser.MCBasicTypesTestParser;
+import de.monticore.types.mccollectiontypes._ast.ASTMCListType;
+import de.monticore.types.mccollectiontypes._ast.ASTMCTypeArgument;
+import de.monticore.types.mccollectiontypestest._parser.MCCollectionTypesTestParser;
 import de.se_rwth.commons.logging.Log;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Optional;
 
 import static de.monticore.types2.SymTypeExpressionFactory.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
-public class TypeCheckBasicTest {
+public class SynthesizeSymTypeFromMCCollectionTypesTest {
   
   /**
    * Focus: Interplay between TypeCheck and the assisting visitors on the
-   * Basic configuration, i.e.
+   * extended configuration,
    * i.e. for
-   *    expressions/ExpressionsBasis.mc4
-   *    literals/MCLiteralsBasis.mc4
-   *    types/MCBasicTypes.mc4
+   *    types/MCCollectionTypes.mc4
    */
   
-  @BeforeClass
-  public static void setup() {
+  @Before
+  public void setup() {
     Log.init();
     Log.enableFailQuick(false);
   }
   
-
   // Parer used for convenience:
-  MCBasicTypesTestParser parser = new MCBasicTypesTestParser();
+  MCCollectionTypesTestParser parser = new MCCollectionTypesTestParser();
+  
+  // This is Visitor for Collection types under test:
+  SynthesizeSymTypeFromMCCollectionTypes synt = new SynthesizeSymTypeFromMCCollectionTypes();
+  
   // This is the TypeChecker under Test:
-  TypeCheck tc = new TypeCheckBasic();
+  TypeCheck tc = new TypeCheckBasic(synt);
   
   // ------------------------------------------------------  Tests for Function 1, 1b, 1c
+
+  // reuse some of the tests from MCBasicTypes (to check conformity)
   
   @Test
   public void symTypeFromAST_Test1() throws IOException {
     String s = "double";
-    ASTMCType asttype = parser.parse_StringMCType(s).get();
-    assertEquals(s, tc.symTypeFromAST(asttype).print());
-  }
-  
-  @Test
-  public void symTypeFromAST_Test2() throws IOException {
-    String s = "int";
-    ASTMCType asttype = parser.parse_StringMCType(s).get();
-    assertEquals(s, tc.symTypeFromAST(asttype).print());
-  }
-  
-  @Test
-  public void symTypeFromAST_Test3() throws IOException {
-    String s = "A";
+    parser = new MCCollectionTypesTestParser();
     ASTMCType asttype = parser.parse_StringMCType(s).get();
     assertEquals(s, tc.symTypeFromAST(asttype).print());
   }
@@ -81,26 +69,13 @@ public class TypeCheckBasicTest {
   }
   
   @Test
-  public void symTypeFromAST_VoidTest() throws IOException {
-    ASTMCVoidType v = MCBasicTypesMill.mCVoidTypeBuilder().build();
-    assertEquals("void", tc.symTypeFromAST(v).print());
-  }
-  
-  @Test
   public void symTypeFromAST_ReturnTest() throws IOException {
     ASTMCVoidType v = MCBasicTypesMill.mCVoidTypeBuilder().build();
-    ASTMCReturnType r = MCBasicTypesMill.mCReturnTypeBuilder().setMCVoidType(v).build();
+    ASTMCReturnType r = MCBasicTypesMill.mCReturnTypeBuilder()
+                                  .setMCVoidType(v).build();
     assertEquals("void", tc.symTypeFromAST(r).print());
   }
 
-  @Test
-  public void symTypeFromAST_ReturnTest2() throws IOException {
-    ASTMCVoidType v = MCBasicTypesMill.mCVoidTypeBuilder().build();
-    // im Prinzip dassselbe via Parser:
-    ASTMCReturnType r = parser.parse_StringMCReturnType("void").get();
-    assertEquals("void", tc.symTypeFromAST(r).print());
-  }
-  
   @Test
   public void symTypeFromAST_ReturnTest3() throws IOException {
     // und nochmal einen normalen Typ:
@@ -108,8 +83,36 @@ public class TypeCheckBasicTest {
     ASTMCReturnType r = parser.parse_StringMCReturnType(s).get();
     assertEquals(s, tc.symTypeFromAST(r).print());
   }
+
+  // new forms of Types coming from MCCollectionType
   
-  // ------------------------------------------------------  Tests for Function 2
+  @Test
+  public void symTypeFromAST_TestListQual() throws IOException {
+    String s = "List<a.z.Person>";
+    ASTMCType asttype = parser.parse_StringMCType(s).get();
+    assertEquals(s, tc.symTypeFromAST(asttype).print());
+  }
+  
+  @Test
+  public void symTypeFromAST_TestListQual2() throws IOException {
+    String s = "Set<Auto>";
+    ASTMCType asttype = parser.parse_StringMCType(s).get();
+    assertEquals(s, tc.symTypeFromAST(asttype).print());
+  }
+  
+  @Test
+  public void symTypeFromAST_TestListQual3() throws IOException {
+    String s = "Map<int,Auto>";
+    ASTMCType asttype = parser.parse_StringMCType(s).get();
+    assertEquals(s, tc.symTypeFromAST(asttype).print());
+  }
+  
+  @Test
+  public void symTypeFromAST_TestListQual4() throws IOException {
+    String s = "Set<int>";
+    ASTMCType asttype = parser.parse_StringMCType(s).get();
+    assertEquals(s, tc.symTypeFromAST(asttype).print());
+  }
   
   
   
