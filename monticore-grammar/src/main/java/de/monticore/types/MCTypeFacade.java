ab90d0e88206312285c290342ca75a63e47613d5
pichler <pichler@se-rwth.de>
2019-10-16 10:07:23 +0200

    fix facade

--- monticore-grammar/src/main/java/de/monticore/types/MCTypeFacade.java
+++ monticore-grammar/src/main/java/de/monticore/types/MCTypeFacade.java
@@ -4,18 +4,13 @@
 package de.monticore.types;
 
 import com.google.common.collect.Lists;
-import de.monticore.cd.cd4code._ast.CD4CodeMill;
-import de.monticore.cd.cd4code._parser.CD4CodeParser;
 import de.monticore.types.mcbasictypes._ast.*;
 import de.monticore.types.mccollectiontypes._ast.*;
 import de.monticore.types.mcfullgenerictypes._ast.ASTMCArrayType;
 import de.monticore.types.mcfullgenerictypes._ast.ASTMCWildcardTypeArgument;
 import de.monticore.types.mcfullgenerictypes._ast.MCFullGenericTypesMill;
-import de.se_rwth.commons.logging.Log;
 
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Optional;
 
 /**
  * Class that helps with the creation of ASTMCTypes
@@ -27,10 +22,7 @@ public class MCTypeFacade {
 
   private static MCTypeFacade MCTypeFacade;
 
-  private final MCfullgenerictypep parser;
-
   private MCTypeFacade() {
-    this.parser = new CD4CodeParser();
   }
 
   public static MCTypeFacade getInstance() {
@@ -40,6 +32,9 @@ public class MCTypeFacade {
     return MCTypeFacade;
   }
 
+  /**
+   * qualified type creation methods
+   */
 
   public ASTMCQualifiedType createQualifiedType(final Class<?> clazz) {
     return createQualifiedType(clazz.getSimpleName());
@@ -50,98 +45,13 @@ public class MCTypeFacade {
     return MCBasicTypesMill.mCQualifiedTypeBuilder().setMCQualifiedName(qualName).build();
   }
 
-  public ASTMCObjectType createOptionalTypeOf(final Class<?> clazz) {
-    return createOptionalTypeOf(clazz.getSimpleName());
-  }
+  /**
+   * typeArgument creation methods
+   */
 
-  public ASTMCOptionalType createOptionalTypeOf(final String name) {
-    ASTMCTypeArgument arg = MCFullGenericTypesMill.mCBasicTypeArgumentBuilder().setMCQualifiedType(createQualifiedType(name)).build();
-    return MCFullGenericTypesMill.mCOptionalTypeBuilder().addMCTypeArgument(arg).build();
-  }
-
-  public ASTMCOptionalType createOptionalTypeOf(final ASTMCType type) {
-    return createOptionalTypeOf(MCFullGenericTypesHelper.printType(type));
-  }
-
-  public ASTMCOptionalType createOptionalTypeOf(final ASTMCTypeArgument type) {
-    return createOptionalTypeOf(MCFullGenericTypesHelper.printType(type));
-  }
-
-  public ASTMCListType createListTypeOf(final Class<?> clazz) {
-    return createListTypeOf(clazz.getSimpleName());
-  }
-
-  public ASTMCListType createListTypeOf(final String name) {
-    Optional<ASTMCTypeArgument> arg = null;
-    try {
-      arg = new CD4CodeParser().parse_StringMCTypeArgument(name);
-    } catch (IOException e) {
-      Log.error("0xA0115 Cannot generate TypeArgument for " + name);
-    }
-    if (!arg.isPresent()) {
-      Log.error("0xA0116 Cannot generate TypeArgument for " + name);
-    }
-    return CD4CodeMill.mCListTypeBuilder().addMCTypeArgument(arg.get()).build();
-  }
-
-  public ASTMCListType createListTypeOf(final ASTMCType type) {
-    return createListTypeOf(MCCollectionTypesHelper.printType(type));
-  }
-
-  public ASTMCSetType createSetTypeOf(final Class<?> clazz) {
-    return createSetTypeOf(clazz.getSimpleName());
-  }
-
-  public ASTMCSetType createSetTypeOf(final String name) {
-    Optional<ASTMCTypeArgument> arg = MCFullGenericTypesMill.typearg;
-    try {
-      arg = new CD4CodeParser().parse_StringMCTypeArgument(name);
-    } catch (IOException e) {
-      Log.error("0xA0115 Cannot generate TypeArgument for " + name);
-    }
-    if (!arg.isPresent()) {
-      Log.error("0xA0116 Cannot generate TypeArgument for " + name);
-    }
-    return CD4CodeMill.mCSetTypeBuilder().addMCTypeArgument(arg.get()).build();
-  }
-
-  public ASTMCSetType createSetTypeOf(final ASTMCType type) {
-    return createSetTypeOf(MCCollectionTypesHelper.printType(type));
-  }
-
-  public ASTMCType createCollectionTypeOf(final Class<?> clazz) {
-    return createCollectionTypeOf(clazz.getSimpleName());
-  }
-
-  public ASTMCType createCollectionTypeOf(final String name) {
-    ASTMCTypeArgument arg = MCFullGenericTypesMill.mCBasicTypeArgumentBuilder().setMCQualifiedType(createQualifiedType(name)).build();
-    // TODO Ersetze CD4Code durch was besseres
-    return MCFullGenericTypesMill.mCBasicGenericTypeBuilder().setNameList(Lists.newArrayList("Collection"))
-        .setMCTypeArgumentList(Lists.newArrayList(arg)).build();
-  }
-
-  public ASTMCType createCollectionTypeOf(final ASTMCType type) {
-    return createCollectionTypeOf(MCCollectionTypesHelper.printType(type));
-  }
-
-  public ASTMCMapType createMapTypeOf(final String firstType, final String secondType) {
-    ASTMCTypeArgument first = MCFullGenericTypesMill.mCBasicTypeArgumentBuilder().setMCQualifiedType(createQualifiedType(firstType)).build();
-    ASTMCTypeArgument second = MCFullGenericTypesMill.mCBasicTypeArgumentBuilder().setMCQualifiedType(createQualifiedType(secondType)).build();
-    return MCFullGenericTypesMill.mCMapTypeBuilder().setKey(first).setValue(second).build();
-  }
-
-  public ASTMCArrayType createArrayType(final Class<?> clazz, int dimension) {
-    return createArrayType(clazz.getSimpleName(), dimension);
-  }
-
-  public ASTMCArrayType createArrayType(final String name, int dimension) {
-    return createArrayType(this.createQualifiedType(name), dimension);
-  }
-
-  public ASTMCArrayType createArrayType(final ASTMCType type, int dimension) {
-    return MCFullGenericTypesMill.mCArrayTypeBuilder()
-        .setMCType(type)
-        .setDimensions(dimension)
+  public ASTMCBasicTypeArgument createBasicTypeArgumentOf(String name) {
+    return MCFullGenericTypesMill.mCBasicTypeArgumentBuilder()
+        .setMCQualifiedType(createQualifiedType(name))
         .build();
   }
 
@@ -159,6 +69,120 @@ public class MCTypeFacade {
         .build();
   }
 
+  /**
+   * optional type of ASTMCBasicTypeArgument
+   */
+
+  public ASTMCOptionalType createOptionalTypeOf(final String name) {
+    return MCFullGenericTypesMill.mCOptionalTypeBuilder()
+        .addMCTypeArgument(createBasicTypeArgumentOf(name))
+        .build();
+  }
+
+  public ASTMCOptionalType createOptionalTypeOf(final Class<?> clazz) {
+    return createOptionalTypeOf(clazz.getSimpleName());
+  }
+
+  public ASTMCOptionalType createOptionalTypeOf(final ASTMCType type) {
+    return createOptionalTypeOf(MCFullGenericTypesHelper.printType(type));
+  }
+
+  /**
+   * list types of ASTMCBasicTypeArgument
+   */
+
+  public ASTMCListType createListTypeOf(final String name) {
+    return MCFullGenericTypesMill.mCListTypeBuilder()
+        .addMCTypeArgument(createBasicTypeArgumentOf(name))
+        .build();
+  }
+
+  public ASTMCListType createListTypeOf(final Class<?> clazz) {
+    return createListTypeOf(clazz.getSimpleName());
+  }
+
+  public ASTMCListType createListTypeOf(final ASTMCType type) {
+    return createListTypeOf(MCCollectionTypesHelper.printType(type));
+  }
+
+  /**
+   * set types of ASTMCBasicTypeArgument
+   */
+
+  public ASTMCSetType createSetTypeOf(final String name) {
+    return MCFullGenericTypesMill.mCSetTypeBuilder()
+        .addMCTypeArgument(createBasicTypeArgumentOf(name))
+        .build();
+  }
+
+  public ASTMCSetType createSetTypeOf(final Class<?> clazz) {
+    return createSetTypeOf(clazz.getSimpleName());
+  }
+
+  public ASTMCSetType createSetTypeOf(final ASTMCType type) {
+    return createSetTypeOf(MCCollectionTypesHelper.printType(type));
+  }
+
+  /**
+   * collection types of ASTMCBasicTypeArgument
+   */
+
+  public ASTMCType createCollectionTypeOf(final String name) {
+    return MCFullGenericTypesMill.mCBasicGenericTypeBuilder()
+        .setNameList(Lists.newArrayList("Collection"))
+        .setMCTypeArgumentList(Lists.newArrayList(createBasicTypeArgumentOf(name)))
+        .build();
+  }
+
+  public ASTMCType createCollectionTypeOf(final Class<?> clazz) {
+    return createCollectionTypeOf(clazz.getSimpleName());
+  }
+
+  public ASTMCType createCollectionTypeOf(final ASTMCType type) {
+    return createCollectionTypeOf(MCCollectionTypesHelper.printType(type));
+  }
+
+  /**
+   * map types of ASTMCBasicTypeArgument
+   */
+
+  public ASTMCMapType createMapTypeOf(final String firstType, final String secondType) {
+    return MCFullGenericTypesMill.mCMapTypeBuilder()
+        .setKey(createBasicTypeArgumentOf(firstType))
+        .setValue(createBasicTypeArgumentOf(secondType))
+        .build();
+  }
+
+  public ASTMCMapType createMapTypeOf(final Class<?> firstType, final Class<?> secondType) {
+    return createMapTypeOf(firstType.getSimpleName(), secondType.getSimpleName());
+  }
+
+  public ASTMCMapType createMapTypeOf(final ASTMCType firstType, final ASTMCType secondType) {
+    return createMapTypeOf(MCCollectionTypesHelper.printType(firstType), MCCollectionTypesHelper.printType(secondType));
+  }
+
+  /**
+   * array types
+   */
+
+  public ASTMCArrayType createArrayType(final ASTMCType type, int dimension) {
+    return MCFullGenericTypesMill.mCArrayTypeBuilder()
+        .setMCType(type)
+        .setDimensions(dimension)
+        .build();
+  }
+
+  public ASTMCArrayType createArrayType(final Class<?> clazz, int dimension) {
+    return createArrayType(clazz.getSimpleName(), dimension);
+  }
+
+  public ASTMCArrayType createArrayType(final String name, int dimension) {
+    return createArrayType(this.createQualifiedType(name), dimension);
+  }
+
+  /**
+   * primitive types
+   */
 
   public ASTMCVoidType createVoidType() {
     return MCBasicTypesMill.mCVoidTypeBuilder()
