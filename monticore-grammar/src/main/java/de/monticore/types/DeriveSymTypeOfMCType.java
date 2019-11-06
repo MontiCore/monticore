e96498f12e7530a1da7693f3d3ab7310fe9b024c
pichler <pichler@se-rwth.de>
2019-11-06 09:44:54 +0100

    reactivate MCType2SymTypeExpressionTest

--- monticore-grammar/src/main/java/de/monticore/types/DeriveSymTypeOfMCType.java
+++ monticore-grammar/src/main/java/de/monticore/types/DeriveSymTypeOfMCType.java
@@ -108,9 +108,19 @@ public class DeriveSymTypeOfMCType implements MCFullGenericTypesVisitor {
     typeArgumentMapping.put(basicTypeArgument,o);
   }
   public void endVisit(ASTMCCustomTypeArgument basicTypeArgument) {
-    SymTypeOfObject o = createTypeObject(basicTypeArgument.getMCType().getName());
-    //TODO RE rekursiv fehlt!
-    typeArgumentMapping.put(basicTypeArgument,o);
+    if (basicTypeArgument.getMCType() instanceof ASTMCGenericType) {
+      List<SymTypeExpression> argumentList = new LinkedList<>();
+      for (ASTMCTypeArgument typeArg : ((ASTMCGenericType) basicTypeArgument.getMCType()).getMCTypeArgumentList()) {
+        argumentList.add(typeArgumentMapping.get(typeArg));
+      }
+      SymTypeOfGenerics o = createGenerics(basicTypeArgument.getMCType().getName(), argumentList);
+      //TODO RE rekursiv fehlt!
+      typeArgumentMapping.put(basicTypeArgument, o);
+    } else {
+      SymTypeOfObject o = createTypeObject(basicTypeArgument.getMCType().getName());
+      //TODO RE rekursiv fehlt!
+      typeArgumentMapping.put(basicTypeArgument, o);
+    }
   }
 
   public void endVisit(ASTMCWildcardTypeArgument basicTypeArgument) {
