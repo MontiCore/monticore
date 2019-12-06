ff2792310ca22f4dbae112573516f07869e03e65
pichler <pichler@se-rwth.de>
2019-12-06 10:46:22 +0100

    line breaks

--- docs/scripts/findDoubleFileNames.sh
+++ docs/scripts/findDoubleFileNames.sh
@@ -83,10 +83,7 @@ diff $filelist.2 $filelist.1 \
 for i in `cat $filelist.3`
 do
   echo $i "    in: <br/>"
-  grep -i "/"$i"\." $filelist.0 > $filelist.0
-  sed -i 's/^/  /g' $filelist.0
-  sed -i '/^0x/G' $filelist.0
-  cat $filelist.0
+  grep "/"$i"\." $filelist.0 | sed 's/$/<br\/>/g'
   echo "<br/>"
 done
 
