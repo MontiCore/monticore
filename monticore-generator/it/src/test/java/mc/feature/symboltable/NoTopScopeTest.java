edc977a507e2df16a44a4d3e3d7c2467a65c1e8f
pichler <pichler@se-rwth.de>
2020-01-28 08:35:36 +0100

    add test for inherited symbols

--- monticore-generator/it/src/test/java/mc/feature/symboltable/NoTopScopeTest.java
+++ monticore-generator/it/src/test/java/mc/feature/symboltable/NoTopScopeTest.java
@@ -5,6 +5,12 @@ import de.monticore.symboltable.ISymbol;
 import mc.feature.symboltable.notopscope._ast.ASTFoo;
 import mc.feature.symboltable.notopscope._parser.NoTopScopeParser;
 import mc.feature.symboltable.notopscope._symboltable.*;
+import mc.feature.symboltable.subnotopscope._ast.ASTSubFoo;
+import mc.feature.symboltable.subnotopscope._parser.SubNoTopScopeParser;
+import mc.feature.symboltable.subnotopscope._symboltable.SubNoTopScopeArtifactScope;
+import mc.feature.symboltable.subnotopscope._symboltable.SubNoTopScopeGlobalScope;
+import mc.feature.symboltable.subnotopscope._symboltable.SubNoTopScopeLanguage;
+import mc.feature.symboltable.subnotopscope._symboltable.SubNoTopScopeSymbolTableCreatorDelegator;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,10 +22,15 @@ import static org.junit.Assert.*;
 
 public class NoTopScopeTest {
 
-  private NoTopScopeArtifactScope scope;
+  /**
+   * test the generation of the method getTopLevelSymbol() of an ArtifactScope
+   * if the start production is only a symbol and not a scope spanning symbol
+   * -> check if exactly one symbol is in the ArtifactScope
+   * -> do NOT check for SubScopes since there exists none here
+   */
 
-  @Before
-  public void setUp() throws IOException {
+  @Test
+  public void testGetTopLevelSymbol() throws IOException {
     NoTopScopeParser scopeAttributesParser = new NoTopScopeParser();
     Optional<ASTFoo> astSup = scopeAttributesParser.parse("src/test/resources/mc/feature/symboltable/NoTopScope.st");
     assertFalse(scopeAttributesParser.hasErrors());
@@ -29,20 +40,42 @@ public class NoTopScopeTest {
     NoTopScopeLanguage lang = new NoTopScopeLanguage();
     NoTopScopeGlobalScope globalScope = new NoTopScopeGlobalScope(modelPath, lang);
     NoTopScopeSymbolTableCreatorDelegator symbolTableCreator = new NoTopScopeSymbolTableCreatorDelegator(globalScope);
-    scope = symbolTableCreator.createFromAST(astSup.get());
-  }
+    NoTopScopeArtifactScope scope = symbolTableCreator.createFromAST(astSup.get());
 
-  /**
-   * test the generation of the method getTopLevelSymbol() of an ArtifactScope
-   * if the start production is only a symbol and not a scope spanning symbol
-   * -> check if exactly one symbol is in the ArtifactScope
-   * -> do NOT check for SubScopes since there exists none here
-   */
-
-  @Test
-  public void testGetTopLevelSymbol(){
+    // only one symbol
     Optional<ISymbol> topLevelSymbol = scope.getTopLevelSymbol();
     assertTrue(topLevelSymbol.isPresent());
     assertEquals("A", topLevelSymbol.get().getName());
+
+    // two symbols
+    FooSymbol eSymbol = new FooSymbol("E");
+    scope.add(eSymbol);
+    topLevelSymbol = scope.getTopLevelSymbol();
+    assertFalse(topLevelSymbol.isPresent());
+  }
+
+  @Test
+  public void testGetTopLevelSymbolWithInherited() throws IOException {
+    SubNoTopScopeParser scopeAttributesParser = new SubNoTopScopeParser();
+    Optional<ASTSubFoo> astSup = scopeAttributesParser.parse("src/test/resources/mc/feature/symboltable/SubNoTopScope.st");
+    assertFalse(scopeAttributesParser.hasErrors());
+    assertTrue(astSup.isPresent());
+
+    ModelPath modelPath = new ModelPath(Paths.get("src/test/resources/mc/feature/symboltable"));
+    SubNoTopScopeLanguage lang = new SubNoTopScopeLanguage();
+    SubNoTopScopeGlobalScope globalScope = new SubNoTopScopeGlobalScope(modelPath, lang);
+    SubNoTopScopeSymbolTableCreatorDelegator symbolTableCreator = new SubNoTopScopeSymbolTableCreatorDelegator(globalScope);
+    SubNoTopScopeArtifactScope scope = symbolTableCreator.createFromAST(astSup.get());
+
+    // only one symbol
+    Optional<ISymbol> topLevelSymbol = scope.getTopLevelSymbol();
+    assertTrue(topLevelSymbol.isPresent());
+    assertEquals("A", topLevelSymbol.get().getName());
+
+    // two symbols (add symbol from super grammar)
+    FooSymbol eSymbol = new FooSymbol("E");
+    scope.add(eSymbol);
+    topLevelSymbol = scope.getTopLevelSymbol();
+    assertFalse(topLevelSymbol.isPresent());
   }
 }
