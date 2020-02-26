ef1aebbf30779f28d6802807fc736a61356f752e
Arvid Butting <butting@se-rwth.de>
2020-02-26 17:07:35 +0100

    Revert "fix it tests"
    This reverts commit eacaedaecdc85d349a1afa6788db461753acbfd3

--- monticore-generator/it/src/main/java/mc/embedding/composite/_symboltable/ICompositeGlobalScope.java
+++ monticore-generator/it/src/main/java/mc/embedding/composite/_symboltable/ICompositeGlobalScope.java
@@ -1,30 +1,24 @@
 /* (c) https://github.com/MontiCore/monticore */
-package mc.embedding.external.composite._symboltable;
+package mc.embedding.composite._symboltable;
 
-
-import de.monticore.io.paths.ModelPath;
 import de.monticore.symboltable.modifiers.AccessModifier;
-import mc.embedding.external.embedded._symboltable.TextSymbol;
+import mc.embedding.embedded._symboltable.TextSymbol;
+import mc.embedding.host._symboltable.ContentSymbol;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
-public class CompositeGlobalScope extends CompositeGlobalScopeTOP{
-
-  public CompositeGlobalScope(ModelPath modelPath,
-      CompositeLanguage language) {
-    super(modelPath, language);
-  }
-
-  @Override public List<ContentSymbol> resolveAdaptedContent(boolean foundSymbols,
+public interface ICompositeGlobalScope extends ICompositeGlobalScopeTOP{
+  
+  @Override default List<ContentSymbol> resolveAdaptedContent(boolean foundSymbols,
                                                               String symbolName, AccessModifier modifier, Predicate<ContentSymbol> predicate) {
     Collection<TextSymbol> symbols = resolveTextMany(foundSymbols, symbolName, modifier, x -> true);
     return symbols.stream().map(s -> new Text2ContentAdapter(s)).collect(Collectors.toList());
   }
   
-  @Override public CompositeGlobalScope getRealThis() {
+  @Override default ICompositeGlobalScope getRealThis(){
     return this;
   }
 }
