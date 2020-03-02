ebf60269e259c5caadfc3d31d44ebd725661804c
Katrin Hölldobler <hoelldobler@se-rwth.de>
2020-03-02 16:05:48 +0100

    minor

--- docs/scripts/preprocessing.sh
+++ docs/scripts/preprocessing.sh
@@ -4,11 +4,10 @@
 # is used from '.gitlab-ci.yml'(gitlab) and '.travis.yml'(github)
 
 # execute report scripts and print output to *.md file, to use these in pages
-mkdir docs/_intern
-sh docs/scripts/errorList.sh '../../' 'target/site/errorList' > docs/_intern/ErrorList.md
-sh docs/scripts/detailedErrorList.sh '../../' 'target/site/detailedErrorList' > docs/_intern/DetailedErrorList.md
-sh docs/scripts/findDoubleFileNames.sh './' 'target/site/findDoubleFileNames' > docs/_intern/FindDoubleFileNames.md
-sh docs/scripts/ftlAnalysis.sh './' 'configure.StartAllOutput' 'target/site/ftlAnalysis' > docs/_intern/FtlAnalysis.md
+sh docs/scripts/errorList.sh '../../' 'target/site/errorList' > docs/scripts/ErrorList.md
+sh docs/scripts/detailedErrorList.sh '../../' 'target/site/detailedErrorList' > docs/scripts/DetailedErrorList.md
+sh docs/scripts/findDoubleFileNames.sh './' 'target/site/findDoubleFileNames' > docs/scripts/FindDoubleFileNames.md
+sh docs/scripts/ftlAnalysis.sh './' 'configure.StartAllOutput' 'target/site/ftlAnalysis' > docs/scripts/FtlAnalysis.md
 echo "[INFO] Executed report scripts for pages"
 
 # move all directories that contain *.md files to the docs folder
