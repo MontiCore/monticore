72f48aa86d7d918b19df738a45054bce8ed10ef3
Katrin Hölldobler <hoelldobler@se-rwth.de>
2020-03-03 08:56:58 +0100

    links again

--- docs/scripts/preprocessing.sh
+++ docs/scripts/preprocessing.sh
@@ -4,17 +4,18 @@
 # is used from '.gitlab-ci.yml'(gitlab) and '.travis.yml'(github)
 
 # execute report scripts and print output to *.md file, to use these in pages
-sh docs/scripts/errorList.sh '../../' 'target/site/errorList' > docs/scripts/ErrorList.md
-sh docs/scripts/detailedErrorList.sh '../../' 'target/site/detailedErrorList' > docs/scripts/DetailedErrorList.md
-sh docs/scripts/findDoubleFileNames.sh './' 'target/site/findDoubleFileNames' > docs/scripts/FindDoubleFileNames.md
-sh docs/scripts/ftlAnalysis.sh './' 'configure.StartAllOutput' 'target/site/ftlAnalysis' > docs/scripts/FtlAnalysis.md
-echo "[INFO] Executed report scripts for pages"
+#sh docs/scripts/errorList.sh '../../' 'target/site/errorList' > docs/scripts/ErrorList.md
+#sh docs/scripts/detailedErrorList.sh '../../' 'target/site/detailedErrorList' > docs/scripts/DetailedErrorList.md
+#sh docs/scripts/findDoubleFileNames.sh './' 'target/site/findDoubleFileNames' > docs/scripts/FindDoubleFileNames.md
+#sh docs/scripts/ftlAnalysis.sh './' 'configure.StartAllOutput' 'target/site/ftlAnalysis' > docs/scripts/FtlAnalysis.md
+#echo "[INFO] Executed report scripts for pages"
 
 # move all directories that contain *.md files to the docs folder
 # because mkdocs can only find *.md files there
 mv monticore-grammar docs/monticore-grammar
 mv monticore-runtime docs/monticore-runtime
 mv 00.org docs/00.org
+mv docs/*.md docs/docs/
 mv *.md docs/
 mv *.png docs/
 echo "[INFO] Moved *.md files to 'docs' folder"
