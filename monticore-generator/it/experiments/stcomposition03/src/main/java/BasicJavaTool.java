6a5571e0182d25ca041c5a9aef17cd18fd2df4c6
Butting <butting@se-rwth.de>
2020-04-17 21:07:09 +0200

    Symbol Table aggregation experiment

---
+++ monticore-generator/it/experiments/stcomposition03/src/main/java/BasicJavaTool.java
@@ -0,0 +1,53 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+import basicjava._ast.ASTCompilationUnit;
+import basicjava._parser.BasicJavaParser;
+import basicjava._symboltable.*;
+import de.monticore.io.paths.ModelPath;
+import de.se_rwth.commons.logging.Log;
+import org.antlr.v4.runtime.RecognitionException;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class BasicJavaTool {
+
+  public static BasicJavaArtifactScope createJavaSymTab(String model, ModelPath modelPath) {
+    ASTCompilationUnit ast = parse(model);
+    BasicJavaGlobalScope globalScope = BasicJavaSymTabMill.basicJavaGlobalScopeBuilder()
+        .setModelPath(modelPath)
+        .setBasicJavaLanguage(new BasicJavaLanguage()) //will be removed soon
+        .build();
+
+    //initialize symbol table creators
+    BasicJavaSymbolTableCreator stc = BasicJavaSymTabMill
+        .basicJavaSymbolTableCreatorBuilder()
+        .addToScopeStack(globalScope)
+        .build();
+
+    return stc.createFromAST(ast);
+  }
+
+  /**
+   * Parse the model contained in the specified file.
+   *
+   * @param model - file to parse
+   * @return
+   */
+  public static ASTCompilationUnit parse(String model) {
+    try { BasicJavaParser parser = new BasicJavaParser() ;
+      Optional<ASTCompilationUnit> optResult = parser.parse(model);
+
+      if (!parser.hasErrors() && optResult.isPresent()) {
+        return optResult.get();
+      }
+      Log.error("Model could not be parsed.");
+    }
+    catch (RecognitionException | IOException e) {
+      Log.error("Failed to parse " + model, e);
+    }
+    System.exit(1);
+    return null;
+  }
+
+}
--- monticore-generator/it/experiments/stcomposition03/src/main/java/javaandaut/JavaAndAutTool.java
+++ monticore-generator/it/experiments/stcomposition03/src/main/java/javaandaut/JavaAndAutTool.java
@@ -2,5 +2,48 @@
 
 package javaandaut;
 
+import automata6._ast.ASTAutomaton;
+import automata6._parser.Automata6Parser;
+import automata6._symboltable.*;
+import de.monticore.io.paths.ModelPath;
+import de.se_rwth.commons.logging.Log;
+import org.antlr.v4.runtime.RecognitionException;
+
+import java.io.IOException;
+import java.util.Optional;
+
 public class JavaAndAutTool {
+
+  public static Automata6ArtifactScope createJavaAndAutSymTab(String model, ModelPath modelPath) {
+    ASTAutomaton ast = parseAut(model);
+    Automata6GlobalScope globalScope = Automata6SymTabMill.automata6GlobalScopeBuilder()
+        .setModelPath(modelPath)
+        .setAutomata6Language(new Automata6Language()) //will be removed soon
+        .build();
+    globalScope.addAdaptedStimulusSymbolResolvingDelegate(new AutomataResolvingDelegate(modelPath));
+
+    //initialize symbol table creators
+    Automata6SymbolTableCreator stc = Automata6SymTabMill
+        .automata6SymbolTableCreatorBuilder()
+        .addToScopeStack(globalScope)
+        .build();
+
+    return stc.createFromAST(ast);
+  }
+
+  public static ASTAutomaton parseAut(String model) {
+    try { Automata6Parser parser = new Automata6Parser() ;
+      Optional<ASTAutomaton> optResult = parser.parse(model);
+
+      if (!parser.hasErrors() && optResult.isPresent()) {
+        return optResult.get();
+      }
+      Log.error("Model could not be parsed.");
+    }
+    catch (RecognitionException | IOException e) {
+      Log.error("Failed to parse " + model, e);
+    }
+    System.exit(1);
+    return null;
+  }
 }
