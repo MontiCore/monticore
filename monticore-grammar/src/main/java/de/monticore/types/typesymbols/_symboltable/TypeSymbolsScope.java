914a7f1ee93a784405ee0cf572879fd0cfb39bdd
hoelldobler <hoelldobler@se-rwth.de>
2020-06-02 16:12:55 +0200

    copyright

--- monticore-grammar/src/main/java/de/monticore/types/typesymbols/_symboltable/TypeSymbolsScope.java
+++ monticore-grammar/src/main/java/de/monticore/types/typesymbols/_symboltable/TypeSymbolsScope.java
@@ -1,3 +1,4 @@
+/* (c) https://github.com/MontiCore/monticore */
 package de.monticore.types.typesymbols._symboltable;
 
 import de.monticore.expressions.expressionsbasis._symboltable.IExpressionsBasisScope;
