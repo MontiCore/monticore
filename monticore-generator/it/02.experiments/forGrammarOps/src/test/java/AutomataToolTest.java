7de049b04a3b3d031184ae970af45c6c2e303ce8
Bernhard Rumpe <rumpe@se-rwth.de>
2020-08-17 16:36:24 +0200

    BR

---
+++ monticore-generator/it/02.experiments/forGrammarOps/src/test/java/AutomataToolTest.java
@@ -0,0 +1,40 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+import automata.AutomataTool;
+import org.junit.*;
+import de.se_rwth.commons.logging.Log;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import de.se_rwth.commons.logging.LogStub;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+
+public class AutomataToolTest {
+  
+  @BeforeClass
+  public static void init() {
+    LogStub.init();         // replace log by a sideffect free variant
+    // LogStub.initPlusLog();  // for manual testing purpose only
+    Log.enableFailQuick(false);
+  }
+  
+  @Before
+  public void setUp() {
+    Log.clearFindings();
+    LogStub.clearPrints();
+  }
+  
+  @Test
+  public void executeSimple12() {
+    AutomataTool.main(new String[] { "src/test/resources/example/Simple12.aut" });
+    Log.printFindings();
+    assertEquals(0, Log.getFindings().size());
+    // LogStub.printPrints();
+    List<String> p = LogStub.getPrints();
+    assertEquals(6, p.size());
+  }
+  
+}
