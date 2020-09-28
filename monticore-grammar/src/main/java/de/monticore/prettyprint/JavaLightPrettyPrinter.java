f14886ca12569f67f6d1fbdf66caa91d318af581
breuer <breuer@se-rwth.de>
2020-09-28 09:08:25 +0200

    add MCShadowingJavaBlock

--- monticore-grammar/src/main/java/de/monticore/prettyprint/JavaLightPrettyPrinter.java
+++ monticore-grammar/src/main/java/de/monticore/prettyprint/JavaLightPrettyPrinter.java
@@ -2,11 +2,9 @@
 
 package de.monticore.prettyprint;
 
-import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
 import de.monticore.javalight._ast.*;
 import de.monticore.javalight._visitor.JavaLightVisitor;
 import de.monticore.statements.prettyprint.MCCommonStatementsPrettyPrinter;
-import de.monticore.types.mcbasictypes._ast.ASTMCObjectType;
 
 import java.util.Iterator;
 
@@ -33,9 +31,7 @@ public class JavaLightPrettyPrinter extends MCCommonStatementsPrettyPrinter impl
     if (a.isStatic()) {
       getPrinter().print("static ");
     }
-    getPrinter().println("{");
-    a.forEachMCBlockStatements(b -> b.accept(getRealThis()));
-    getPrinter().println("}");
+    a.getMCShadowingJavaBlock().accept(getRealThis());
     CommentPrettyPrinter.printPostComments(a, getPrinter());
   }
 
