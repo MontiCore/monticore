4e2abbb80b976dd5a10f6be68f4c2166261bd1ea
n_jansen <nico.jansen1@rwth-aachen.de>
2020-10-22 18:32:25 +0200

    Add an experiment that tests the new traversers.

---
+++ monticore-generator/it/02.experiments/traverser/src/main/java/tree/visitor/LeafCounter.java
@@ -0,0 +1,26 @@
+package tree.visitor;
+
+import trees._visitor.TreesVisitor2;
+
+public class LeafCounter implements TreesVisitor2 {
+  
+  int count = 0;
+  
+  public LeafCounter() {
+    count = 0;
+  }
+  
+  @Override
+  public void visit(trees._ast.ASTLeaf node) {
+    count++;
+  }
+  
+  public void resetCount() {
+    count = 0;
+  }
+  
+  public int getCount() {
+    return count;
+  }
+  
+}
