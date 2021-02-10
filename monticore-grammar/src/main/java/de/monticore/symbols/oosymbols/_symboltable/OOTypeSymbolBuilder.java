f16953dc51bb18e31ec63e4c74f6f04bdc00f23d
Bernhard Rumpe <rumpe@se-rwth.de>
2021-02-10 15:12:31 +0100

    BR

--- monticore-grammar/src/main/java/de/monticore/symbols/oosymbols/_symboltable/OOTypeSymbolBuilder.java
+++ monticore-grammar/src/main/java/de/monticore/symbols/oosymbols/_symboltable/OOTypeSymbolBuilder.java
@@ -1,6 +1,4 @@
-// (c) https://github.com/MontiCore/monticore
-
-// (c) https://github.com/MontiCore/monticore
+/* (c) https://github.com/MontiCore/monticore */
 
 package de.monticore.symbols.oosymbols._symboltable;
 
