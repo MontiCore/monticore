bf1a3fea0566981294697bba9631e017bddb80fb
Butting <butting@se-rwth.de>
2021-02-22 11:01:28 +0100

    omiot serialization of default values in JsonPrinter

--- monticore-runtime/src/main/java/de/monticore/symboltable/IScope.java
+++ monticore-runtime/src/main/java/de/monticore/symboltable/IScope.java
@@ -21,69 +21,25 @@ import static java.util.stream.Collectors.toSet;
 public interface IScope {
 
   IScope getEnclosingScope();
-
-  /**
-   * @return number of symbols directly defined/contained in this scope (not in enclosing scope).
-   */
   int getSymbolsSize();
-
-  /**
-   * @return true, if this scope shadows symbols of the enclosing scope. By default, named scopes
-   * (see #getName()) are shadowing scopes.
-   */
   boolean isShadowing();
-
-  /**
-   * @return true, if this scope is spanned by a symbol. For example, a Java method spans a
-   * method scope.
-   */
   boolean isPresentSpanningSymbol();
-
-  /**
-   * States whether this scope exports symbols that can be used from outside the scope.
-   * For example, a Java class exports symbols. In contrast, a Java if-block does notisym
-   * export any symbols, hence, its locally defined variables cannot be referenced
-   * from outside. By default, a scope with a name exports its symbols (although
-   * this does not apply for Java methods).
-   *
-   * @return true, if this scope exports symbols that can be used from outside the scope.
-   */
   boolean isExportingSymbols();
-
   boolean isOrdered();
-
   void setExportingSymbols(boolean b);
-
   void setShadowing(boolean b);
-
   void setOrdered(boolean b);
-
-  /**
-   * @param node the corresponding ast node
-   */
   void setAstNode(ASTNode node);
-
   void setAstNodeAbsent();
-
   ASTNode getAstNode();
-
   boolean isPresentAstNode();
-
   void setSpanningSymbol(IScopeSpanningSymbol symbol);
-
   void setSpanningSymbolAbsent();
-
-
   IScopeSpanningSymbol getSpanningSymbol();
-
   void setName(String name);
-
   void setNameAbsent();
-
   String getName();
-
   boolean isPresentName();
-
   default List<String> getRemainingNameForResolveDown(String symbolName) {
     final FluentIterable<String> nameParts = getNameParts(symbolName);
     return Lists.newArrayList((nameParts.size() > 1) ? DOT.join(nameParts.skip(1)) : symbolName);
---
+++ monticore-test/01.experiments/forVisitors/src/main/java/l/_ast/ASTC.java
@@ -0,0 +1,9 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+package l._ast;
+
+public class ASTC extends ASTCTOP {
+  @Override public String getName() {
+    return "C";
+  }
+}
