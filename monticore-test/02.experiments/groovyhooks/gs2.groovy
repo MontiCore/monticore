9db07a565824b43da714aad513c30143e443f4f3
Nico Jansen <nico.jansen1@rwth-aachen.de>
2021-04-07 21:11:46 +0200

    Add experiment for groovy hooks.

---
+++ monticore-test/02.experiments/groovyhooks/gs2.groovy
@@ -0,0 +1,4 @@
+/* (c) https://github.com/MontiCore/monticore */
+Log.info("Execute script for groovy hook point 2.", "GS2")
+cdName = args[2].get(0).getCDDefinition().getName()
+Log.info("Class diagram name: " + cdName, "GS2")
