c2a2210e181d0b549e287b7a528723084a26e2b8
Nico Jansen <nico.jansen1@rwth-aachen.de>
2021-04-10 14:30:35 +0200

    Remove inc check from python script.

--- monticore-test/it/it-python.groovy
+++ monticore-test/it/it-python.groovy
@@ -12,16 +12,12 @@ globalScope = createMCGlobalScope(modelPath)
 // Parse grammar
 astGrammars = parseGrammars(grammars)
 
-IncrementalChecker.initialize(out, report)
-
 Reporting.off()
 
 for (astGrammar in astGrammars) {
     input = grammarIterator.next()
-    if (force || !IncrementalChecker.isUpToDate(input, modelPath, templatePath, handcodedPath)) {
-        astGrammar = createSymbolsFromAST(globalScope, astGrammar)
+    astGrammar = createSymbolsFromAST(globalScope, astGrammar)
 
-        // Generate parser
-        generateParser(glex, astGrammar, globalScope, handcodedPath, templatePath, out, false, Languages.PYTHON_3)
-    }
+    // Generate parser
+    generateParser(glex, astGrammar, globalScope, handcodedPath, templatePath, out, false, Languages.PYTHON_3)
 }
