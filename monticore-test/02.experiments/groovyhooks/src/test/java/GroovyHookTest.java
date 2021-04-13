4ecb71f94bd67210fde0a72a3e0653f725922aa8
Nico Jansen <nico.jansen1@rwth-aachen.de>
2021-04-13 16:56:18 +0200

    Add a more sophisticated groovy hook test that manipulates the AST after decoration.

---
+++ monticore-test/02.experiments/groovyhooks/src/test/java/GroovyHookTest.java
@@ -0,0 +1,30 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+import automata.AutomataTool;
+import automata._ast.ASTAutomaton;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Main class for the some Demonstration to Parse
+ */
+public class GroovyHookTest {
+
+  private AutomataTool tool = new AutomataTool();
+
+  /**
+   * @throws IOException
+   */
+  @Test
+  public void testGH1() throws IOException {
+    ASTAutomaton ast = tool.parse("src/test/resources/example/PingPong.aut");
+    assertNotNull(ast);
+    assertEquals("PingPong", ast.getName());
+    assertTrue(ast.newMethod());
+  }
+
+
+}
