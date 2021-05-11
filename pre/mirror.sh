06466ba06dcfcd0d5abb788e4628705a24706369
Oliver Kautz <oliver.kautz@rwth-aachen.de>
2021-05-11 12:25:08 +0000

    Update mirror.sh

--- pre/mirror.sh
+++ pre/mirror.sh
@@ -5,9 +5,9 @@ do
 done
 echo "[INFO] Removed all occureneces of '[[_TOC_]]' in *.md files"
 # Replace descriptions of not publicly reachable RWTH GitLab links
-#for file in $(find . -type f -name "*.md")
-#do
-#  content=$(java -jar MDLinkChangerCLI.jar -f $file)
-#  echo $content > $file
-#done
-#echo "[INFO] Replaced descriptions of not publicly available GitLab links"
+for file in $(find . -type f -name "*.md")
+do
+  content=$(java -jar MDLinkChangerCLI.jar -f $file)
+  echo "$content" > $file
+done
+echo "[INFO] Replaced descriptions of not publicly available GitLab links"
