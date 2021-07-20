56e3b902c3fcdc4ef5ac3f9199a1f79906e3854b
niklasdienstknecht <dienstknecht@se-rwth.de>
2021-07-20 12:23:59 +0200

    fix test

--- monticore-generator/src/test/java/de/monticore/codegen/cd2java/cli/CLIDecoratorTest.java
+++ monticore-generator/src/test/java/de/monticore/codegen/cd2java/cli/CLIDecoratorTest.java
@@ -49,7 +49,7 @@ public class CLIDecoratorTest extends DecoratorTestCase {
 
   @Test
   public void testMethodCount() {
-    assertEquals(14, cliClass.getCDMethodList().size());
+    assertEquals(16, cliClass.getCDMethodList().size());
   }
 
   @Test
