dd65f5c9cfaa3180513b2324408f47df0e1c7b15
breuer <breuer@se-rwth.de>
2021-09-09 10:44:01 +0200

    calculate without resolving

--- monticore-grammar/src/main/java/de/monticore/grammar/grammar_withconcepts/SynthesizeFromMCBT4Grammar.java
+++ monticore-grammar/src/main/java/de/monticore/grammar/grammar_withconcepts/SynthesizeFromMCBT4Grammar.java
@@ -3,8 +3,10 @@ package de.monticore.grammar.grammar_withconcepts;
 
 import de.monticore.symbols.basicsymbols._symboltable.TypeSymbol;
 import de.monticore.symbols.basicsymbols._symboltable.TypeSymbolSurrogate;
+import de.monticore.symbols.basicsymbols._symboltable.TypeVarSymbol;
 import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types.check.SymTypeExpressionFactory;
+import de.monticore.types.check.SymTypeOfObject;
 import de.monticore.types.check.SynthesizeSymTypeFromMCBasicTypes;
 import de.monticore.types.mcbasictypes._ast.ASTMCQualifiedName;
 
@@ -13,9 +15,12 @@ import java.util.Optional;
 public class SynthesizeFromMCBT4Grammar extends SynthesizeSymTypeFromMCBasicTypes {
 
   @Override
-  protected Optional<SymTypeExpression> handleIfNotFound(ASTMCQualifiedName qName) {
+  public void endVisit(ASTMCQualifiedName qName) {
     TypeSymbol surrogate = new TypeSymbolSurrogate(qName.getQName());
     surrogate.setEnclosingScope(getScope(qName.getEnclosingScope()));
-    return Optional.of(SymTypeExpressionFactory.createTypeObject(surrogate));
+    SymTypeExpression symType = SymTypeExpressionFactory.createTypeObject(surrogate);
+
+    typeCheckResult.setCurrentResult(symType);
   }
+
 }
