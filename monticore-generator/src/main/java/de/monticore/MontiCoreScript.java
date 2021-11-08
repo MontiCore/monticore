76ca2d488f489139bb85adc7b13568d8085c3300
breuer <breuer@se-rwth.de>
2021-11-08 16:36:07 +0100

    Use CD4C

--- monticore-generator/src/main/java/de/monticore/MontiCoreScript.java
+++ monticore-generator/src/main/java/de/monticore/MontiCoreScript.java
@@ -5,6 +5,7 @@ package de.monticore;
 import com.google.common.collect.Lists;
 import com.google.common.io.Files;
 import com.google.common.io.Resources;
+import de.monticore.cd.methodtemplates.CD4C;
 import de.monticore.cd4analysis._symboltable.ICD4AnalysisGlobalScope;
 import de.monticore.cd4analysis._symboltable.ICD4AnalysisScope;
 import de.monticore.cd4codebasis._ast.ASTCDConstructor;
@@ -598,6 +599,8 @@ public class MontiCoreScript extends Script implements GroovyRunner {
       GeneratorSetup setup = new GeneratorSetup();
       setup.setAdditionalTemplatePaths(templatePath.getEntries().stream().map(p -> new File(p.toUri())).collect(Collectors.toList()));
       setup.setGlex(glex);
+      CD4C.init(setup);
+      CD4C.getInstance().setEmptyBodyTemplate("core.EmptyBody");
 
       TemplateController tc = setup.getNewTemplateController(configTemplate);
       TemplateHookPoint hp = new TemplateHookPoint(configTemplate);
@@ -1064,6 +1067,7 @@ public class MontiCoreScript extends Script implements GroovyRunner {
     setup.setAdditionalTemplatePaths(templatePath.getEntries().stream().map(p -> new File(p.toUri())).collect(Collectors.toList()));
     setup.setModelName(diagramName);
     setup.setGlex(glex);
+
     CDGenerator generator = new CDGenerator(setup);
     generator.generate(decoratedCD);
   }
