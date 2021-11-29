53f56107b24c55d6a6a966243fb03a1fc746b31c
breuer <breuer@se-rwth.de>
2021-11-29 12:43:12 +0100

    add ReturnStatements to JavaLight

--- monticore-grammar/src/main/java/de/monticore/statements/mccommonstatements/cocos/ReturnTypeAssignmentIsValid.java
+++ monticore-grammar/src/main/java/de/monticore/statements/mccommonstatements/cocos/ReturnTypeAssignmentIsValid.java
@@ -1,42 +1,36 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.statements.mccommonstatements.cocos;
 
-import de.monticore.expressions.expressionsbasis._visitor.ExpressionsBasisTraverser;
 import de.monticore.javalight.JavaLightMill;
 import de.monticore.javalight._ast.ASTMethodDeclaration;
 import de.monticore.javalight._cocos.JavaLightASTMethodDeclarationCoCo;
 import de.monticore.javalight._visitor.JavaLightTraverser;
-import de.monticore.javalight._visitor.JavaLightVisitor2;
-import de.monticore.statements.mcreturnstatements.MCReturnStatementsMill;
 import de.monticore.statements.mcreturnstatements._ast.ASTReturnStatement;
-import de.monticore.statements.mcreturnstatements._visitor.MCReturnStatementsTraverser;
 import de.monticore.statements.mcreturnstatements._visitor.MCReturnStatementsVisitor2;
-import de.monticore.statements.mcstatementsbasis._visitor.MCStatementsBasisTraverser;
 import de.monticore.types.check.TypeCheck;
 
 import java.util.List;
 
 public class ReturnTypeAssignmentIsValid implements JavaLightASTMethodDeclarationCoCo {
-  
-  JavaLightTraverser traverser;
-  
+
   TypeCheck typeCheck;
   
-  public ReturnTypeAssignmentIsValid(TypeCheck typeCheck, JavaLightTraverser traverser){
+  public ReturnTypeAssignmentIsValid(TypeCheck typeCheck){
     this.typeCheck = typeCheck;
-    this.traverser = traverser;
   }
   
   
   @Override
   public void check(ASTMethodDeclaration node) {
-    
-    MCReturnStatementsTraverser traverser = MCReturnStatementsMill.traverser();
+    // Collect return-statments
+    JavaLightTraverser traverser = JavaLightMill.traverser();
     JavaReturnStatementCollector returnStatementCollector = new JavaReturnStatementCollector(){};
     traverser.add4MCReturnStatements(returnStatementCollector);
-    
     node.accept(traverser);
-    
+    List<ASTReturnStatement> retStatements = returnStatementCollector.getReturnStatementList();
+
+    // Check return-Statements
+    // todo
   }
   
   private class JavaReturnStatementCollector implements MCReturnStatementsVisitor2 {
