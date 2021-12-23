441b8e3fb4469d84e56aa51bcd2911cabb24e65e
Nico Jansen <nico.jansen1@rwth-aachen.de>
2021-12-23 11:43:30 +0100

    Update tool class of experiment.

--- monticore-test/01.experiments/forAst/src/main/java/grammarforast15/GrammarForAST15Tool.java
+++ monticore-test/01.experiments/forAst/src/main/java/grammarforast15/GrammarForAST15Tool.java
@@ -1,65 +1,68 @@
 /* (c) https://github.com/MontiCore/monticore */
 package grammarforast15;
 
-import java.io.IOException;
-import java.util.Optional;
-
-import org.antlr.v4.runtime.RecognitionException;
-
-import grammarforast15._ast.ASTS;
-import grammarforast15._parser.GrammarForAST15Parser;
 import de.se_rwth.commons.logging.Log;
+import grammarforast15._ast.ASTS;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  * Main class for the grammarforast15 DSL tool.
- *
  */
-public class GrammarForAST15Tool {
-  
+public class GrammarForAST15Tool extends GrammarForAST15ToolTOP {
+
   /**
    * Use the single argument for specifying the single input grammarforast15 file.
-   * 
+   *
    * @param args
    */
   public static void main(String[] args) {
 
     // use normal logging (no DEBUG, TRACE)
     Log.ensureInitalization();
-    
-    if (args.length != 1) {
-      Log.error("0xEE743 Please specify only one single path to the input model.");
-      return;
-    }
+
     Log.info("GrammarForAST15 DSL Tool", "GrammarForAST15Tool");
     Log.info("------------------", "GrammarForAST15Tool");
     String model = args[0];
-    
-    // parse the model and create the AST representation
-    final ASTS ast = parse(model);
-    Log.info(model + " parsed successfully!", "GrammarForAST15Tool");
-    
+
+    GrammarForAST15Tool tool = new GrammarForAST15Tool();
+    tool.run(args);
+
   }
-  
-  /**
-   * Parse the model contained in the specified file.
-   * 
-   * @param model - file to parse
-   * @return
-   */
-  public static ASTS parse(String model) {
+
+  @Override
+  public void run(String[] args) {
+    init();
+    Options options = initOptions();
     try {
-      GrammarForAST15Parser parser = new GrammarForAST15Parser() ;
-      Optional<ASTS> optGrammarForAST15 = parser.parse(model);
-      
-      if (!parser.hasErrors() && optGrammarForAST15.isPresent()) {
-        return optGrammarForAST15.get();
+      //create CLI Parser and parse input options from commandline
+      CommandLineParser cliparser = new org.apache.commons.cli.DefaultParser();
+      CommandLine cmd = cliparser.parse(options, args);
+
+      //help: when --help
+      if (cmd.hasOption("h")) {
+        printHelp(options);
+        //do not continue, when help is printed.
+        return;
       }
-      Log.error("0xEE843 Model could not be parsed.");
+      //version: when --version
+      else if (cmd.hasOption("v")) {
+        printVersion();
+        //do not continue when help is printed
+        return;
+      }
+
+      if (cmd.hasOption("i")) {
+        String model = cmd.getOptionValue("i");
+        final ASTS ast = parse(model);
+        Log.info(model + " parsed successfully!", "GrammarForAST15Tool");
+      }
+
+    } catch (ParseException e) {
+      // e.getMessage displays the incorrect input-parameters
+      Log.error("0xEE743 Could not process GrammarForAST15Tool parameters: " + e.getMessage());
     }
-    catch (RecognitionException | IOException e) {
-      Log.error("0xEE643 Failed to parse " + model, e);
-    }
-    return null;
   }
-  
 }
