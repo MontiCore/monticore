5a16587b8d34284fe7a5c25bd3cd7efa4904cd74
Sebastian Stüber <spam@spam.com>
2022-04-27 16:28:32 +0200

    #3150 removed counter which is causing differences when starting the generator multiple times

--- monticore-generator/src/main/java/de/monticore/dstlgen/util/DSTLService.java
+++ monticore-generator/src/main/java/de/monticore/dstlgen/util/DSTLService.java
@@ -10,13 +10,9 @@ public class DSTLService {
     this.grammar = grammar;
   }
 
-  protected int count = 0;
   public String getGeneratedErrorCode(String name) {
     // Use the string representation
-    // also use a count to make sure no double codes can appear
-    // because sometimes there is not enough information for a unique string
-    String codeString = grammar.getPackageList() + grammar.getName() + name + count;
-    count++;
+    String codeString = grammar.getPackageList() + grammar.getName() + name;
     //calculate hashCode, but limit the values to have at most 5 digits
     int hashCode = Math.abs(codeString.hashCode() % 100000);
     //use String formatting to add leading zeros to always have 5 digits
