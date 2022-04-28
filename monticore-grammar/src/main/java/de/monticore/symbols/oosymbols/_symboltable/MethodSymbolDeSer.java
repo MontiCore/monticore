1ee1afacba98d26c0673ae859b1beeb6728808e7
Jonas Böcker <jonas.boecker@gmx.net>
2022-04-28 11:07:33 +0200

    Update MethodSymbolDeSer.java

--- monticore-grammar/src/main/java/de/monticore/symbols/oosymbols/_symboltable/MethodSymbolDeSer.java
+++ monticore-grammar/src/main/java/de/monticore/symbols/oosymbols/_symboltable/MethodSymbolDeSer.java
@@ -8,13 +8,13 @@ import de.monticore.types.check.SymTypeExpressionDeSer;
 public class MethodSymbolDeSer extends MethodSymbolDeSerTOP {
 
   @Override
-  protected void serializeType(SymTypeExpression returnType, OOSymbolsSymbols2Json s2j) {
-    SymTypeExpressionDeSer.serializeMember(s2j.getJsonPrinter(), "returnType", returnType);
+  protected void serializeType(SymTypeExpression type, OOSymbolsSymbols2Json s2j) {
+    SymTypeExpressionDeSer.serializeMember(s2j.getJsonPrinter(), "type", type);
   }
 
   @Override
   public SymTypeExpression deserializeType(JsonObject symbolJson) {
-    return SymTypeExpressionDeSer.deserializeMember("returnType", symbolJson);
+    return SymTypeExpressionDeSer.deserializeMember("type", symbolJson);
   }
 
 }
