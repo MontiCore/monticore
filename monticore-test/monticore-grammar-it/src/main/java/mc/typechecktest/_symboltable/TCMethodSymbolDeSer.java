2435724bfb99648b0ea3c152c582a589ce747d33
Jonas Böcker <jonas.boecker@gmx.net>
2022-04-28 11:13:04 +0200

    Update TCMethodSymbolDeSer.java

--- monticore-test/monticore-grammar-it/src/main/java/mc/typechecktest/_symboltable/TCMethodSymbolDeSer.java
+++ monticore-test/monticore-grammar-it/src/main/java/mc/typechecktest/_symboltable/TCMethodSymbolDeSer.java
@@ -8,11 +8,11 @@ import de.monticore.types.check.SymTypeExpressionDeSer;
 public class TCMethodSymbolDeSer extends TCMethodSymbolDeSerTOP {
   @Override
   protected void serializeType(SymTypeExpression type, TypeCheckTestSymbols2Json s2j) {
-    SymTypeExpressionDeSer.serializeMember(s2j.getJsonPrinter(), "returnType", type);
+    SymTypeExpressionDeSer.serializeMember(s2j.getJsonPrinter(), "type", type);
   }
 
   @Override
   protected SymTypeExpression deserializeType(JsonObject symbolJson) {
-    return SymTypeExpressionDeSer.deserializeMember("returnType", symbolJson);
+    return SymTypeExpressionDeSer.deserializeMember("type", symbolJson);
   }
 }
