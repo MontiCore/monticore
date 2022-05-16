fdeeecea3fe95dc652ec6caf909930ff81807eb7
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2022-05-16 17:27:00 +0200

    #3149 Fix failing test + add TODO

--- monticore-runtime/src/main/java/de/monticore/generating/templateengine/freemarker/alias/DefineHookPointAlias.java
+++ monticore-runtime/src/main/java/de/monticore/generating/templateengine/freemarker/alias/DefineHookPointAlias.java
@@ -16,8 +16,11 @@ public class DefineHookPointAlias extends Alias {
   public Object exec(List arguments) throws TemplateModelException {
     if(arguments.size() == 2){
       return getGlex().defineHookPoint(getTc(), arguments.get(0).toString(), (ASTNode) ((WrapperTemplateModel)arguments.get(1)).getWrappedObject());
-    }else{
+    }else if(arguments.size() == 1){
       return getGlex().defineHookPoint(getTc(), arguments.get(0).toString());
+    } else {
+      throw new TemplateModelException("Expecting 1 or 2 arguments but got " + arguments.size());
     }
+    // TODO AHe: Check that all aliases have the correct number of arguments
   }
 }
