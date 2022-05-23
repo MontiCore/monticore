9dbafcd0bfeb937257f4cadb8897c0ddaeeeac67
Alexander Hellwig <alexander.hellwig@rwth-aachen.de>
2022-05-23 11:26:30 +0200

    #3149 Fix: Most Aliases need argument size check

--- monticore-runtime/src/main/java/de/monticore/generating/templateengine/freemarker/alias/AddToGlobalVarAlias.java
+++ monticore-runtime/src/main/java/de/monticore/generating/templateengine/freemarker/alias/AddToGlobalVarAlias.java
@@ -1,7 +1,7 @@
 package de.monticore.generating.templateengine.freemarker.alias;
 
-public class AddToGlobalVarAlias extends GlexAlias {
+public class AddToGlobalVarAlias extends SimpleGlexAlias {
   public AddToGlobalVarAlias() {
-    super("addToGlobalVar", "addToGlobalVar");
+    super("addToGlobalVar", "addToGlobalVar", 2);
   }
 }
--- monticore-runtime/src/main/java/de/monticore/generating/templateengine/freemarker/alias/BindHookPointAlias.java
+++ monticore-runtime/src/main/java/de/monticore/generating/templateengine/freemarker/alias/BindHookPointAlias.java
@@ -2,6 +2,6 @@ package de.monticore.generating.templateengine.freemarker.alias;
 
 public class BindHookPointAlias extends SimpleGlexAlias {
   public BindHookPointAlias() {
-    super("bindHookPoint", "bindHookPoint", 1);
+    super("bindHookPoint", "bindHookPoint", 2);
   }
 }
