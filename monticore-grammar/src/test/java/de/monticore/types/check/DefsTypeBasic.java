0ecaaf52646cc410c9c1455b25dc33662cd7f5c7
Flo Drux <drux@se-rwth.de>
2022-08-05 12:27:31 +0200

    DefsTypeBasic.java fix comment layout

--- monticore-grammar/src/test/java/de/monticore/types/check/DefsTypeBasic.java
+++ monticore-grammar/src/test/java/de/monticore/types/check/DefsTypeBasic.java
@@ -55,7 +55,8 @@ public class DefsTypeBasic {
     return type(name,name);
   }
   
-  /** create TypeSymbols (some defaults apply)
+  /** 
+   * create TypeSymbols (some defaults apply)
    */
   public static OOTypeSymbol type(String name, String fullName) {
     return OOSymbolsMill.oOTypeSymbolBuilder()
@@ -144,7 +145,8 @@ public class DefsTypeBasic {
     return t;
   }
 
-  /** create FunctionSymbols (some defaults apply)
+  /** 
+   * create FunctionSymbols (some defaults apply)
    */
   public static FunctionSymbol function(String name, SymTypeExpression returnType) {
     FunctionSymbol f = BasicSymbolsMill.functionSymbolBuilder()
@@ -158,7 +160,8 @@ public class DefsTypeBasic {
     return f;
   }
   
-  /** create MethodSymbols (some defaults apply)
+  /**
+   * create MethodSymbols (some defaults apply)
    */
   public static MethodSymbol method(String name, SymTypeExpression returnType) {
     MethodSymbol m = OOSymbolsMill.methodSymbolBuilder()
@@ -177,7 +180,8 @@ public class DefsTypeBasic {
     return m;
   }
 
-  /** create FieldSymbols (some defaults apply)
+  /** 
+   * create FieldSymbols (some defaults apply)
    */
   public static FieldSymbol field(String name, SymTypeExpression type) {
     return OOSymbolsMill.fieldSymbolBuilder()
@@ -188,7 +192,8 @@ public class DefsTypeBasic {
             .build();
   }
   
-  /** add a Type to a Scope (bidirectional)
+  /** 
+   * add a Type to a Scope (bidirectional)
    */
   public static void add2scope(IOOSymbolsScope p, OOTypeSymbol s) {
     s.setEnclosingScope(p);
@@ -196,14 +201,16 @@ public class DefsTypeBasic {
     p.add((TypeSymbol) s);
   }
   
-  /** add a Filed (e.g. a Variable) to a Scope (bidirectional)
+  /**
+   * add a Filed (e.g. a Variable) to a Scope (bidirectional)
    */
   public static void add2scope(IOOSymbolsScope p, FieldSymbol s) {
     s.setEnclosingScope(p);
     p.add(s);
   }
 
-  /** add a Method to a Scope (bidirectional)
+  /**
+   * add a Method to a Scope (bidirectional)
    */
   public static void add2scope(IOOSymbolsScope p, MethodSymbol s){
     s.setEnclosingScope(p);
@@ -211,7 +218,8 @@ public class DefsTypeBasic {
     p.add((FunctionSymbol) s);
   }
 
-  /** add a Function to a Scope (bidirectional)
+  /** 
+   * add a Function to a Scope (bidirectional)
    */
   public static void add2scope(IOOSymbolsScope p, FunctionSymbol s) {
     s.setEnclosingScope(p);
