70d1048d79bee24fb3f5142c6ac669243a93c72e
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2022-12-15 16:29:30 +0100

    Deprecate TypeCheck even more

--- monticore-grammar/src/main/java/de/monticore/types/check/TypeCheck.java
+++ monticore-grammar/src/main/java/de/monticore/types/check/TypeCheck.java
@@ -14,59 +14,73 @@ public class TypeCheck {
    */
   protected static TypeRelations typeRelations = new TypeRelations();
 
+  @Deprecated
   public static boolean compatible(SymTypeExpression left,
                                    SymTypeExpression right) {
     return typeRelations.compatible(left, right);
   }
 
+  @Deprecated
   public static boolean isSubtypeOf(SymTypeExpression subType, SymTypeExpression superType){
     return typeRelations.isSubtypeOf(subType, superType);
   }
 
+  @Deprecated
   protected static int calculateInheritanceDistance(SymTypeExpression specific, SymTypeExpression general){
     return typeRelations.calculateInheritanceDistance(specific, general);
   }
 
+  @Deprecated
   public static int calculateInheritanceDistance(SymTypePrimitive specific, SymTypePrimitive general) {
     return typeRelations.calculateInheritanceDistance(specific, general);
   }
 
+  @Deprecated
   public static boolean isBoolean(SymTypeExpression type) {
     return typeRelations.isBoolean(type);
   }
 
+  @Deprecated
   public static boolean isInt(SymTypeExpression type) {
     return typeRelations.isInt(type);
   }
 
+  @Deprecated
   public static boolean isDouble(SymTypeExpression type) {
     return typeRelations.isDouble(type);
   }
 
+  @Deprecated
   public static boolean isFloat(SymTypeExpression type) {
     return typeRelations.isFloat(type);
   }
 
+  @Deprecated
   public static boolean isLong(SymTypeExpression type) {
     return typeRelations.isLong(type);
   }
 
+  @Deprecated
   public static boolean isChar(SymTypeExpression type) {
     return typeRelations.isChar(type);
   }
 
+  @Deprecated
   public static boolean isShort(SymTypeExpression type) {
     return typeRelations.isShort(type);
   }
 
+  @Deprecated
   public static boolean isByte(SymTypeExpression type) {
     return typeRelations.isByte(type);
   }
 
+  @Deprecated
   public static boolean isVoid(SymTypeExpression type) {
     return typeRelations.isVoid(type);
   }
 
+  @Deprecated
   public static boolean isString(SymTypeExpression type) {
     return typeRelations.isString(type);
   }
