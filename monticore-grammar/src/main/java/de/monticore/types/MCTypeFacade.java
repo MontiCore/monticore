b01e870e42f24024d503df83ec7727168252ce63
Marita Breuer <breuer@se-rwth.de>
2023-04-03 17:21:15 +0200

    undo

--- monticore-grammar/src/main/java/de/monticore/types/MCTypeFacade.java
+++ monticore-grammar/src/main/java/de/monticore/types/MCTypeFacade.java
@@ -258,9 +258,7 @@ public class MCTypeFacade {
   }
 
   public boolean isBooleanType(ASTMCType type) {
-    return (type instanceof ASTMCPrimitiveType && ((ASTMCPrimitiveType) type).isBoolean()) ||
-            "Boolean".equals(MCBasicTypesMill.mcBasicTypesPrettyPrinter().prettyprint(type)) ||
-            "java.lang.Boolean".equals((MCBasicTypesMill.mcBasicTypesPrettyPrinter().prettyprint(type)));
+    return type instanceof ASTMCPrimitiveType && ((ASTMCPrimitiveType) type).isBoolean();
   }
 
   public ASTMCPrimitiveType createIntType() {
