a002131bc6be00bed3bbe4a98477d2c2795f7b7d
Jonas Böcker <jonas.boecker@gmx.net>
2023-05-23 18:18:25 +0200

    Interface no implementation coco

--- monticore-grammar/src/main/java/de/monticore/grammar/cocos/InterfaceNTWithoutImplementationOnlyInComponentGrammar.java
+++ monticore-grammar/src/main/java/de/monticore/grammar/cocos/InterfaceNTWithoutImplementationOnlyInComponentGrammar.java
@@ -8,93 +8,116 @@ import de.monticore.grammar.grammar._cocos.GrammarASTMCGrammarCoCo;
 import de.monticore.grammar.grammar._symboltable.MCGrammarSymbol;
 import de.monticore.grammar.grammar._symboltable.ProdSymbol;
 import de.monticore.grammar.grammar._symboltable.ProdSymbolSurrogate;
+import de.monticore.grammar.grammar._symboltable.RuleComponentSymbol;
 import de.se_rwth.commons.logging.Log;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Checks that abstract nonterminals witheout extending productions only occur in a component grammar.
- *
-
  */
 public class InterfaceNTWithoutImplementationOnlyInComponentGrammar implements GrammarASTMCGrammarCoCo {
 
   public static final String ERROR_CODE = "0xA0278";
 
   public static final String ERROR_MSG_FORMAT = " The interface nonterminal %s must not be used without nonterminals " +
-          "implementing it in a grammar not marked as a grammar component.";
+      "implementing it in a grammar not marked as a grammar component.";
 
   @Override
   public void check(ASTMCGrammar a) {
     MCGrammarSymbol grammarSymbol = a.getSymbol();
 
+    // if the given grammar is a component grammar we ignore it, since there we allow interfaces without an
+    // implementation
     if (!a.isComponent()) {
+
+      // we collect all interface productions from the grammar and all its super grammars and save them to the list
+      // interfaceProds
       List<ProdSymbol> interfaceProds = grammarSymbol.getProds().stream().
           filter(ProdSymbol::isIsInterface).collect(Collectors.toList());
-      for(MCGrammarSymbol symbol: grammarSymbol.getAllSuperGrammars()){
+      for (MCGrammarSymbol symbol : grammarSymbol.getAllSuperGrammars()) {
         Collection<ProdSymbol> prodSymbols = symbol.getProds();
-        for(ProdSymbol mcProdSymbol : prodSymbols){
+        for (ProdSymbol mcProdSymbol : prodSymbols) {
           if (mcProdSymbol.isIsInterface()) {
             interfaceProds.add(mcProdSymbol);
           }
         }
       }
 
+      // for every interface production we get all other interfaces implementing it and save it to map of all interface
+      // productions mapping to all their subinterfaces
+      Map<String, List<ProdSymbol>> subSymbols = new HashMap<>();
+      for (ProdSymbol interfaceProd : interfaceProds) {
+        for (ProdSymbol superInterface : interfaceProd.getSuperInterfaceProds()) {
+          if (!subSymbols.containsKey(superInterface.getName())) {
+            subSymbols.put(superInterface.getName(), new ArrayList<>());
+          }
+          subSymbols.get(superInterface.getName()).add(interfaceProd);
+        }
+      }
+
+      // we collect all non-interface productions from the grammar and all its super-grammars and save them to the list
+      // prods
       List<ProdSymbol> prods = grammarSymbol.getProds().stream().
           filter(prodSymbol -> prodSymbol.isClass() || prodSymbol.isIsAbstract()).collect(Collectors.toList());
-      for(MCGrammarSymbol symbol: grammarSymbol.getAllSuperGrammars()){
+      for (MCGrammarSymbol symbol : grammarSymbol.getAllSuperGrammars()) {
         Collection<ProdSymbol> prodSymbols = symbol.getProds();
-        for(ProdSymbol mcProdSymbol : prodSymbols){
+        for (ProdSymbol mcProdSymbol : prodSymbols) {
           if (mcProdSymbol.isIsAbstract() || mcProdSymbol.isClass()) {
             prods.add(mcProdSymbol);
           }
         }
       }
 
-      if(!interfaceProds.isEmpty()) {
-        List<ProdSymbol> temp = new ArrayList<>(interfaceProds);
-        for(ProdSymbol interfaceProdSymbol : interfaceProds){
-          for(ProdSymbolSurrogate interfaceProdExtended : interfaceProdSymbol.getSuperInterfaceProds()){
-            for(int i = interfaceProds.size()-1;i>=0;--i){
-              ProdSymbol interfaceProd = interfaceProds.get(i);
-              if(interfaceProdExtended.getName().equals(interfaceProd.getName())){
-                temp.remove(interfaceProdExtended.lazyLoadDelegate());
+      // from the list interfaceProds we remove every interface production that does not get used in a non-interface
+      // production
+      if (!interfaceProds.isEmpty()) {
+        List<ProdSymbol> temp = new ArrayList<>();
+        for (ProdSymbol prod : prods) {
+          for (RuleComponentSymbol component : prod.getProdComponents()) {
+            for (ProdSymbol interfaceProd : interfaceProds) {
+              if (component.isPresentReferencedType() && component.getReferencedType().equals(interfaceProd.getName())) {
+                temp.add(interfaceProd);
               }
             }
           }
         }
         interfaceProds = temp;
       }
-        
-      if(!interfaceProds.isEmpty()){
-        for (ProdSymbol prodSymbol : prods) {
-          for (ProdSymbolSurrogate interfaceProdImplemented : prodSymbol.getSuperInterfaceProds()) {
-            for (int i = interfaceProds.size() - 1; i >= 0; --i) {
-              ProdSymbol interfaceProd = interfaceProds.get(i);
-              if (interfaceProdImplemented.getName().equals(interfaceProd.getName())) {
-                interfaceProds.remove(i);
-              }
-            }
-          }
+
+      // for every remaining interface production we check if it or any subinterface gets implemented anywhere and
+      // otherwise log an error since it gets used in a production without getting implemented
+      for (ProdSymbol interf : interfaceProds) {
+        if (!isImplemented(interf, prods, subSymbols)) {
+          Log.error(String.format(ERROR_CODE + ERROR_MSG_FORMAT, interf.getName()), a.get_SourcePositionStart());
         }
       }
-
-      for (ProdSymbol interf: interfaceProds) {
-        for (ProdSymbol prod : prods) {
-          List<String> checkList = Lists.newArrayList(interf.getName());
-          interf.getSuperInterfaceProds().stream().forEach(i -> checkList.add(i.getName()));
-          for (String name: checkList) {
-            if (prod.getProdComponents().stream().anyMatch(p -> p.isPresentReferencedType() && p.getReferencedType().equals(name))) {
-              Log.error(String.format(ERROR_CODE + ERROR_MSG_FORMAT, name), a.get_SourcePositionStart());
-            }
-          }
-        }
-      }
-
     }
+  }
+
+  protected boolean isImplemented(ProdSymbol interfaceSymbol, List<ProdSymbol> prods, Map<String, List<ProdSymbol>> subSymbols) {
+
+    // if a production exists that directly implements the interface return true
+    for (ProdSymbol prod : prods) {
+      for (ProdSymbol implemented : prod.getSuperInterfaceProds()) {
+        if (implemented.getName().equals(interfaceSymbol.getName())) {
+          return true;
+        }
+      }
+    }
+
+    // otherwise get all interfaces that extend this interface and recursively call this method on them. if any of them
+    // return true end the method and return true. otherwise, if none of its subinterface gets implemented, return false
+    if (subSymbols.containsKey(interfaceSymbol.getName())) {
+      for (ProdSymbol subSymbol : subSymbols.get(interfaceSymbol.getName())) {
+        if (isImplemented(subSymbol, prods, subSymbols)) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 
 }
--- monticore-grammar/src/test/java/de/monticore/grammar/cocos/InterfaceNTWithoutImplementationOnlyInComponentGrammarTest.java
+++ monticore-grammar/src/test/java/de/monticore/grammar/cocos/InterfaceNTWithoutImplementationOnlyInComponentGrammarTest.java
@@ -4,29 +4,51 @@ package de.monticore.grammar.cocos;
 
 import de.monticore.grammar.grammar_withconcepts._cocos.Grammar_WithConceptsCoCoChecker;
 import de.se_rwth.commons.logging.LogStub;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static de.monticore.grammar.cocos.InterfaceNTWithoutImplementationOnlyInComponentGrammar.ERROR_CODE;
-import static de.se_rwth.commons.logging.LogStub.enableFailQuick;
+
 import de.se_rwth.commons.logging.Log;
 
 public class InterfaceNTWithoutImplementationOnlyInComponentGrammarTest extends CocoTest {
 
-  private final String MESSAGE = " The interface nonterminal A must not be used without nonterminals " +
-          "implementing it in a grammar not marked as a grammar component.";
+  private final String MESSAGE_A = " The interface nonterminal A must not be used without nonterminals " +
+      "implementing it in a grammar not marked as a grammar component.";
+
+  private final String MESSAGE_D = " The interface nonterminal D must not be used without nonterminals " +
+      "implementing it in a grammar not marked as a grammar component.";
+
+  private final String MESSAGE_Z = " The interface nonterminal Z must not be used without nonterminals " +
+      "implementing it in a grammar not marked as a grammar component.";
+
   private static final Grammar_WithConceptsCoCoChecker checker = new Grammar_WithConceptsCoCoChecker();
+
   private final String grammar = "de.monticore.grammar.cocos.invalid.A0278.A0278";
+
   private final String grammar2 = "de.monticore.grammar.cocos.invalid.A0278.A0278b";
+
   private final String grammar3 = "de.monticore.grammar.cocos.invalid.A0278.A0278c";
-  
+
+  private final String grammar4 = "de.monticore.grammar.cocos.invalid.A0278.A0278d";
+
+  private final String grammar5 = "de.monticore.grammar.cocos.valid.InterfaceWithoutImplementation";
+
+  private final String grammar6 = "de.monticore.grammar.cocos.valid.InterfaceWithoutImplementation2";
+
+  private final String grammar7 = "de.monticore.grammar.cocos.valid.InterfaceWithoutImplementation3";
+
+  private final String grammar8 = "de.monticore.grammar.cocos.invalid.A0278.A0278e";
+
   @Before
   public void before() {
     LogStub.init();
     Log.enableFailQuick(false);
   }
-  
+
   @BeforeClass
   public static void disableFailQuick() {
     checker.addCoCo(new InterfaceNTWithoutImplementationOnlyInComponentGrammar());
@@ -35,19 +57,29 @@ public class InterfaceNTWithoutImplementationOnlyInComponentGrammarTest extends 
   @Test
   public void testInvalid() {
     testInvalidGrammar(grammar, ERROR_CODE,
-            MESSAGE, checker);
+        MESSAGE_A, checker);
   }
 
   @Test
   public void testInvalid2() {
     testInvalidGrammar(grammar2, ERROR_CODE,
-        MESSAGE, checker);
+        MESSAGE_A, checker);
   }
 
   @Test
   public void testInvalid3() {
     testInvalidGrammar(grammar3, ERROR_CODE,
-        MESSAGE, checker);
+        MESSAGE_A, checker);
+  }
+
+  @Test
+  public void testInvalid4() {
+    testInvalidGrammar(grammar4, ERROR_CODE, MESSAGE_D, checker);
+  }
+
+  @Test
+  public void testInvalid5() {
+    testInvalidGrammar(grammar8, ERROR_CODE, MESSAGE_Z, checker);
   }
 
   @Test
@@ -60,5 +92,24 @@ public class InterfaceNTWithoutImplementationOnlyInComponentGrammarTest extends 
     testValidGrammar("de.monticore.grammar.cocos.valid.Overriding", checker);
   }
 
+  @Test
+  public void testCorrect3() {
+    testValidGrammar(grammar5, checker);
+  }
+
+  @Test
+  public void testCorrect4() {
+    testValidGrammar(grammar6, checker);
+  }
+
+  @Test
+  public void testCorrect5() {
+    testValidGrammar(grammar7, checker);
+  }
+
+  @After
+  public void after() {
+    Log.getFindings().clear();
+  }
 
 }
