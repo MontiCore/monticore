147feacaab3d022bfb5edafd1d971dc65d330310
jonas.boecker <boecker@se-rwth.de>
2023-10-04 15:59:02 +0200

    add coco for literal assignment regex

---
+++ monticore-grammar/src/main/java/de/monticore/expressions/assignmentexpressions/cocos/LiteralAssignmentMatchesRegExExpressionCoCo.java
@@ -0,0 +1,44 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.expressions.assignmentexpressions.cocos;
+
+import de.monticore.expressions.assignmentexpressions.AssignmentExpressionsMill;
+import de.monticore.expressions.assignmentexpressions._ast.ASTAssignmentExpression;
+import de.monticore.expressions.assignmentexpressions._cocos.AssignmentExpressionsASTAssignmentExpressionCoCo;
+import de.monticore.expressions.assignmentexpressions._util.AssignmentExpressionsTypeDispatcher;
+import de.monticore.expressions.expressionsbasis._ast.ASTLiteralExpression;
+import de.monticore.literals.mccommonliterals.MCCommonLiteralsMill;
+import de.monticore.literals.mccommonliterals._util.MCCommonLiteralsTypeDispatcher;
+import de.monticore.types.check.IDerive;
+import de.monticore.types.check.TypeCheckResult;
+import de.se_rwth.commons.logging.Log;
+
+public class LiteralAssignmentMatchesRegExExpressionCoCo implements AssignmentExpressionsASTAssignmentExpressionCoCo {
+
+  protected IDerive derive;
+
+  public LiteralAssignmentMatchesRegExExpressionCoCo(IDerive derive) {
+    this.derive = derive;
+  }
+
+  @Override
+  public void check(ASTAssignmentExpression node) {
+    TypeCheckResult leftResult = derive.deriveType(node.getLeft());
+    AssignmentExpressionsTypeDispatcher expressionsDispatcher = AssignmentExpressionsMill.typeDispatcher();
+    MCCommonLiteralsTypeDispatcher literalDispatcher = MCCommonLiteralsMill.typeDispatcher();
+
+    if (leftResult.isPresentResult()) {
+      if (leftResult.getResult().isRegExType() && expressionsDispatcher.isASTLiteralExpression(node.getRight())) {
+        ASTLiteralExpression literalExpression = expressionsDispatcher.asASTLiteralExpression(node.getRight());
+        if (literalDispatcher.isASTStringLiteral(literalExpression.getLiteral())) {
+          String s = literalDispatcher.asASTStringLiteral(literalExpression).getSource();
+          String regex = leftResult.getResult().asRegExType().getRegExString();
+
+          if (!s.matches(regex)) {
+            Log.error("0xFD724 Variable of a regex type gets assigned to a string which is not compatible.");
+          }
+        }
+      }
+    }
+  }
+
+}
---
+++ monticore-grammar/src/test/java/de/monticore/expressions/assignmentexpressions/cocos/LiteralAssignmentMatchesRegExExpressionCoCoTest.java
@@ -0,0 +1,151 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.expressions.assignmentexpressions.cocos;
+
+import de.monticore.expressions.assignmentexpressions._cocos.AssignmentExpressionsCoCoChecker;
+import de.monticore.expressions.combineexpressionswithliterals.CombineExpressionsWithLiteralsMill;
+import de.monticore.expressions.combineexpressionswithliterals._ast.ASTFoo;
+import de.monticore.expressions.combineexpressionswithliterals._symboltable.ICombineExpressionsWithLiteralsArtifactScope;
+import de.monticore.expressions.combineexpressionswithliterals._visitor.CombineExpressionsWithLiteralsTraverser;
+import de.monticore.expressions.commonexpressions.types3.util.CommonExpressionsLValueRelations;
+import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
+import de.monticore.types.check.FullDeriveFromCombineExpressionsWithLiterals;
+import de.monticore.types.check.FullSynthesizeFromCombineExpressionsWithLiterals;
+import de.monticore.types.check.IDerive;
+import de.monticore.types.check.TypeCheckResult;
+import de.monticore.types.check.types3wrapper.TypeCheck3AsIDerive;
+import de.monticore.types.check.types3wrapper.TypeCheck3AsISynthesize;
+import de.monticore.types.mcbasictypes._ast.ASTMCType;
+import de.monticore.types3.Type4Ast;
+import de.monticore.types3.util.CombineExpressionsWithLiteralsTypeTraverserFactory;
+import de.se_rwth.commons.logging.Log;
+import de.se_rwth.commons.logging.LogStub;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class LiteralAssignmentMatchesRegExExpressionCoCoTest {
+
+  @Before
+  public void before() {
+    LogStub.init();
+    Log.enableFailQuick(false);
+    CombineExpressionsWithLiteralsMill.reset();
+    CombineExpressionsWithLiteralsMill.init();
+  }
+
+  @Test
+  public void testCorrectAssignments() throws IOException {
+    testValid("R\"hello\"", "t = \"hello\"");
+    testValid("R\"a|b\" ", "t =  \"a\"");
+    testValid("R\"a|b\" ", "t =  \"b\"");
+    testValid("R\"a[b]\" ", "t =  \"ab\"");
+    testValid("R\"a[bc]d\" ", "t =  \"abd\"");
+    testValid("R\"a[bc]d\" ", "t =  \"acd\"");
+    testValid("R\"a[b.c]\" ", "t =  \"a.\"");
+    testValid("R\"a[b-c]\" ", "t =  \"ab\"");
+    testValid("R\"a(?:bcd)e\" ", "t =  \"abcde\"");
+    testValid("R\"a(b|c)\\1d\" ", "t =  \"abbd\"");
+    testValid("R\"a(b|c)\\1d\" ", "t =  \"accd\"");
+    testValid("R\"abZ\" ", "t =  \"abZ\"");
+    testValid("R\"...\" ", "t =  \"abc\"");
+    testValid("R\"...\" ", "t =  \"123\"");
+    testValid("R\"...\" ", "t =  \"z9x\"");
+    testValid("R\"0129\" ", "t =  \"0129\"");
+    testValid("R\"^(b|c)d\" ", "t =  \"bd\"");
+    testValid("R\"a(bc)*d\" ", "t =  \"abcbcbcd\"");
+    testValid("R\"a(b|c){2,33}d\" ", "t =  \"abbbbbbcd\"");
+    testValid("R\"a(b|c){4}d\" ", "t =  \"acbbcd\"");
+    testValid("R\"a\\p{Lower}b\" ", "t =  \"azb\"");
+    testValid("R\"a\\w\\Bc\" ", "t =  \"abc\"");
+  }
+
+  @Test
+  public void testIncorrectAssignments() throws IOException {
+    testInvalid("R\"hello\"", "t = \"hi\"");
+    testInvalid("R\"a|b\" ", "t =  \"c\"");
+    testInvalid("R\"a[b]\" ", "t =  \"aa\"");
+    testInvalid("R\"a[bc]d\" ", "t =  \"adc\"");
+    testInvalid("R\"a[bc]d\" ", "t =  \"ad\"");
+    testInvalid("R\"a[b.c]\" ", "t =  \"a\"");
+    testInvalid("R\"a[b-c]\" ", "t =  \"lol\"");
+    testInvalid("R\"a(?:bcd)e\" ", "t =  \"ae\"");
+    testInvalid("R\"a(b|c)\\1d\" ", "t =  \"test\"");
+    testInvalid("R\"a(b|c)\\1d\" ", "t =  \"the\"");
+    testInvalid("R\"abZ\" ", "t =  \"cake\"");
+    testInvalid("R\"...\" ", "t =  \"is\"");
+    testInvalid("R\"...\" ", "t =  \"a\"");
+    testInvalid("R\"...\" ", "t =  \"liee\"");
+    testInvalid("R\"0129\" ", "t =  \"\"");
+    testInvalid("R\"^(b|c)d\" ", "t =  \"aaaaaaaaaa\"");
+    testInvalid("R\"a(bc)*d\" ", "t =  \"baby\"");
+    testInvalid("R\"a(b|c){2,33}d\" ", "t =  \"dont\"");
+    testInvalid("R\"a(b|c){4}d\" ", "t =  \"hurt\"");
+    testInvalid("R\"a\\p{Lower}b\" ", "t =  \"me\"");
+    testInvalid("R\"a\\w\\Bc\" ", "t =  \"ende\"");
+  }
+
+  protected void testValid(String type, String exprStr) throws IOException {
+    check(type, exprStr);
+    assertTrue(Log.getFindings().isEmpty());
+    Log.clearFindings();
+  }
+
+  protected void testInvalid(String type, String exprStr) throws IOException {
+    check(type, exprStr);
+    assertEquals(1, Log.getFindings().size());
+    assertTrue(Log.getFindings().get(0).getMsg().startsWith("0xFD724"));
+    Log.clearFindings();
+  }
+
+  protected void check(String type, String exprStr) throws IOException {
+    CombineExpressionsWithLiteralsMill.globalScope().clear();
+
+    CombineExpressionsWithLiteralsTypeTraverserFactory factory = new CombineExpressionsWithLiteralsTypeTraverserFactory();
+    Type4Ast type4Ast = new Type4Ast();
+    CombineExpressionsWithLiteralsTraverser traverser = factory.createTraverser(type4Ast);
+    TypeCheck3AsIDerive derive = new TypeCheck3AsIDerive(traverser, type4Ast, new CommonExpressionsLValueRelations());
+    TypeCheck3AsISynthesize synthesize = new TypeCheck3AsISynthesize(traverser, type4Ast);
+
+    Optional<ASTMCType> optType = CombineExpressionsWithLiteralsMill.parser().parse_StringMCType(type);
+    assertTrue(optType.isPresent());
+
+    TypeCheckResult typeExpression = synthesize.synthesizeType(optType.get());
+    assertTrue(typeExpression.isPresentResult());
+
+    CombineExpressionsWithLiteralsMill.globalScope().add(CombineExpressionsWithLiteralsMill.variableSymbolBuilder()
+        .setName("t")
+        .setType(typeExpression.getResult())
+        .build());
+
+    Optional<ASTExpression> exprOpt = CombineExpressionsWithLiteralsMill
+        .parser().parse_StringExpression(exprStr);
+    assertTrue(exprOpt.isPresent());
+
+    generateScopes(exprOpt.get());
+
+    assertTrue(Log.getFindings().isEmpty());
+    getChecker(derive).checkAll(exprOpt.get());
+  }
+
+  protected AssignmentExpressionsCoCoChecker getChecker(IDerive derive) {
+    AssignmentExpressionsCoCoChecker checker = new AssignmentExpressionsCoCoChecker();
+    checker.addCoCo(new LiteralAssignmentMatchesRegExExpressionCoCo(derive));
+    return checker;
+  }
+
+  protected void generateScopes(ASTExpression expr) {
+    // create a root
+    ASTFoo rootNode = CombineExpressionsWithLiteralsMill.fooBuilder()
+        .setExpression(expr)
+        .build();
+    ICombineExpressionsWithLiteralsArtifactScope rootScope =
+        CombineExpressionsWithLiteralsMill.scopesGenitorDelegator()
+            .createFromAST(rootNode);
+    rootScope.setName("fooRoot");
+  }
+}
