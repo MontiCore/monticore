5d90d524154340b9f7d8379d9090f8f68cf406a6
lamelizard <lamelizard@users.noreply.github.com>
2023-11-27 17:26:46 +0100

    Move CreatorExpression to UglyExpressions.mc4

--- monticore-grammar/src/main/java/de/monticore/types/check/DeriveSymTypeOfUglyExpressions.java
+++ monticore-grammar/src/main/java/de/monticore/types/check/DeriveSymTypeOfUglyExpressions.java
@@ -1,15 +1,27 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.types.check;
 
+import com.google.common.collect.Lists;
+import de.monticore.expressions.expressionsbasis._ast.ASTArguments;
+import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
+import de.monticore.expressions.uglyexpressions._ast.ASTArrayCreator;
+import de.monticore.expressions.uglyexpressions._ast.ASTArrayDimensionByExpression;
+import de.monticore.expressions.uglyexpressions._ast.ASTClassCreator;
 import de.monticore.expressions.uglyexpressions._ast.ASTInstanceofExpression;
 import de.monticore.expressions.uglyexpressions._ast.ASTTypeCastExpression;
 import de.monticore.expressions.uglyexpressions._visitor.UglyExpressionsHandler;
 import de.monticore.expressions.uglyexpressions._visitor.UglyExpressionsTraverser;
 import de.monticore.expressions.uglyexpressions._visitor.UglyExpressionsVisitor2;
+import de.monticore.symbols.basicsymbols._symboltable.FunctionSymbol;
+import de.monticore.symbols.basicsymbols._symboltable.VariableSymbol;
+import de.monticore.symboltable.modifiers.AccessModifier;
 import de.monticore.types.mcbasictypes._ast.ASTMCReturnType;
 import de.monticore.types.mcbasictypes._ast.ASTMCType;
 import de.monticore.types.mccollectiontypes._ast.ASTMCTypeArgument;
 import de.se_rwth.commons.logging.Log;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static de.monticore.types.check.TypeCheck.compatible;
 
@@ -134,6 +146,142 @@ public class DeriveSymTypeOfUglyExpressions
       return castResult;
     }
     return SymTypeExpressionFactory.createObscureType();
+  }
+
+  @Override
+  public void traverse(ASTClassCreator creator) {
+    SymTypeExpression extType;
+    SymTypeExpression wholeResult = SymTypeExpressionFactory.createObscureType();
+    deprecated_traverse(creator.getMCType());
+    if (getTypeCheckResult().isPresentResult()) {
+      extType = getTypeCheckResult().getResult();
+    }
+    else {
+      getTypeCheckResult().reset();
+      getTypeCheckResult().setResult(wholeResult);
+      logError("0xA1311", creator.getMCType().get_SourcePositionStart());
+      return;
+    }
+
+    if (!extType.isObscureType()) {
+
+      if (!extType.isPrimitive()) {
+        //see if there is a constructor fitting for the arguments
+        List<FunctionSymbol> constructors = extType.getMethodList(extType.getTypeInfo().getName(), false, AccessModifier.ALL_INCLUSION);
+        if (!constructors.isEmpty()) {
+          if (testForCorrectArguments(constructors, creator.getArguments())) {
+            wholeResult = extType;
+          }
+        }
+        else if (creator.getArguments().isEmptyExpressions()) {
+          //no constructor in this class -> default constructor without arguments, only possible if arguments in creator are empty
+          wholeResult = extType;
+        }
+      }
+
+      getTypeCheckResult().reset();
+      getTypeCheckResult().setResult(wholeResult);
+      if (wholeResult.isObscureType()) {
+        logError("0xA1312", creator.get_SourcePositionStart());
+      }
+    }
+  }
+
+  @Override
+  public void traverse(ASTArrayCreator creator) {
+    SymTypeExpression extTypeResult;
+    SymTypeExpression wholeResult = SymTypeExpressionFactory.createObscureType();
+
+    deprecated_traverse(creator.getMCType());
+    if (getTypeCheckResult().isPresentResult()) {
+      extTypeResult = getTypeCheckResult().getResult();
+    }
+    else {
+      getTypeCheckResult().reset();
+      getTypeCheckResult().setResult(wholeResult);
+      logError("0xA0314", creator.getMCType().get_SourcePositionStart());
+      return;
+    }
+
+    if (!extTypeResult.isObscureType()) {
+      //the definition of the Arrays are based on the assumption that MCType is not an array
+      if (!extTypeResult.isArrayType()) {
+        if (creator.getArrayDimensionSpecifier() instanceof ASTArrayDimensionByExpression) {
+          ASTArrayDimensionByExpression arrayInitializer = (ASTArrayDimensionByExpression) creator.getArrayDimensionSpecifier();
+          int dim = arrayInitializer.getDimList().size() + arrayInitializer.getExpressionList().size();
+          //teste dass alle Expressions integer-zahl sind
+          for (ASTExpression expr : arrayInitializer.getExpressionList()) {
+            expr.accept(getTraverser());
+            if (getTypeCheckResult().isPresentResult()) {
+              SymTypeExpression result = getTypeCheckResult().getResult();
+              if (result.isPrimitive()) {
+                if (!((SymTypePrimitive) result).isIntegralType()) {
+                  getTypeCheckResult().reset();
+                  getTypeCheckResult().setResult(wholeResult);
+                  logError("0xA0315", expr.get_SourcePositionStart());
+                  return;
+                }
+              }
+              else {
+                getTypeCheckResult().reset();
+                getTypeCheckResult().setResult(wholeResult);
+                logError("0xA0316", expr.get_SourcePositionStart());
+                return;
+              }
+            }
+            else {
+              getTypeCheckResult().reset();
+              getTypeCheckResult().setResult(wholeResult);
+              logError("0xA0317", expr.get_SourcePositionStart());
+              return;
+            }
+          }
+          wholeResult = SymTypeExpressionFactory.createTypeArray(extTypeResult.getTypeInfo(), dim, extTypeResult);
+        }
+      }
+
+      getTypeCheckResult().reset();
+      getTypeCheckResult().setResult(wholeResult);
+      if (wholeResult.isObscureType()) {
+        logError("0xA0318", creator.get_SourcePositionStart());
+      }
+    }
+  }
+
+  protected List<SymTypeExpression> calculateCorrectArguments(ASTArguments args) {
+    List<SymTypeExpression> argList = Lists.newArrayList();
+    for (int i = 0; i < args.getExpressionList().size(); i++) {
+      args.getExpression(i).accept(getTraverser());
+      if (getTypeCheckResult().isPresentResult()) {
+        argList.add(getTypeCheckResult().getResult());
+      }
+      else {
+        getTypeCheckResult().reset();
+        argList.add(SymTypeExpressionFactory.createObscureType());
+        logError("0xA0313", args.getExpressionList().get(i).get_SourcePositionStart());
+      }
+    }
+    return argList;
+  }
+
+  protected boolean testForCorrectArguments(List<FunctionSymbol> constructors, ASTArguments arguments) {
+    List<SymTypeExpression> symTypeOfArguments = calculateCorrectArguments(arguments);
+    outer:
+    for (FunctionSymbol constructor : constructors) {
+      if (constructor.getParameterList().size() == symTypeOfArguments.size()) {
+        //get the types of the constructor arguments
+        List<SymTypeExpression> constructorArguments = constructor.getParameterList().stream().map(VariableSymbol::getType).collect(Collectors.toList());
+        for (int i = 0; i < constructorArguments.size(); i++) {
+          if (!compatible(constructorArguments.get(i), symTypeOfArguments.get(i))) {
+            //wrong constructor, argument is not compatible to constructor definition
+            continue outer;
+          }
+        }
+        //if this is reached, then the arguments match a constructor's arguments -> return true
+        return true;
+      }
+    }
+    return false;
   }
 
   // Warning: deprecated and wrong behavior
