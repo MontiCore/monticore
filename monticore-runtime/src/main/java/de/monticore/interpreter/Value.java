45f41bde94f8346ebdc43dff0732074dff8ca2fb
jonas.boecker <boecker@se-rwth.de>
2023-11-30 15:35:57 +0100

    add distinct error codes to values

--- monticore-runtime/src/main/java/de/monticore/interpreter/Value.java
+++ monticore-runtime/src/main/java/de/monticore/interpreter/Value.java
@@ -44,37 +44,37 @@ public interface Value {
   }
 
   default int asInt() {
-    Log.error("0x31251 Type int is not applicable for result value.");
+    Log.error("0x31252 Type int is not applicable for result value.");
     return 0;
   }
 
   default double asDouble() {
-    Log.error("0x31251 Type double is not applicable for result value.");
+    Log.error("0x31253 Type double is not applicable for result value.");
     return 0.0;
   }
 
   default String asString() {
-    Log.error("0x31251 Type String is not applicable for result value.");
+    Log.error("0x31254 Type String is not applicable for result value.");
     return "";
   }
 
   default char asChar() {
-    Log.error("0x31251 Type char is not applicable for result value.");
+    Log.error("0x31255 Type char is not applicable for result value.");
     return '\u0000';
   }
 
   default Object asObject() {
-    Log.error("0x31251 Type Object is not applicable for result value.");
+    Log.error("0x31256 Type Object is not applicable for result value.");
     return new Object();
   }
 
   default long asLong() {
-    Log.error("0x31251 Type long is not applicable for result value.");
+    Log.error("0x31257 Type long is not applicable for result value.");
     return 0L;
   }
 
   default float asFloat() {
-    Log.error("0x31251 Type float is not applicable for result value.");
+    Log.error("0x31258 Type float is not applicable for result value.");
     return 0.0f;
   }
 }
