2da7dfa7e57643bf69ba2685aa5b12d48749055c
lamelizard <lamelizard@users.noreply.github.com>
2024-01-08 13:18:37 +0100

    AbstractTypeVisitorTest::hasErrorCode

--- monticore-grammar/src/test/java/de/monticore/types3/AbstractTypeVisitorTest.java
+++ monticore-grammar/src/test/java/de/monticore/types3/AbstractTypeVisitorTest.java
@@ -349,6 +349,10 @@ public class AbstractTypeVisitorTest extends AbstractTypeTest {
     return getFirstErrorCodes(Log.getErrorCount());
   }
 
+  protected boolean hasErrorCode(String code) {
+    return getAllErrorCodes().stream().anyMatch(code::equals);
+  }
+
   protected void assertHasErrorCode(String code) {
     assertTrue(
         "Error \"" + code + "\" expected, "
@@ -358,7 +362,7 @@ public class AbstractTypeVisitorTest extends AbstractTypeTest {
             .map(Finding::buildMsg)
             .collect(Collectors.joining(System.lineSeparator()))
             + System.lineSeparator(),
-        getAllErrorCodes().stream().anyMatch(code::equals)
+        hasErrorCode(code)
     );
   }
 
