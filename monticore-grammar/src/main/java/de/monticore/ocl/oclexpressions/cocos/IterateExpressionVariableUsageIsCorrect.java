3c163a9007306be55406c2b947e99933acd48353
lamelizard <lamelizard@users.noreply.github.com>
2024-01-08 14:55:55 +0100

    cp OCL CoCos

---
+++ monticore-grammar/src/main/java/de/monticore/ocl/oclexpressions/cocos/IterateExpressionVariableUsageIsCorrect.java
@@ -0,0 +1,21 @@
+// (c) https://github.com/MontiCore/monticore
+package de.monticore.ocl.oclexpressions.cocos;
+
+import de.monticore.ocl.oclexpressions._ast.ASTIterateExpression;
+import de.monticore.ocl.oclexpressions._cocos.OCLExpressionsASTIterateExpressionCoCo;
+import de.se_rwth.commons.logging.Log;
+
+public class IterateExpressionVariableUsageIsCorrect
+    implements OCLExpressionsASTIterateExpressionCoCo {
+
+  @Override
+  public void check(ASTIterateExpression node) {
+    String variableName = node.getInit().getName();
+    if (!node.getName().equals(variableName)) {
+      Log.error(
+          String.format(
+              "0xOCL020 variable declared in IterateExpression must be used as accumulator variable"),
+          node.get_SourcePositionStart());
+    }
+  }
+}
