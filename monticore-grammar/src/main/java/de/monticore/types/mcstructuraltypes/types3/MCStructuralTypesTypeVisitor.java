01f708776e78520f16442291916d55ac2f0d38c5
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2024-01-23 14:59:37 +0100

    StructuralTypes -> InfixTypes

--- monticore-grammar/src/main/java/de/monticore/types/mcstructuraltypes/types3/MCStructuralTypesTypeVisitor.java
+++ monticore-grammar/src/main/java/de/monticore/types/mcstructuraltypes/types3/MCStructuralTypesTypeVisitor.java
@@ -4,16 +4,17 @@ package de.monticore.types.mcstructuraltypes.types3;
 import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types.check.SymTypeExpressionFactory;
 import de.monticore.types.mcbasictypes._ast.ASTMCType;
+import de.monticore.types.mcstructuraltypes._ast.ASTMCBracketType;
 import de.monticore.types.mcstructuraltypes._ast.ASTMCIntersectionType;
+import de.monticore.types.mcstructuraltypes._ast.ASTMCTupleType;
 import de.monticore.types.mcstructuraltypes._ast.ASTMCUnionType;
 import de.monticore.types.mcstructuraltypes._visitor.MCStructuralTypesVisitor2;
 import de.monticore.types3.AbstractTypeVisitor;
-import de.se_rwth.commons.logging.Log;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MCStructuralTypesTypeVisitor extends AbstractTypeVisitor
     implements MCStructuralTypesVisitor2 {
@@ -23,8 +24,7 @@ public class MCStructuralTypesTypeVisitor extends AbstractTypeVisitor
     SymTypeExpression symType;
 
     List<SymTypeExpression> arguments = new LinkedList<>();
-    List<ASTMCType> astArguments =
-        transformUnionTree2List(unionType.getMCTypeList());
+    List<ASTMCType> astArguments = transformUnionTree2List(unionType);
     if (astArguments.isEmpty()) {
       // error logged
       getType4Ast().setTypeOfTypeIdentifier(unionType,
@@ -50,7 +50,7 @@ public class MCStructuralTypesTypeVisitor extends AbstractTypeVisitor
 
     List<SymTypeExpression> arguments = new LinkedList<>();
     List<ASTMCType> astArguments =
-        transformIntersectionTree2List(intersectionType.getMCTypeList());
+        transformIntersectionTree2List(intersectionType);
     if (astArguments.isEmpty()) {
       // error logged
       getType4Ast().setTypeOfTypeIdentifier(intersectionType,
@@ -71,66 +71,67 @@ public class MCStructuralTypesTypeVisitor extends AbstractTypeVisitor
     getType4Ast().setTypeOfTypeIdentifier(intersectionType, symType);
   }
 
+  public void endVisit(ASTMCTupleType tupleType) {
+    SymTypeExpression symType;
+
+    List<SymTypeExpression> arguments = tupleType.streamMCTypes()
+        .map(t -> getType4Ast().getPartialTypeOfTypeId(t))
+        .collect(Collectors.toList());
+    if (arguments.stream().anyMatch(SymTypeExpression::isObscureType)) {
+      // error logged
+      symType = SymTypeExpressionFactory.createObscureType();
+    }
+    else {
+      symType = SymTypeExpressionFactory.createTuple(arguments);
+    }
+
+    getType4Ast().setTypeOfTypeIdentifier(tupleType, symType);
+  }
+
+  public void endVisit(ASTMCBracketType bracketType) {
+    SymTypeExpression symType;
+    symType = getType4Ast().getPartialTypeOfTypeId(bracketType.getMCType());
+    getType4Ast().setTypeOfTypeIdentifier(bracketType, symType);
+  }
+
   // Helper
 
   /**
-   * in case that the AST has not been modified,
    * converts the parsed tree to a list
+   * this is a specific kind of type normalization,
+   * which also reduces the complexity of the type printed,
+   * which is the reason this has been added here.
+   * E.g., given A | B | C, (A | B | C) is printed rather than ((A | B) | C).
    */
-  protected List<ASTMCType> transformUnionTree2List(List<ASTMCType> mcTypes) {
+  protected List<ASTMCType> transformUnionTree2List(ASTMCType mcType) {
     List<ASTMCType> result = new ArrayList<>();
-    for (ASTMCType mcType : mcTypes) {
-      // todo replace with typedispatcher as soon as the issues are fixed
-      if (mcType instanceof ASTMCUnionType &&
-          !((ASTMCUnionType) mcType).isPresentParenthesis()) {
-        List<ASTMCType> flattenedAstTypes = transformUnionTree2List(
-            ((ASTMCUnionType) mcType).getMCTypeList()
-        );
-        result.addAll(flattenedAstTypes);
-      }
-      else if (mcType instanceof ASTMCIntersectionType &&
-          !((ASTMCIntersectionType) mcType).isPresentParenthesis()) {
-        Log.errorUser("0xFD770 use parenthesis when combining "
-                + "union and intersection types",
-            mcType.get_SourcePositionStart(),
-            mcType.get_SourcePositionEnd()
-        );
-        return Collections.emptyList();
-      }
-      else {
-        result.add(mcType);
-      }
+    // todo replace with typedispatcher as soon as the issues are fixed
+    if (mcType instanceof ASTMCUnionType) {
+      result.addAll(transformUnionTree2List(((ASTMCUnionType) mcType).getLeft()));
+      result.addAll(transformUnionTree2List(((ASTMCUnionType) mcType).getRight()));
+    }
+    else {
+      result.add(mcType);
     }
     return result;
   }
 
   /**
-   * in case that the AST has not been modified,
    * converts the parsed tree to a list
+   * this is a specific kind of type normalization,
+   * which also reduces the complexity of the type printed,
+   * which is the reason this has been added here.
+   * E.g., given A & B & C, (A & B & C) is printed rather than ((A & B) & C).
    */
-  protected List<ASTMCType> transformIntersectionTree2List(List<ASTMCType> mcTypes) {
+  protected List<ASTMCType> transformIntersectionTree2List(ASTMCType mcType) {
     List<ASTMCType> result = new ArrayList<>();
-    for (ASTMCType mcType : mcTypes) {
-      // todo replace with typedispatcher as soon as the issues are fixed
-      if (mcType instanceof ASTMCIntersectionType &&
-          !((ASTMCIntersectionType) mcType).isPresentParenthesis()) {
-        List<ASTMCType> flattenedAstTypes = transformIntersectionTree2List(
-            ((ASTMCIntersectionType) mcType).getMCTypeList()
-        );
-        result.addAll(flattenedAstTypes);
-      }
-      else if (mcType instanceof ASTMCUnionType &&
-          !((ASTMCUnionType) mcType).isPresentParenthesis()) {
-        Log.errorUser("0xFD771 use parenthesis when combining "
-                + "union and intersection types",
-            mcType.get_SourcePositionStart(),
-            mcType.get_SourcePositionEnd()
-        );
-        return Collections.emptyList();
-      }
-      else {
-        result.add(mcType);
-      }
+    // todo replace with typedispatcher as soon as the issues are fixed
+    if (mcType instanceof ASTMCIntersectionType) {
+      result.addAll(transformIntersectionTree2List(((ASTMCIntersectionType) mcType).getLeft()));
+      result.addAll(transformIntersectionTree2List(((ASTMCIntersectionType) mcType).getRight()));
+    }
+    else {
+      result.add(mcType);
     }
     return result;
   }
