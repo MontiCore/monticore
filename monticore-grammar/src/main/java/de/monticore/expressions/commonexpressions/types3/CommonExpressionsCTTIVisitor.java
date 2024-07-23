28597752b164bec5d6aef4411caae924f63acece
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2024-07-23 13:38:10 +0200

    TypeCheck CTTI allow to handle operators

--- monticore-grammar/src/main/java/de/monticore/expressions/commonexpressions/types3/CommonExpressionsCTTIVisitor.java
+++ monticore-grammar/src/main/java/de/monticore/expressions/commonexpressions/types3/CommonExpressionsCTTIVisitor.java
@@ -8,19 +8,14 @@ import de.monticore.expressions.commonexpressions._ast.ASTFieldAccessExpression;
 import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
 import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types.check.SymTypeOfFunction;
-import de.monticore.types3.generics.constraints.ExpressionCompatibilityConstraint;
 import de.monticore.types3.generics.context.InferenceContext;
 import de.monticore.types3.generics.context.InferenceResult;
 import de.monticore.types3.generics.context.InferenceVisitorMode;
 import de.monticore.types3.generics.util.CompileTimeTypeCalculator;
-import de.se_rwth.commons.logging.Log;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-
-import static de.monticore.types.check.SymTypeExpressionFactory.createObscureType;
 
 /**
  * CommonExpressionsTypeVisitor + generics support
@@ -71,6 +66,10 @@ public class CommonExpressionsCTTIVisitor
 
     if (infCtx.getVisitorMode() != InferenceVisitorMode.TYPE_CHECKING) {
       expr.getExpression().accept(getTraverser());
+      List<InferenceResult> infRess = getInferenceContext4Ast()
+          .getContextOfExpression(expr.getExpression()).getInferenceResults();
+      getInferenceContext4Ast()
+          .getContextOfExpression(expr).setInferredTypes(infRess);
     }
     else {
       visit(expr);
@@ -99,80 +98,11 @@ public class CommonExpressionsCTTIVisitor
       }
     }
 
-    InferenceContext funcCtx = visitForInference(expr);
-
-    List<InferenceResult> inferenceResults = funcCtx.getInferenceResults();
-    // small check that the visitor is configured correctly
-    if (mode == InferenceVisitorMode.TYPE_CHECKING &&
-        !inferenceResults.isEmpty()
-    ) {
-      Log.error("0xFD212 internal error: unexpected inference result."
-              + " TypeCheck misconfigured?",
-          expr.get_SourcePositionStart(),
-          expr.get_SourcePositionEnd()
-      );
-      getType4Ast().setTypeOfExpression(expr, createObscureType());
-      return;
-    }
-    // handle errors
-    if (inferenceResults.stream().anyMatch(InferenceResult::hasErrorOccurred)
-        || (getType4Ast().hasPartialTypeOfExpression(expr.getExpression())
-        && getType4Ast().getPartialTypeOfExpr(expr.getExpression()).isObscureType())
-    ) {
-      // already logged
-      getType4Ast().setTypeOfExpression(expr, createObscureType());
-      return;
-    }
-
-    //pass inference information upwards
-    if (!inferenceResults.isEmpty()) {
-      if (inferenceResults.stream().anyMatch(
-          infRes -> infRes.getLastInferenceMode()
-              == InferenceVisitorMode.APPLICABILITY_TEST
-      )) {
-        // Since there are inference results,
-        // there is a function with a return type that must be inferred.
-        // Thus, this callExpression is not pertinent to applicability
-        InferenceResult applicabilityRes = new InferenceResult();
-        applicabilityRes.setLastInferenceMode(InferenceVisitorMode.APPLICABILITY_TEST);
-        callCtx.setInferredTypes(List.of(applicabilityRes));
-      }
-      else if (inferenceResults.stream().allMatch(infRes -> infRes.getLastInferenceMode()
-          == InferenceVisitorMode.EXPRESSION_COMPATIBILITY_REDUCTION
-      )) {
-        List<ExpressionCompatibilityConstraint> constraints = new ArrayList<>();
-        for (InferenceResult inferenceResult : inferenceResults) {
-          constraints.addAll(inferenceResult.getB4C());
-        }
-        InferenceResult applicabilityRes = new InferenceResult();
-        applicabilityRes.setLastInferenceMode(InferenceVisitorMode.EXPRESSION_COMPATIBILITY_REDUCTION);
-        applicabilityRes.setB3(Collections.emptyList());
-        applicabilityRes.setB4C(constraints);
-        callCtx.setInferredTypes(List.of(applicabilityRes));
-      }
-    }
-    // or calculate the type
-    else {
-      // here: mode = InferenceVisitorMode.TYPE_CHECKING;
-      // at this point, the function type has been calculated
-
-      // set argument target types
-      // the calculated function type should
-      // 1. not contain any inference variables
-      // 2. and have fixed arity
-      SymTypeOfFunction targetFunction = getType4Ast()
-          .getPartialTypeOfExpr(expr.getExpression()).asFunctionType();
-      for (int i = 0; i < expr.getArguments().sizeExpressions(); i++) {
-        ASTExpression argExpr = expr.getArguments().getExpression(i);
-        getInferenceContext4Ast().setTargetTypeOfExpression(
-            argExpr,
-            targetFunction.getArgumentType(i)
-        );
-      }
-
-      visit(expr);
-      // traverse without already calculated function
-      expr.getArguments().accept(getTraverser());
+    visitForInference(expr);
+    CompileTimeTypeCalculator.handleCall(expr, expr.getExpression(),
+        expr.getArguments().getExpressionList(),
+        getTraverser(), getType4Ast(), getInferenceContext4Ast());
+    if (getType4Ast().hasPartialTypeOfExpression(expr)) {
       endVisit(expr);
     }
   }
@@ -184,17 +114,13 @@ public class CommonExpressionsCTTIVisitor
    * The kind of information gathered is
    * dependent on the inference mode the visitor is in.
    */
-  protected InferenceContext visitForInference(ASTCallExpression expr) {
+  protected void visitForInference(ASTCallExpression expr) {
     InferenceContext callCtx = getInferenceContext4Ast().getContextOfExpression(expr);
     InferenceVisitorMode mode = callCtx.getVisitorMode();
     ASTExpression funcExpr = expr.getExpression();
     InferenceContext funcCtx = new InferenceContext();
     funcCtx.setVisitorMode(mode);
-    int parCount = expr.getArguments().sizeExpressions();
-    funcCtx.getPartialFunctionInfo().setParameterCount(parCount);
-    for (int i = 0; i < parCount; i++) {
-      funcCtx.getPartialFunctionInfo().setArgumentExpr(i, expr.getArguments().getExpression(i));
-    }
+    funcCtx.getPartialFunctionInfo().setArgumentExprs(expr.getArguments().getExpressionList());
     if (callCtx.hasTargetType()) {
       funcCtx.getPartialFunctionInfo().setReturnTargetType(callCtx.getTargetType());
     }
@@ -212,8 +138,9 @@ public class CommonExpressionsCTTIVisitor
     )) {
       getType4Ast().reset(funcExpr);
     }
-    getInferenceContext4Ast().reset(funcExpr);
-    return funcCtx;
+    else {
+      getInferenceContext4Ast().reset(funcExpr);
+    }
   }
 
   @Override
@@ -222,7 +149,7 @@ public class CommonExpressionsCTTIVisitor
       SymTypeExpression resolvedType
   ) {
     CompileTimeTypeCalculator.handleResolvedType(
-        expr, Optional.of(resolvedType),
+        expr, resolvedType,
         getTraverser(), getType4Ast(), getInferenceContext4Ast()
     );
   }
