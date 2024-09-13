d9dadc7414508f2528a39f433748891e4659cc1d
David Schmalzing <schmalzing@se-rwth.de>
2024-09-13 13:54:19 +0200

    Use gradle tool

--- monticore-generator/src/main/java/de/monticore/gradle/gen/MCGradleTool.java
+++ monticore-generator/src/main/java/de/monticore/gradle/gen/MCGradleTool.java
@@ -8,13 +8,13 @@ import de.monticore.grammar.grammar_withconcepts.Grammar_WithConceptsMill;
 public class MCGradleTool extends MontiCoreTool {
 
   /**
-   * Main method that is called from command line and runs the MontiCore tool.
+   * Main method entry that configures and runs the MontiCore tool.
    *
    * @param args The input parameters for configuring the MontiCore tool
    */
   public static void main(String[] args) {
     GradleLog.init();
     Grammar_WithConceptsMill.init();
-    new MontiCoreTool().run(args);
+    new MCGradleTool().run(args);
   }
 }
