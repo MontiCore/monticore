41a1dc9d2ebe130941e91229d857c6b33b1315de
David Pascal Schmalzing <schmalzing@se-rwth.de>
2024-10-28 18:21:35 +0100

    Adapt to typecheck 3

--- monticore-grammar/src/main/java/de/monticore/statements/mccommonstatements/cocos/AssertIsValid.java
+++ monticore-grammar/src/main/java/de/monticore/statements/mccommonstatements/cocos/AssertIsValid.java
@@ -1,11 +1,13 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.statements.mccommonstatements.cocos;
 
+import com.google.common.base.Preconditions;
 import de.monticore.statements.mcassertstatements._ast.ASTAssertStatement;
 import de.monticore.statements.mcassertstatements._cocos.MCAssertStatementsASTAssertStatementCoCo;
 import de.monticore.types.check.SymTypeExpression;
-import de.monticore.types.check.TypeCheck;
 import de.monticore.types.check.TypeCalculator;
+import de.monticore.types3.SymTypeRelations;
+import de.monticore.types3.TypeCheck3;
 import de.se_rwth.commons.logging.Log;
 
 public class AssertIsValid implements MCAssertStatementsASTAssertStatementCoCo {
@@ -17,27 +19,46 @@ public class AssertIsValid implements MCAssertStatementsASTAssertStatementCoCo {
   public static final String ERROR_CODE_2 = "0xA0902";
   
   public static final String ERROR_MSG_FORMAT_2 = "Assert-statement must not be of void type.";
-  
+
   TypeCalculator typeCheck;
-  
-  public AssertIsValid(TypeCalculator typeCheck){
-    
+
+  /**
+   * @deprecated use default constructor
+   */
+  @Deprecated
+  public AssertIsValid(TypeCalculator typeCheck) {
     this.typeCheck = typeCheck;
-    
   }
-  
+
   @Override
   public void check(ASTAssertStatement node) {
+    Preconditions.checkNotNull(node);
+
+    SymTypeExpression assertion;
+
+    if (typeCheck != null) {
+      // support deprecated behavior
+      assertion = typeCheck.typeOf(node.getAssertion());
+    } else {
+      assertion = TypeCheck3.typeOf(node.getAssertion());
+    }
   
-    SymTypeExpression result = typeCheck.typeOf(node.getAssertion());
-  
-    if(!TypeCheck.isBoolean(result)){
+    if(!SymTypeRelations.isBoolean(assertion)){
       Log.error(ERROR_CODE + ERROR_MSG_FORMAT, node.getAssertion().get_SourcePositionStart());
     }
-    
+
     if(node.isPresentMessage()) {
-      result = typeCheck.typeOf(node.getMessage());
-      if (!TypeCheck.isVoid(result)) {
+
+      SymTypeExpression message;
+
+      if (typeCheck != null) {
+        // support deprecated behavior
+        message = typeCheck.typeOf(node.getMessage());
+      } else {
+        message = TypeCheck3.typeOf(node.getMessage());
+      }
+
+      if (message.isVoidType()) {
         Log.error(ERROR_CODE_2 + ERROR_MSG_FORMAT_2, node.getMessage().get_SourcePositionStart());
       }
     }
