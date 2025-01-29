556cbd3f9bdbbe4f584e3eb4132b02991c9f1882
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2025-01-29 09:21:25 +0100

    ExpressionValid extend deprecation explanation

--- monticore-grammar/src/main/java/de/monticore/expressions/cocos/ExpressionValid.java
+++ monticore-grammar/src/main/java/de/monticore/expressions/cocos/ExpressionValid.java
@@ -11,6 +11,10 @@ import java.util.Optional;
  * @deprecated not compatible with target typing,
  * thus, this needs to be replaced with multiple CoCos,
  * which do pass the target type if available.
+ * These CoCos have to be created for each non-terminal that contains
+ * an Expression.
+ * Please refer to the TypeCheck3 documentation and specifically
+ * {@link de.monticore.types3.TypeCheck3#typeOf(ASTExpression, de.monticore.types.check.SymTypeExpression)}.
  */
 @Deprecated
 public class ExpressionValid implements ExpressionsBasisASTExpressionCoCo {
