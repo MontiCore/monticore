7798fc57ec6b9d6d6cadd8c64b59811d85a85b1c
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2025-02-12 18:04:40 +0100

    Stream library

--- monticore-libraries/stream-symbols-it/src/test/java/de/monticore/symbols/library/StreamTypeTest.java
+++ monticore-libraries/stream-symbols-it/src/test/java/de/monticore/symbols/library/StreamTypeTest.java
@@ -1,43 +1,52 @@
 // (c) https://github.com/MontiCore/monticore
 package de.monticore.symbols.library;
 
-import de.monticore.io.FileReaderWriter;
 import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
 import de.monticore.symbols.basicsymbols._symboltable.TypeSymbol;
 import de.monticore.symbols.basicsymbols._symboltable.TypeVarSymbol;
 import de.monticore.symbols.oosymbols.OOSymbolsMill;
-import de.monticore.symbols.oosymbols._symboltable.*;
+import de.monticore.symbols.oosymbols._symboltable.MethodSymbol;
+import de.monticore.symbols.oosymbols._symboltable.MethodSymbolDeSer;
+import de.monticore.symbols.oosymbols._symboltable.OOTypeSymbolDeSer;
 import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types.check.SymTypeOfGenerics;
+import de.monticore.types3.util.WithinScopeBasicSymbolsResolver;
 import de.se_rwth.commons.logging.Log;
 import de.se_rwth.commons.logging.LogStub;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.net.JarURLConnection;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
+import java.util.jar.JarFile;
 
-@Disabled
 public class StreamTypeTest {
 
   @BeforeEach
-  public void init() throws URISyntaxException, IOException {
+  public void init() throws IOException {
     LogStub.init();
     Log.enableFailQuick(false);
 
     OOSymbolsMill.reset();
     OOSymbolsMill.init();
     BasicSymbolsMill.initializePrimitives();
-    // workaround to get library path working in emf
-    Path symbolsPath = new File(FileReaderWriter.getResource(getClass().getClassLoader(), "symbols/streams/Stream.symtabdefinitionsym").get().toURI()).toPath().getParent();
-    BasicSymbolsMill.globalScope().getSymbolPath().addEntry(symbolsPath);
-    // workaround as CDs not known
+
+    // workaround to get library path working (in emf)
+    URL streamURL = StreamTypeTest.class.getClassLoader().getResource("Stream.symtabdefinitionsym");
+    Assertions.assertNotNull(streamURL);
+    // need to be expanded if ever false (could be a folder?)
+    Assertions.assertEquals("jar", streamURL.getProtocol());
+    JarURLConnection urlConnection = (JarURLConnection) streamURL.openConnection();
+    JarFile jar = urlConnection.getJarFile();
+    Path jarPath = Path.of(jar.getName());
+    BasicSymbolsMill.globalScope().getSymbolPath().addEntry(jarPath);
+
+    // workaround as CDs are not known
     BasicSymbolsMill.globalScope().putSymbolDeSer("de.monticore.cdbasis._symboltable.CDTypeSymbol", new OOTypeSymbolDeSer());
     BasicSymbolsMill.globalScope().putSymbolDeSer("de.monticore.cd4codebasis._symboltable.CDMethodSignatureSymbol", new MethodSymbolDeSer());
   }
@@ -54,9 +63,9 @@ public class StreamTypeTest {
   }
 
   @Test
-  public void resolveStaticEmpty() {
-    MethodSymbol method = resolveMethod("Stream.emptyStream");
-    Assertions.assertTrue(method.getParameterList().isEmpty());
+  public void resolveStaticRepeat() {
+    MethodSymbol method = getMethodSymbol("Stream.repeat");
+    Assertions.assertEquals(2, method.getParameterList().size());
     assertIsStreamWithTypeVar(method.getType());
     Assertions.assertTrue(method.isIsStatic());
     List<TypeVarSymbol> typeVars = method.getSpannedScope().getLocalTypeVarSymbols();
@@ -65,18 +74,8 @@ public class StreamTypeTest {
   }
 
   @Test
-  public void resolveStreamMethodAppendFirst() {
-    MethodSymbol method = resolveMethod("Stream.appendFirst");
-    Assertions.assertTrue(method.isIsStatic());
-    Assertions.assertEquals(2, method.getParameterList().size());
-    Assertions.assertTrue(method.getParameterList().get(0).getType().isTypeVariable());
-    assertIsStreamWithTypeVar(method.getParameterList().get(1).getType());
-    assertIsStreamWithTypeVar(method.getType());
-  }
-
-  @Test
   public void resolveStreamMethodLen() {
-    MethodSymbol method = resolveMethod("Stream.len");
+    MethodSymbol method = getMethodSymbol("Stream.len");
     Assertions.assertEquals(0, method.getParameterList().size());
     Assertions.assertTrue(method.getEnclosingScope().getSpanningSymbol() instanceof TypeSymbol);
     Assertions.assertEquals("Stream", method.getEnclosingScope().getSpanningSymbol().getName());
@@ -85,46 +84,49 @@ public class StreamTypeTest {
 
   @Test
   public void resolveStreamFunctions() {
-    resolveCommonMemberMethods("Stream");
     resolveCommonMemberMethods("EventStream");
     resolveCommonMemberMethods("SyncStream");
     resolveCommonMemberMethods("ToptStream");
+    resolveCommonMemberMethods("UntimedStream");
+
+    resolveStaticMethods("Stream");
+    resolveStaticMethods("EventStream");
+    resolveStaticMethods("SyncStream");
+    resolveStaticMethods("ToptStream");
   }
 
   protected void resolveStaticMethods(String streamType) {
-    resolveMethod(streamType + ".emptyStream");
-    resolveMethod(streamType + ".appendFirst");
-    resolveMethod(streamType + ".conc");
-    resolveMethod(streamType + ".repeat");
-    resolveMethod(streamType + ".iterate");
+    testResolveMethod(streamType + ".repeat");
+    testResolveMethod(streamType + ".iterate");
+    testResolveMethod(streamType + ".projFst");
+    testResolveMethod(streamType + ".projSnd");
   }
 
   protected void resolveCommonMemberMethods(String streamType) {
-    resolveMethod(streamType + ".len");
-    resolveMethod(streamType + ".first");
-    resolveMethod(streamType + ".dropFirst");
-    resolveMethod(streamType + ".nth");
-    resolveMethod(streamType + ".take");
-    resolveMethod(streamType + ".drop");
-    resolveMethod(streamType + ".times");
-    resolveMethod(streamType + ".map");
-    resolveMethod(streamType + ".filter");
-    resolveMethod(streamType + ".takeWhile");
-    resolveMethod(streamType + ".dropWhile");
-    resolveMethod(streamType + ".rcDups");
+    testResolveMethod(streamType + ".len");
+    testResolveMethod(streamType + ".first");
+    testResolveMethod(streamType + ".dropFirst");
+    testResolveMethod(streamType + ".nth");
+    testResolveMethod(streamType + ".take");
+    testResolveMethod(streamType + ".times");
+    testResolveMethod(streamType + ".map");
+    testResolveMethod(streamType + ".filter");
+    testResolveMethod(streamType + ".takeWhile");
+    testResolveMethod(streamType + ".dropWhile");
+    testResolveMethod(streamType + ".rcDups");
   }
 
   @Test
   public void resolveTimedStreamMethods() {
-    resolveMethod("EventStream.delay");
-    resolveMethod("EventStream.mapSlice");
-    resolveMethod("EventStream.rougherTime");
-    resolveMethod("EventStream.finerTime");
-    resolveMethod("EventStream.sync");
-    resolveMethod("EventStream.topt");
-    resolveMethod("EventStream.untimed");
-    resolveMethod("ToptStream.sizeEps");
-    resolveMethod("SyncStream.event");
+    testResolveMethod("EventStream.delay");
+    testResolveMethod("EventStream.mapSlice");
+    testResolveMethod("EventStream.rougherTime");
+    testResolveMethod("EventStream.finerTime");
+    testResolveMethod("EventStream.sync");
+    testResolveMethod("EventStream.topt");
+    testResolveMethod("EventStream.untimed");
+    testResolveMethod("ToptStream.sizeEmptyTimeslices");
+    testResolveMethod("SyncStream.event");
   }
 
   @Test
@@ -140,10 +142,18 @@ public class StreamTypeTest {
     Assertions.assertTrue(((SymTypeOfGenerics) type).getArgument(0).isTypeVariable());
   }
 
-  protected MethodSymbol resolveMethod(String name) {
-    Optional<MethodSymbol> emptyOpt = OOSymbolsMill.globalScope().resolveMethod(name);
-    Assertions.assertTrue(emptyOpt.isPresent());
-    Assertions.assertTrue(emptyOpt.get().isIsMethod());
-    return emptyOpt.get();
+  protected void testResolveMethod(String name) {
+    Optional<SymTypeExpression> methodType = WithinScopeBasicSymbolsResolver.resolveNameAsExpr(BasicSymbolsMill.globalScope(), name);
+    Assertions.assertTrue(methodType.isPresent(), name);
+    Assertions.assertTrue(methodType.get().isFunctionType()
+        || methodType.get().isIntersectionType()
+    );
   }
+
+  protected MethodSymbol getMethodSymbol(String name) {
+    testResolveMethod(name);
+    Optional<SymTypeExpression> methodType = WithinScopeBasicSymbolsResolver.resolveNameAsExpr(BasicSymbolsMill.globalScope(), name);
+    return (MethodSymbol) methodType.get().asFunctionType().getSymbol();
+  }
+
 }
