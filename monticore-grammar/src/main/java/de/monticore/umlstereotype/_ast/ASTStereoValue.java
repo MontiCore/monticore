222cf51bf6754965f31d849fde344d6dd0f404c3
Tom Bursch <tombursch@gmail.com>
2025-03-13 11:27:41 +0100

    Cleanup

--- monticore-grammar/src/main/java/de/monticore/umlstereotype/_ast/ASTStereoValue.java
+++ monticore-grammar/src/main/java/de/monticore/umlstereotype/_ast/ASTStereoValue.java
@@ -1,23 +1,51 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.umlstereotype._ast;
 
-public  class ASTStereoValue extends ASTStereoValueTOP {
+import de.monticore.expressions.expressionsbasis.ExpressionsBasisMill;
+import de.monticore.literals.mccommonliterals.MCCommonLiteralsMill;
+import de.monticore.literals.mccommonliterals._ast.ASTStringLiteral;
+import de.se_rwth.commons.logging.Log;
 
-  @Override
-  public boolean equalAttributes(Object o) {
-    ASTStereoValue comp;
-    if ((o instanceof ASTStereoValue)) {
-      comp = (ASTStereoValue) o;
-    } else {
-      return false;
-    }
+import java.util.Optional;
 
-    // Don't check derived attributes
-    // comparing name
-    if ( (this.name == null && comp.name != null)
-            || (this.name != null && !this.name.equals(comp.name)) ) {
-      return false;
+public class ASTStereoValue extends ASTStereoValueTOP {
+
+  protected Optional<ASTStringLiteral> text = Optional.empty();
+
+  public boolean isPresentText() {
+    if (text.isEmpty() && isPresentExpression() &&
+      ExpressionsBasisMill
+        .typeDispatcher()
+        .isExpressionsBasisASTLiteralExpression(expression.get())
+      && MCCommonLiteralsMill.typeDispatcher()
+      .isMCCommonLiteralsASTStringLiteral(ExpressionsBasisMill
+        .typeDispatcher().asExpressionsBasisASTLiteralExpression(expression.get())
+        .getLiteral())) {
+      text = Optional.of(MCCommonLiteralsMill.typeDispatcher()
+        .asMCCommonLiteralsASTStringLiteral(ExpressionsBasisMill.typeDispatcher()
+          .asExpressionsBasisASTLiteralExpression(expression.get())
+          .getLiteral()));
     }
-    return true;
+    return text.isPresent();
+  }
+
+  public ASTStringLiteral getText() {
+    if (isPresentText()) {
+      return this.text.get();
+    }
+    Log.error("0xA7003x52066 get for Text can't return a value. Attribute is empty.");
+    // Normally this statement is not reachable
+    throw new IllegalStateException();
+  }
+
+  public String getValue() {
+    if (isPresentText()) {
+      return getText().getValue();
+    }
+    return "";
+  }
+
+  public void setContent(String content) {
+    text = Optional.of(MCCommonLiteralsMill.stringLiteralBuilder().setSource("\"" + content + "\"").build());
   }
 }
