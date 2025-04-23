dd5386b0db69b183b07630e567c17dfe1f9e55b7
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2025-04-23 16:43:05 +0200

    MCAssertions extended message capabilities + assertNoFindings

--- monticore-runtime-tests/src/main/java/de/monticore/runtime/junit/jupyter/MCAssertions.java
+++ monticore-runtime-tests/src/main/java/de/monticore/runtime/junit/jupyter/MCAssertions.java
@@ -29,8 +29,23 @@ public class MCAssertions {
    * @return returns the found Finding
    * @see MCAssertions#assertHasFinding(Predicate, String)
    */
-  public static Finding assertHasFindingStartsWith(String expectedPrefix, String message) {
+  public static Finding assertHasFindingStartingWith(String expectedPrefix, String message) {
     return assertHasFinding(f -> f.getMsg().startsWith(expectedPrefix), message);
+  }
+
+  /**
+   * Asserts that at least one Finding starts with the expected prefix
+   * and removes that Finding from the Log
+   *
+   * @param expectedPrefix the expected prefix
+   * @return returns the found Finding
+   * @see MCAssertions#assertHasFinding(Predicate)
+   */
+  public static Finding assertHasFindingStartingWith(String expectedPrefix) {
+    return assertHasFindingStartingWith(expectedPrefix,
+        "Expected a Log-Finding with prefix " + expectedPrefix +
+            ", but did not find any"
+    );
   }
 
   /**
@@ -42,13 +57,29 @@ public class MCAssertions {
    * @return returns the list of found Findings
    * @see MCAssertions#assertHasFindings(Predicate, String)
    */
-  public static Collection<Finding> assertHasFindingsStartsWith(String expectedPrefix, String message) {
+  public static Collection<Finding> assertHasFindingsStartingWith(String expectedPrefix, String message) {
     return assertHasFindings(f -> f.getMsg().startsWith(expectedPrefix), message);
   }
 
   /**
+   * Asserts that at least one Finding starts with the expected prefix
+   * and removes all matching Findings from the Log
+   *
+   * @param expectedPrefix the expected prefix
+   * @return returns the list of found Findings
+   * @see MCAssertions#assertHasFinding(Predicate)
+   */
+  public static Collection<Finding> assertHasFindingsStartingWith(String expectedPrefix) {
+    return assertHasFindingsStartingWith(expectedPrefix,
+        "Expected Log-Findings with prefix " + expectedPrefix +
+            ", but did not find any"
+    );
+  }
+
+  /**
    * Asserts that at least one Finding matches the predicate
-   * and removes that Finding from the Log
+   * and removes that Finding from the Log.
+   * If multiple Findings match, only the first of them will be removed.
    *
    * @param predicate the predicate
    * @param message   the message to fail with iff no finding was found
@@ -63,7 +94,22 @@ public class MCAssertions {
         return f;
       }
     }
-    return failFindings(message);
+    return failAndPrintFindings(message);
+  }
+
+  /**
+   * Asserts that at least one Finding matches the predicate
+   * and removes that Finding from the Log.
+   * If multiple Findings match, only the first of them will be removed.
+   *
+   * @param predicate the predicate
+   * @return returns the found Finding
+   */
+  public static Finding assertHasFinding(Predicate<Finding> predicate) {
+    return assertHasFinding(predicate,
+        "Expected a Log-Finding matching given predicate" +
+            ", but did not find any"
+    );
   }
 
   /**
@@ -85,20 +131,72 @@ public class MCAssertions {
       }
     }
     if (matchingFindings.isEmpty())
-      return failFindings(message);
+      return failAndPrintFindings(message);
     return matchingFindings;
   }
 
   /**
+   * Asserts that at least one Finding matches the predicate
+   * and removes all matching Findings from the Log
+   *
+   * @param predicate the predicate
+   * @return returns the list of found Findings
+   */
+  public static Collection<Finding> assertHasFindings(Predicate<Finding> predicate) {
+    return assertHasFindings(predicate,
+        "Expected Log-Findings matching given predicate" +
+            ", but did not find any"
+    );
+  }
+
+  /**
+   * Asserts that no findings are present.
+   *
+   * @param message the message to fail with iff findings were found
+   */
+  public static void assertNoFindings(String message) {
+    if (!Log.getFindings().isEmpty()) {
+      failAndPrintFindings(message);
+    }
+  }
+
+  /**
+   * Asserts that no findings are present.
+   */
+  public static void assertNoFindings() {
+    assertNoFindings("Encountered Log-Findings while expecting none");
+  }
+
+  /**
    * Fails a test with the given failure message.
-   * See Javadoc for {@link Assertions#fail(String, Throwable)} for an explanation of this method's generic return type V.
+   * Additionally, Lists the state of Log-Findings.
+   * See Javadoc for {@link Assertions#fail(String, Throwable)}
+   * for an explanation of this method's generic return type V.
    *
    * @param message message to print
    * @return nothing
    */
-  public static <V> V failFindings(String message) {
-    return Assertions.fail(message + ", got findings " +
-            Log.getFindings().stream().map(Finding::toString)
-                    .collect(Collectors.joining(System.lineSeparator())));
+  public static <V> V failAndPrintFindings(String message) {
+    StringBuilder messageWithFindings = new StringBuilder();
+    if (!message.isBlank()) {
+      messageWithFindings.append(message);
+    }
+    else {
+      messageWithFindings.append("Failed (no reason stated)");
+    }
+    messageWithFindings.append(System.lineSeparator());
+    if (Log.getFindings().isEmpty()) {
+      messageWithFindings.append("Got no Log-Findings.");
+    }
+    else {
+      messageWithFindings.append("Got Log-Findings:");
+      messageWithFindings.append(System.lineSeparator());
+      messageWithFindings.append(Log.getFindings().stream()
+          .map(Finding::buildMsg)
+          .collect(Collectors.joining(System.lineSeparator()))
+      );
+    }
+    return Assertions.fail(messageWithFindings.toString());
   }
+
 }
