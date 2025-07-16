7801a7a59c32059b1bff328d2db8d3d3b4175371
Alex Lüpges <luepges@se-rwth.de>
2025-07-16 14:15:26 +0200

    WIP breaking change detection

---
+++ prepare-next-release/applyPatches.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+# (c) https://github.com/MontiCore/monticore
+# Script for applying all patches to check if we are about to break the next release of MontiCore
+
+# Start by switching to a new branch (using the suffix -crystal-ball)
+currentBranch=`git rev-parse --abbrev-ref HEAD`
+git checkout -b $currentBranch-crystal-ball
+if [ "$?" != "0" ]; then
+    echo "Failed to switch to new branch $currentBranch-crystal-ball"
+    exit 1
+fi
+# And apply all patches
+git am --abort >/dev/null 2>&1
+git am --3way prepare-next-release/*.patch
+if [ "$?" != "0" ]; then
+    echo "Failed to apply the patches."
+    echo "Please consult the 'PreparingTheNextRelease' documentation "
+    exit 1
+else
+    echo "Cleanly applied patches"
+fi
---
+++ prepare-next-release/buildPatches.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# (c) https://github.com/MontiCore/monticore
+# Script for creating new patch files
+
+
+currentBranch=`git rev-parse --abbrev-ref HEAD`
+originalBranch="${currentBranch/-crystal-ball/}"
+
+echo "Creating patch difference to $originalBranch"
+git format-patch --no-stat --minimal -N -o .\prepare-next-release\ $originalBranch --
+if [ "$?" != "0" ]; then
+    echo "Failed to create patch differences"
+    exit 1
+fi
+echo "Switching back to branch: $originalBranch"
+git checkout $originalBranch
