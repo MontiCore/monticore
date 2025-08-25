744f432575d1f3dede507042c6087d38638b3fd8
Hendrik7889 <44064629+Hendrik7889@users.noreply.github.com>
2025-08-25 13:26:42 +0200

    initial steps

---
+++ monticore-test/it/src/test/java/mc/feature/symboltable/SymbolsWithSubKindsTest.java
@@ -0,0 +1,24 @@
+/* (c) https://github.com/MontiCore/monticore */
+
+package mc.feature.symboltable;
+
+import de.se_rwth.commons.logging.Log;
+import de.se_rwth.commons.logging.LogStub;
+import mc.GeneratorIntegrationsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class SymbolsWithSubKindsTest extends GeneratorIntegrationsTest {
+
+    @BeforeEach
+    public void before() {
+        LogStub.init();
+        Log.enableFailQuick(false);
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
