6f5da6e92b99c0c1794aa14e0e3a193a63b419e1
Hendrik7889 <44064629+Hendrik7889@users.noreply.github.com>
2025-09-11 13:53:06 +0200

    finished up test

--- monticore-test/it/src/test/java/mc/feature/symboltable/SymbolsWithSubKindsTest.java
+++ monticore-test/it/src/test/java/mc/feature/symboltable/SymbolsWithSubKindsTest.java
@@ -11,7 +11,13 @@ import mc.feature.symboltable.notopscope._ast.ASTFoo;
 import mc.feature.symboltable.notopscope._parser.NoTopScopeParser;
 import mc.feature.symboltable.notopscope._symboltable.INoTopScopeArtifactScope;
 import mc.feature.symboltable.notopscope._symboltable.INoTopScopeGlobalScope;
+import mc.feature.symboltable.symbolswithsubkinds.SymbolsWithSubKindsMill;
 import mc.feature.symboltable.symbolswithsubkinds._ast.*;
+import mc.feature.symboltable.symbolswithsubkinds._parser.SymbolsWithSubKindsParser;
+import mc.feature.symboltable.symbolswithsubkinds._symboltable.ISymbolsWithSubKindsArtifactScope;
+import mc.feature.symboltable.symbolswithsubkinds._symboltable.ISymbolsWithSubKindsGlobalScope;
+import mc.feature.symboltable.symbolswithsubkinds._symboltable.ISymbolsWithSubKindsScope;
+import mc.feature.symboltable.symbolswithsubkinds._symboltable.SymbolsWithSubKindsScope;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,26 +33,56 @@ public class SymbolsWithSubKindsTest extends GeneratorIntegrationsTest {
     public void before() {
         LogStub.init();
         Log.enableFailQuick(false);
+        SymbolsWithSubKindsMill.reset();
+        SymbolsWithSubKindsMill.init();
     }
 
     @Test
     public void testIScopeSpanningSymbol() throws IOException {
         // parse model
-        NoTopScopeParser scopeAttributesParser = new NoTopScopeParser();
-        Optional<ASTFoo> astSup = scopeAttributesParser.parse("src/test/resources/mc/feature/symboltable/SymbolsWithSubKinds.st.st");
+        SymbolsWithSubKindsParser scopeAttributesParser = new SymbolsWithSubKindsParser();
+        Optional<ASTZoo> astSup = scopeAttributesParser.parse("src/test/resources/mc/feature/symboltable/SymbolsWithSubKinds.st");
         Assertions.assertFalse(scopeAttributesParser.hasErrors());
         Assertions.assertTrue(astSup.isPresent());
 
         // create symboltable
-        INoTopScopeGlobalScope globalScope = NoTopScopeMill.globalScope();
+        ISymbolsWithSubKindsGlobalScope globalScope = SymbolsWithSubKindsMill.globalScope();
         globalScope.setFileExt("st");
         globalScope.getSymbolPath().addEntry(Paths.get("src/test/resources/mc/feature/symboltable"));
 
-        INoTopScopeArtifactScope scope = NoTopScopeMill
+        ISymbolsWithSubKindsScope scope = SymbolsWithSubKindsMill
                 .scopesGenitorDelegator().createFromAST(astSup.get());
 
+        Assertions.assertSame(1, scope.getSubScopes().size());
+        ISymbolsWithSubKindsScope spannedScope =  scope.getSubScopes().get(0);
+        Assertions.assertInstanceOf(SymbolsWithSubKindsScope.class , spannedScope);
 
+        // check if the methods return the correct number of symbols
+        // see monticore-test/it/src/test/resources/mc/feature/symboltable/SymbolsWithSubKinds.st
+        Assertions.assertSame(1, spannedScope.getFordSymbolsWithSubKinds().size());
+        Assertions.assertSame(1, spannedScope.getVolvoSymbolsWithSubKinds().size());
+        Assertions.assertSame(3,spannedScope.getTruckSymbolsWithSubKinds().size());
+        Assertions.assertSame(1,spannedScope.getBeatleSymbolsWithSubKinds().size());
+        Assertions.assertSame(1, spannedScope.getMazdaSymbolsWithSubKinds().size());
+        Assertions.assertSame(3,spannedScope.getCabrioletSymbolsWithSubKinds().size());
+        Assertions.assertSame(7,spannedScope.getCarSymbolsWithSubKinds().size());
+        Assertions.assertSame(1, spannedScope.getDogSymbolsWithSubKinds().size());
+        Assertions.assertSame(1, spannedScope.getCatSymbolsWithSubKinds().size());
+        Assertions.assertSame(1, spannedScope.getFishSymbolsWithSubKinds().size());
+        Assertions.assertSame(4,spannedScope.getAnimalSymbolsWithSubKinds().size());
+        Assertions.assertSame(11, spannedScope.getZooSymbolsWithSubKinds().size());
 
+        Assertions.assertEquals("carFordName", spannedScope.getFordSymbolsWithSubKinds().values().get(0).getName());
+        Assertions.assertEquals("carVolvoName", spannedScope.getVolvoSymbolsWithSubKinds().values().get(0).getName());
+        Assertions.assertEquals("carTruckName", spannedScope.getTruckSymbolsWithSubKinds().values().stream().filter(m -> m.getAstNode().getClass().equals(ASTTruck.class)).findAny().get().getName());
+        Assertions.assertEquals("carMazdaName", spannedScope.getMazdaSymbolsWithSubKinds().values().get(0).getName());
+        Assertions.assertEquals("carBeatleName", spannedScope.getBeatleSymbolsWithSubKinds().values().get(0).getName());
+        Assertions.assertEquals("carCabrioletName", spannedScope.getCabrioletSymbolsWithSubKinds().values().stream().filter(m -> m.getAstNode().getClass().equals(ASTCabriolet.class)).findAny().get().getName());
+        Assertions.assertEquals("carCarName", spannedScope.getCarSymbolsWithSubKinds().values().stream().filter(m -> m.getAstNode().getClass().equals(ASTCar.class)).findAny().get().getName());
+        Assertions.assertEquals("animalDogName", spannedScope.getDogSymbolsWithSubKinds().values().get(0).getName());
+        Assertions.assertEquals("animalCatName", spannedScope.getCatSymbolsWithSubKinds().values().get(0).getName());
+        Assertions.assertEquals("animalFishName", spannedScope.getFishSymbolsWithSubKinds().values().get(0).getName());
+        Assertions.assertEquals("animalAnimalName", spannedScope.getAnimalSymbolsWithSubKinds().values().stream().filter(m -> m.getAstNode().getClass().equals(ASTAnimal.class)).findAny().get().getName());
     }
 
     /**
@@ -55,28 +91,30 @@ public class SymbolsWithSubKindsTest extends GeneratorIntegrationsTest {
     @SuppressWarnings("unused")
     @Test
     public void test() {
-        ASTZoo zoo = new ASTZooBuilder().build();
-        Assertions.assertTrue(zoo instanceof ASTSymbolsWithSubKindsNode);
-        ASTCar car = new ASTCarBuilder().build();
-        Assertions.assertTrue(car instanceof ASTVehicle);
-        ASTCabriolet cabriolet = new ASTCabrioletBuilder().build();
-        Assertions.assertTrue(cabriolet instanceof ASTCar);
-        ASTBeatle beatle = new ASTBeatleBuilder().build();
-        Assertions.assertTrue(beatle instanceof ASTCabriolet);
-        ASTMazda mazda = new ASTMazdaBuilder().build();
-        Assertions.assertTrue(mazda instanceof ASTCabriolet);
-        ASTTruck truck = new ASTTruckBuilder().build();
-        Assertions.assertTrue(truck instanceof ASTCar);
-        ASTFord ford = new ASTFordBuilder().build();
-        Assertions.assertTrue(ford instanceof ASTTruck);
-        ASTVolvo volvo = new ASTVolvoBuilder().build();
-        Assertions.assertTrue(volvo instanceof ASTTruck);
-        ASTAnimal animal = new ASTAnimalBuilder().build();
-        Assertions.assertTrue(animal instanceof ASTLeg);
-        ASTDog dog = new ASTDogBuilder().build();
-        Assertions.assertTrue(dog instanceof ASTAnimal);
-        ASTCat cat = new ASTCatBuilder().build();
-        Assertions.assertTrue(cat instanceof ASTAnimal);
+        ASTZoo zoo = new ASTZooBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTSymbolsWithSubKindsNode.class, zoo);
+        ASTCar car = new ASTCarBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTVehicle.class, car);
+        Assertions.assertInstanceOf(ASTZoo.class, car);
+        ASTCabriolet cabriolet = new ASTCabrioletBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTCar.class, cabriolet);
+        ASTBeatle beatle = new ASTBeatleBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTCabriolet.class, beatle);
+        ASTMazda mazda = new ASTMazdaBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTCabriolet.class, mazda);
+        ASTTruck truck = new ASTTruckBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTCar.class, truck);
+        ASTFord ford = new ASTFordBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTTruck.class, ford);
+        ASTVolvo volvo = new ASTVolvoBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTTruck.class, volvo);
+        ASTAnimal animal = new ASTAnimalBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTLeg.class, animal);
+        Assertions.assertInstanceOf(ASTZoo.class, animal);
+        ASTDog dog = new ASTDogBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTAnimal.class, dog);
+        ASTCat cat = new ASTCatBuilder().uncheckedBuild();
+        Assertions.assertInstanceOf(ASTAnimal.class, cat);
         Assertions.assertTrue(Log.getFindings().isEmpty());
     }
 }
