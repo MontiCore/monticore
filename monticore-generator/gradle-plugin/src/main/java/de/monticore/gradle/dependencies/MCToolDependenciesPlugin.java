9a68938fc0d24fe3db0f1d42785f432d38bf8f15
Alex Lüpges <luepges@se-rwth.de>
2025-11-10 16:53:48 +0100

    improve comments

--- monticore-generator/gradle-plugin/src/main/java/de/monticore/gradle/dependencies/MCToolDependenciesPlugin.java
+++ monticore-generator/gradle-plugin/src/main/java/de/monticore/gradle/dependencies/MCToolDependenciesPlugin.java
@@ -14,16 +14,8 @@ import java.util.Properties;
 
 
 /**
- * Creates "grammar" configurations for each sourceSet,
- *  which allows depending on MontiCore grammars and their artifacts.
- *  Example usage:
- * ```
- * dependencies {
- * grammar("de.monticore:grammar:$mc_version")
- * testGrammar("de.monticore.lang:sd-language:$mc_version")
- * }
- * ```
- * Internally, further configurations are used.
+ * Creates "mcTool" configurations for the monticore generator tool.
+ * The MCGenTasks run the tool from this configuration
  */
 public class MCToolDependenciesPlugin implements Plugin<Project> {
 
@@ -34,7 +26,7 @@ public class MCToolDependenciesPlugin implements Plugin<Project> {
     project.getPluginManager().apply("java-library");
 
 
-    // add a new configuration in which the umlp-tool (.jar) is loaded
+    // add a new configuration in which the monticore-generator (.jar) is loaded
     //  (this avoids loading the tool+all its dependencies into the shared plugin-classpath)
     var toolConfig = project.getConfigurations().maybeCreate(MC_CONFIG_TOOL);
     // Load the buildInfo.properties file (containing the current version)
@@ -48,13 +40,13 @@ public class MCToolDependenciesPlugin implements Plugin<Project> {
             buildInfo.getProperty("version"),
             "Can not find version in buildInfo.properties"
     );
-    // Add the dependencies to the toolConfig
+    // Add the (default) dependencies to the toolConfig
     toolConfig.setCanBeResolved(true);
     toolConfig.defaultDependencies(dependencies -> {
       dependencies.add(project.getDependencies().create("de.monticore:monticore-generator:" + version));
     });
     toolConfig.attributes(it -> {
-      // Do not use the shadowed bundling variant
+      // Do not use the shadowed bundling variant (by default)
       it.attribute(Bundling.BUNDLING_ATTRIBUTE, project.getObjects().named(Bundling.class, Bundling.EXTERNAL));
     });
 
