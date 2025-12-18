14dd21000f0177b01773d4f4d0fc32a224a33ba5
Vanol Nguemo Tadoum <nguemovanol@gmail.com>
2025-12-18 16:12:28 +0100

    clean

--- monticore-grammar/src/test/java/de/monticore/types/check/SynthesizeComponentFromMCSimpleGenericTypesTest.java
+++ monticore-grammar/src/test/java/de/monticore/types/check/SynthesizeComponentFromMCSimpleGenericTypesTest.java
@@ -32,14 +32,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 public class SynthesizeComponentFromMCSimpleGenericTypesTest extends AbstractMCTest {
 
