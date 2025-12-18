f8bb79b5cfba039f087dd297729c545174a0648f
Vanol Nguemo Tadoum <nguemovanol@gmail.com>
2025-12-18 16:14:12 +0100

    clean

--- monticore-grammar/src/test/java/de/monticore/types/check/SynthesizeComponentFromMCSimpleGenericTypesTest.java
+++ monticore-grammar/src/test/java/de/monticore/types/check/SynthesizeComponentFromMCSimpleGenericTypesTest.java
@@ -47,7 +47,6 @@ public class SynthesizeComponentFromMCSimpleGenericTypesTest extends AbstractMCT
     Log.enableFailQuick(false);
     Log.clearFindings();
 
-
     BasicSymbolsMill.initializePrimitives();
     BasicSymbolsMill.initializeString();
   }
