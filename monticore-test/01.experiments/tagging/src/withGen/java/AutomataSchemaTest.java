7fe13e215d8afa678d54cef8db9193cc3e238501
Janik Rapp <rapp@se-rwth.de>
2025-12-25 23:37:03 +0100

    migrate tagging tests to junit 5

--- monticore-test/01.experiments/tagging/src/withGen/java/AutomataSchemaTest.java
+++ monticore-test/01.experiments/tagging/src/withGen/java/AutomataSchemaTest.java
@@ -8,29 +8,35 @@ import automatatagdefinition._cocos.AutomataTagDefinitionCoCoChecker;
 import automatatagschema.AutomataTagSchemaMill;
 import automatatagschema._symboltable.AutomataTagSchemaSymbols2Json;
 import automatatagschema._symboltable.IAutomataTagSchemaArtifactScope;
+import com.google.common.base.Preconditions;
 import com.google.common.io.Files;
 import de.monticore.tagging.tags._ast.ASTTagUnit;
 import de.monticore.tagging.tagschema._ast.ASTTagSchema;
 import de.se_rwth.commons.logging.Log;
 import de.se_rwth.commons.logging.LogStub;
 import org.apache.commons.io.FileUtils;
-import org.junit.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class AutomataSchemaTest {
   protected static ASTAutomaton model;
 
+  @TempDir
   static File tempDir;
 
-  @BeforeClass
+  @BeforeAll
   public static void prepare() throws Exception {
-    tempDir = java.nio.file.Files.createTempDirectory("AutomataSchemaTest").toFile();
-    if (!tempDir.exists())
-      tempDir.mkdirs();
     LogStub.init();
     Log.enableFailQuick(false);
 
@@ -53,7 +59,7 @@ public class AutomataSchemaTest {
         IAutomataTagSchemaArtifactScope artifactScope = AutomataTagSchemaMill.scopesGenitorDelegator().createFromAST(schemaOpt.get());
         String symFile = new File(new File(tempDir, "schema"), Files.getNameWithoutExtension(f.getName()) + ".sym").toString();
         symbols2Json.store(artifactScope, symFile);
-        Assert.assertEquals("TagSchema " + f + " logged errors",0, Log.getErrorCount());
+        assertEquals(0, Log.getErrorCount(), "TagSchema " + f + " logged errors");
       } else {
         Log.clearFindings();
         Log.warn("Failed to load TagSchema " + f);
@@ -66,133 +72,133 @@ public class AutomataSchemaTest {
 
     // Finally, switch to the TagDef language
     AutomataTagDefinitionMill.init();
-    Assert.assertEquals(0, Log.getErrorCount());
+    assertEquals(0, Log.getErrorCount());
   }
 
-  @Before
+  @BeforeEach
   public void beforeEach() {
     Log.clearFindings();
   }
 
   @Test
   public void testResolve() throws IOException {
-    Assert.assertTrue(AutomataTagSchemaMill.globalScope().resolveTagSchema("AutomataSchema").isPresent());
-    Assert.assertEquals(0, Log.getErrorCount());
+    assertTrue(AutomataTagSchemaMill.globalScope().resolveTagSchema("AutomataSchema").isPresent());
+    assertEquals(0, Log.getErrorCount());
   }
 
   @Test
   public void testValidTags1() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/Simple.tags");
-    Assert.assertEquals(0, Log.getErrorCount());
+    assertEquals(0, Log.getErrorCount());
   }
 
   @Test
   public void testSpotRootWithSimpleInsteadOfValued() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidTags1.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNWithInvalidSimpleTag() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidTags2.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNHWithInvalidSimpleTag() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidTags3.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNHWithInvalidValuedTag() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidTags4.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNWithinWithInvalidSimpleTag() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidTags5.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testComplexMissing() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidTags7.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testComplexTooMany() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidTags8.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
 
   @Test
   public void testFQNWithinWithInvalidPrivateTag() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidTagsPrivate.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNComplexMissingInner() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidComplexTags1.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNComplexTooManyInner() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidComplexTags2.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNComplexTypoInner() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidComplexTags3.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNComplexNotANumber() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidComplexTags4.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNComplexNotABoolean() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidComplexTags5.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNComplexUnknownInnerComplex() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidComplexTags6.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNComplexInvalidInnerComplex() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidComplexTags7.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNPattern() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidPatternTags1.tags");
-    Assert.assertEquals(1, Log.getErrorCount());
+    assertEquals(1, Log.getErrorCount());
   }
 
   @Test
   public void testFQNPatternWithin() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/InvalidPatternTags2.tags");
-    Assert.assertEquals(2, Log.getErrorCount());
+    assertEquals(2, Log.getErrorCount());
   }
 
   @Test
   public void testFQNNonConformingTags() throws IOException {
     testTagDefCoCoChecker("src/test/resources/models/NonConformingTags.tags");
-    Assert.assertEquals(0, Log.getErrorCount());
+    assertEquals(0, Log.getErrorCount());
   }
 
   protected void testTagDefCoCoChecker(String file) throws IOException {
@@ -203,12 +209,5 @@ public class AutomataSchemaTest {
     AutomataTagDefinitionMill.scopesGenitorDelegator().createFromAST(n.get());
 
     coCoChecker.checkAll(n.get());
-  }
-
-  @AfterClass
-  public static void cleanUp() throws IOException {
-    if (tempDir.exists()) {
-      FileUtils.deleteDirectory(tempDir);
-    }
   }
 }
--- monticore-test/01.experiments/tagging/src/withGen/java/TagSchemaSerializationTest.java
+++ monticore-test/01.experiments/tagging/src/withGen/java/TagSchemaSerializationTest.java
@@ -6,6 +6,7 @@ import automatatagdefinition.AutomataTagDefinitionMill;
 import automatatagdefinition._cocos.AutomataTagDefinitionCoCoChecker;
 import automatatagschema._symboltable.AutomataTagSchemaSymbols2Json;
 import automatatagschema._symboltable.IAutomataTagSchemaArtifactScope;
+import com.google.common.base.Preconditions;
 import de.monticore.fqn.fqnenhancedautomata._symboltable.IFQNEnhancedAutomataArtifactScope;
 import de.monticore.fqn.fqnenhancedautomatatagschema.FQNEnhancedAutomataTagSchemaMill;
 import de.monticore.fqn.fqnenhancedautomatatagschema._symboltable.FQNEnhancedAutomataTagSchemaSymbols2Json;
@@ -15,20 +16,21 @@ import de.monticore.tagging.tags._ast.ASTTagUnit;
 import de.monticore.tagging.tagschema._ast.ASTTagSchema;
 import de.se_rwth.commons.logging.Log;
 import de.se_rwth.commons.logging.LogStub;
-import org.junit.jupiter.api.Assertions;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TagSchemaSerializationTest {
   protected static ASTAutomaton model;
 
-  @BeforeClass
+  @BeforeAll
   public static void prepare() throws Exception {
     LogStub.init();
     Log.enableFailQuick(false);
@@ -47,7 +49,7 @@ public class TagSchemaSerializationTest {
 
   }
 
-  @Before
+  @BeforeEach
   public void beforeEach() {
     Log.clearFindings();
   }
@@ -71,7 +73,7 @@ public class TagSchemaSerializationTest {
 
       String serializedCopy = schemaSymbols2Json.serialize(copy);
 
-      Assert.assertEquals(serialized, serializedCopy);
+      assertEquals(serialized, serializedCopy);
 
     }
   }
