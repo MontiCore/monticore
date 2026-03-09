33567514fe63aaac6b9dbeb68b557de98d55ee20
Hendrik7889 <44064629+Hendrik7889@users.noreply.github.com>
2026-03-09 11:12:45 +0100

    add functions (lcoal vars are not working

--- monticore-test/01.experiments/interpreter/src/main/java/simpleequations/_visitor/SimpleEquationsInterpreter.java
+++ monticore-test/01.experiments/interpreter/src/main/java/simpleequations/_visitor/SimpleEquationsInterpreter.java
@@ -1,10 +1,19 @@
 /* (c) https://github.com/MontiCore/monticore */
 package simpleequations._visitor;
 
+import de.monticore.interpreter.MIScope;
 import de.monticore.interpreter.MIValue;
 import de.monticore.interpreter.MIValueFactory;
 import de.monticore.interpreter.values.ErrorMIValue;
+import de.monticore.interpreter.values.MIReturnSignal;
+import de.monticore.interpreter.values.ModelFunctionMIValue;
 import simpleequations._ast.*;
+import de.monticore.symbols.basicsymbols._symboltable.FunctionSymbol;
+import de.monticore.symbols.basicsymbols._symboltable.VariableSymbol;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class SimpleEquationsInterpreter extends SimpleEquationsInterpreterTOP {
 
@@ -13,11 +22,19 @@ public class SimpleEquationsInterpreter extends SimpleEquationsInterpreterTOP {
   }
 
   public MIValue interpret(ASTProgram node) {
-    node.forEachStatements(s -> s.evaluate(getRealThis()));
+    MIValue result = new ErrorMIValue("Error ASTProgram node");
+    for (ASTStatement s : node.getStatementList()) {
+      //we can skip this as we only need to interpret it when it is getting called
+      if (s instanceof ASTFunctionDefinition) continue;
+      result = s.evaluate(getRealThis());
+      if (result.isReturn()) {
+          return result;
+      }
+    }
     if (node.isPresentExpression()) {
       return node.getExpression().evaluate(getRealThis());
     }
-    return new ErrorMIValue("Error ASTProgram node");
+    return result;
   }
 
   public MIValue interpret(ASTPlusEquation node) {
@@ -60,17 +77,33 @@ public class SimpleEquationsInterpreter extends SimpleEquationsInterpreterTOP {
     return MIValueFactory.createValue(left.asFloat() / right.asFloat());
   }
 
+  public MIValue interpret(ASTGreaterThanExpression node) {
+    MIValue left = node.getLeft().evaluate(getRealThis());
+    MIValue right = node.getRight().evaluate(getRealThis());
+
+    if (left.isInt() && right.isInt()) {
+      return MIValueFactory.createValue(left.asInt() > right.asInt());
+    }
+    return MIValueFactory.createValue(left.asFloat() > right.asFloat());
+  }
+
   public MIValue interpret(ASTVariableDefinition node) {
     MIValue value = node.getValue().evaluate(getRealThis());
-    getRealThis().declareVariable(node.getSymbol(), java.util.Optional.of(value));
-    return new ErrorMIValue("Error ASTVariableDefinition node");
+    getRealThis().declareVariable(node.getSymbol(), Optional.of(value));
+    return value;
   }
 
   public MIValue interpret(ASTVariableUsage node) {
-    var symbol = node.getEnclosingScope().resolveVariableDefinition(node.getName());
     MIValue value = node.getValue().evaluate(getRealThis());
-    symbol.ifPresent(s -> getRealThis().storeVariable(s, value));
-    return new ErrorMIValue("Error ASTVariableUsage node");
+    var symbol = node.getEnclosingScope().resolveVariableDefinition(node.getName());
+
+    if (symbol.isPresent()) {
+      getRealThis().storeVariable(symbol.get(), value);
+    } else {
+      return new ErrorMIValue("Error ASTVariableUsage variable not found");
+    }
+
+    return value;
   }
 
   public MIValue interpret(ASTPrintStatement node) {
@@ -81,7 +114,7 @@ public class SimpleEquationsInterpreter extends SimpleEquationsInterpreterTOP {
     } else if (output.isFloat()) {
       System.out.println(output.asFloat());
     }
-    return new ErrorMIValue("Error ASTPrintStatement node");
+    return output;
   }
 
   public MIValue interpret(ASTNameExpression node) {
@@ -92,6 +125,83 @@ public class SimpleEquationsInterpreter extends SimpleEquationsInterpreterTOP {
       return new ErrorMIValue("Error ASTNameExpression node");
     }
   }
+
+  public MIValue interpret(ASTFunctionCall node) {
+    Optional<FunctionSymbol> functionSymbol = node.getEnclosingScope().resolveFunction(node.getName());
+    if(functionSymbol.isPresent()){
+      ASTFunctionDefinition functionDefinition = (ASTFunctionDefinition) functionSymbol.get().getAstNode();
+      List<MIValue> args = new ArrayList<>();
+      
+      for(ASTExpression expr : node.getArgList().getArgsList()){
+        args.add(expr.evaluate(getRealThis()));
+      }
+
+      MIScope newScope = new MIScope(getRealThis().getCurrentScope());
+      getRealThis().pushScope(newScope);
+
+      try {
+          List<ASTVariableAsParameter> parameters = functionDefinition.getFormalParameters().getFormalParameterListing().streamVariableAsParameters().collect(Collectors.toList());
+          for(int i = 0; i < parameters.size(); i++){
+            VariableSymbol param = parameters.get(i).getSymbol();
+            getRealThis().declareVariable(param, Optional.of(args.get(i)));
+          }
+
+          MIValue result = functionDefinition.getProgram().evaluate(getRealThis());
+
+          while (result.isReturn()){
+            result = result.asReturnValue();
+          }
+
+          if (result.isError()) {
+             System.out.println("Error in function call");
+          }
+          return result;
+      } finally {
+          getRealThis().popScope();
+      }
+    }
+    return new ErrorMIValue("Error ASTFunctionCall node");
+  }
+  
+  public MIValue interpret(ASTIfStatement node) {
+      MIValue condition = node.getCondition().evaluate(getRealThis());
+      if (condition.asBoolean()) {
+          MIValue returnValue =  node.getThenBlock().evaluate(getRealThis());
+          return returnValue;
+      } else if (node.isPresentElseBlock()) {
+          MIValue returnValue = node.getElseBlock().evaluate(getRealThis());
+          return returnValue;
+      }
+      return new ErrorMIValue("IfStatement no branch executed");
+  }
+
+  public MIValue interpret(ASTFunctionBlock node) {
+    MIValue result = new ErrorMIValue("Error ASTFunctionBlock node");
+
+    for (ASTStatement s : node.getStatementList()) {
+      result = s.evaluate(getRealThis());
+
+      if (result.isReturn()) {
+        return result;
+      }
+    }
+
+    // Evaluate the optional trailing expression, if present
+    if (node.isPresentExpression()) {
+      return node.getExpression().evaluate(getRealThis());
+    }
+
+    return result;
+  }
+  
+  public MIValue interpret(ASTReturnStatement node) {
+      MIValue value = node.getExpression().evaluate(getRealThis());
+      if(value.isError()){
+        return new ErrorMIValue("error in return statement");
+      }
+      return new MIReturnSignal(value);
+  }
+
 
   public MIValue interpret(ASTNumberExpression node) {
     return node.getNumber().evaluate(getRealThis());
--- monticore-test/01.experiments/interpreter/src/test/java/simpleequations/_visitor/SimpleEquationsInterpreterTest.java
+++ monticore-test/01.experiments/interpreter/src/test/java/simpleequations/_visitor/SimpleEquationsInterpreterTest.java
@@ -22,6 +22,28 @@ public class SimpleEquationsInterpreterTest {
     SimpleEquationsInterpreter interpreter = new SimpleEquationsInterpreter();
     SimpleEquationsScopesGenitorDelegator delegator = SimpleEquationsMill.scopesGenitorDelegator();
 
+    ASTProgram function = parser.parse_StringProgram("" +
+        "var a = 3; " +
+        "var b = 3; " +
+        "func func1(a, b){ " +
+        " var c = a; " +
+        " if( b > 0 ){ " +
+        "   b = b - 1; " +
+        "   c = a * 3; " +
+        "   var d = func1(c, b);" +
+        "   return d;" +
+        " } else { " +
+        "   return a;" +
+        " };" +
+        "}"+
+        "var result = func1(a, b);" +
+        "print(result);").get();
+    delegator.createFromAST(function);
+    MIValue functionResult = interpreter.interpret(function);
+    assertTrue(functionResult.isInt());
+    assertEquals(functionResult.asInt(), 81);
+
+
     ASTProgram program = parser.parse_StringProgram("var a=3.5; var b=4; print(a); var c=a+b; c;").get();
 
     delegator.createFromAST(program);
