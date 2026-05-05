c0c792cd298bcc7106c7391cf1c2906a8fdf3916
Vanol Nguemo Tadoum <nguemovanol@gmail.com>
2026-05-05 15:30:43 +0200

    minor

--- monticore-grammar/src/test/java/de/monticore/statements/cocos/SwitchCaseTypesValidTest.java
+++ monticore-grammar/src/test/java/de/monticore/statements/cocos/SwitchCaseTypesValidTest.java
@@ -1,17 +1,11 @@
 /* (c) [https://github.com/MontiCore/monticore](https://github.com/MontiCore/monticore) */
 package de.monticore.statements.cocos;
 
-import de.monticore.statements.mccommonstatements._symboltable.IMCCommonStatementsArtifactScope;
-import de.monticore.statements.mccommonstatements._symboltable.MCCommonStatementsSymbols2Json;
 import de.monticore.statements.mccommonstatements.cocos.SwitchCaseTypesValid;
 import de.monticore.statements.mcstatementsbasis._ast.ASTMCBlockStatement;
 import de.monticore.statements.testmccommonstatements.TestMCCommonStatementsMill;
 import de.monticore.statements.testmccommonstatements._cocos.TestMCCommonStatementsCoCoChecker;
-import de.monticore.statements.testmccommonstatements._visitor.TestMCCommonStatementsTraverser;
 import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
-import de.monticore.symbols.basicsymbols._symboltable.VariableSymbol;
-import de.monticore.types.check.FlatExpressionScopeSetter;
-import de.monticore.types.check.SymTypeExpressionFactory;
 import de.monticore.types3.util.CombineExpressionsWithLiteralsTypeTraverserFactory;
 import de.se_rwth.commons.logging.Log;
 import de.se_rwth.commons.logging.LogStub;
@@ -90,83 +84,13 @@ class SwitchCaseTypesValidTest {
     TestMCCommonStatementsCoCoChecker checker = new TestMCCommonStatementsCoCoChecker();
     checker.addCoCo(new SwitchCaseTypesValid());
 
-    // With invalid switch type, case labels should not be validated
     ASTMCBlockStatement ast = parser().parse_StringMCBlockStatement("switch(5.5){case 1:}").orElseThrow();
 
     // When
     checker.checkAll(ast);
 
-    // Then - No errors because type is obscure, so case validation is skipped
-    assertTrue(Log.getFindings().isEmpty(), () -> Log.getFindings().toString());
-  }
-
-  @ParameterizedTest
-  @ValueSource(strings = {"switch(c){}", "switch(c){case Foo:}"})
-  void testSwitchEnumConstantsValid(String expr) throws IOException {
-    // Given
-    IMCCommonStatementsArtifactScope imported =
-      new MCCommonStatementsSymbols2Json().load("target/resources/test/de/monticore/statements/Enum.sym");
-    TestMCCommonStatementsMill.globalScope().addSubScope(imported);
-
-    VariableSymbol variable = TestMCCommonStatementsMill.variableSymbolBuilder()
-      .setName("c")
-      .setType(SymTypeExpressionFactory.createTypeObject(imported.resolveOOType("A").orElseThrow()))
-      .build();
-
-    TestMCCommonStatementsMill.globalScope().add(variable);
-
-    TestMCCommonStatementsCoCoChecker checker = new TestMCCommonStatementsCoCoChecker();
-    checker.addCoCo(new SwitchCaseTypesValid());
-
-    ASTMCBlockStatement ast = parser().parse_StringMCBlockStatement(expr).orElseThrow();
-
-    TestMCCommonStatementsTraverser traverser = TestMCCommonStatementsMill.traverser();
-    traverser.add4ExpressionsBasis(new FlatExpressionScopeSetter(TestMCCommonStatementsMill.globalScope()));
-    ast.accept(traverser);
-
-    // When
-    checker.checkAll(ast);
-
     // Then
     assertTrue(Log.getFindings().isEmpty(), () -> Log.getFindings().toString());
-  }
-
-  @ParameterizedTest
-  @MethodSource("enumInvalidCaseProvider")
-  void testSwitchEnumConstantsInvalidCase(String expr) throws IOException {
-    // Given
-    IMCCommonStatementsArtifactScope imported =
-      new MCCommonStatementsSymbols2Json().load("target/resources/test/de/monticore/statements/Enum.sym");
-    TestMCCommonStatementsMill.globalScope().addSubScope(imported);
-
-    VariableSymbol variable = TestMCCommonStatementsMill.variableSymbolBuilder()
-      .setName("c")
-      .setType(SymTypeExpressionFactory.createTypeObject(imported.resolveOOType("A").orElseThrow()))
-      .build();
-
-    TestMCCommonStatementsMill.globalScope().add(variable);
-
-    TestMCCommonStatementsCoCoChecker checker = new TestMCCommonStatementsCoCoChecker();
-    checker.addCoCo(new SwitchCaseTypesValid());
-
-    ASTMCBlockStatement ast = parser().parse_StringMCBlockStatement(expr).orElseThrow();
-
-    TestMCCommonStatementsTraverser traverser = TestMCCommonStatementsMill.traverser();
-    traverser.add4ExpressionsBasis(new FlatExpressionScopeSetter(TestMCCommonStatementsMill.globalScope()));
-    ast.accept(traverser);
-
-    // When
-    checker.checkAll(ast);
-
-    // Then
-    assertEquals(1, Log.getFindings().size());
-    assertTrue(Log.getFindings().get(0).getMsg().startsWith(SwitchCaseTypesValid.CASE_ERROR_CODE));
-  }
-
-  static Stream<Arguments> enumInvalidCaseProvider() {
-    return Stream.of(
-      arguments("switch(c){case Bar:}")
-    );
   }
 
   @ParameterizedTest
