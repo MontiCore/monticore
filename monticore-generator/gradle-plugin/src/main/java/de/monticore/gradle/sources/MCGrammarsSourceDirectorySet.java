b23c58eafa3cddefe938dbfb9bf6e6b36e46e1eb
jra-se <192100093+jra-se@users.noreply.github.com>
2026-06-02 08:56:32 +0200

    Gradle 9 compatibility (#299)
    * fix use of deprecated DefaultArtifactPublicationSet.addCandidate in MCPublishingPlugin
    
    * add missing annotations to MontiTransExec
    
    * fix deprecated registration of the grammars source set
    
    * add gradle 9.5.1 to MCGenPluginTest
    
    * fix deprecated test listeners
    
    * fix TestListener for Gradle versions < 9.x
    
    * fix deprecated test reporting
    
    * fix test reporting to prevent dedundant name printing
    
    * fix deprecated dependency style

--- monticore-generator/gradle-plugin/src/main/java/de/monticore/gradle/sources/MCGrammarsSourceDirectorySet.java
+++ monticore-generator/gradle-plugin/src/main/java/de/monticore/gradle/sources/MCGrammarsSourceDirectorySet.java
@@ -1,13 +1,13 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.gradle.sources;
 
-import org.gradle.api.Project;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.internal.file.DefaultSourceDirectorySet;
-import org.gradle.api.invocation.Gradle;
+import org.gradle.api.internal.tasks.TaskDependencyFactory;
 import org.gradle.api.tasks.SourceSet;
 
 import javax.annotation.Nonnull;
+import javax.inject.Inject;
 
 /**
  * A set of source files "compiled"/generated using the MCGenTask
@@ -24,10 +24,10 @@ public interface MCGrammarsSourceDirectorySet extends SourceDirectorySet {
   }
 
   // Default implementation class
-  class DefaultMCGrammarsSourceDirectorySet extends DefaultSourceDirectorySet implements MCGrammarsSourceDirectorySet {
-
-    public DefaultMCGrammarsSourceDirectorySet(SourceDirectorySet sourceSet) {
-      super(sourceSet);
+  abstract class DefaultMCGrammarsSourceDirectorySet extends DefaultSourceDirectorySet implements MCGrammarsSourceDirectorySet {
+    @Inject
+    public DefaultMCGrammarsSourceDirectorySet(SourceDirectorySet sourceSet, TaskDependencyFactory taskDependencyFactory) {
+      super(sourceSet, taskDependencyFactory);
     }
   }
 }
