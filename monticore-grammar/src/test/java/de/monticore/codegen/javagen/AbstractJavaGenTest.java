50e4b33b9b23a2af2268413760a07bb1cca4c43f
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2026-06-22 17:24:53 +0200

    Interpreter/CodeGen – major test overhaul

--- monticore-grammar/src/test/java/de/monticore/codegen/javagen/AbstractJavaGenTest.java
+++ monticore-grammar/src/test/java/de/monticore/codegen/javagen/AbstractJavaGenTest.java
@@ -1,99 +1,106 @@
 // (c) https://github.com/MontiCore/monticore
 package de.monticore.codegen.javagen;
 
-import de.monticore.class2mc.OOClass2MCResolver;
+import de.monticore.ast.ASTNode;
 import de.monticore.codegen.CodeGenerator;
-import de.monticore.expressions.combineexpressionswithliterals.CombineExpressionsWithLiteralsMill;
-import de.monticore.expressions.combineexpressionswithliterals._ast.ASTFoo;
-import de.monticore.expressions.combineexpressionswithliterals._symboltable.ICombineExpressionsWithLiteralsArtifactScope;
-import de.monticore.expressions.combineexpressionswithliterals._visitor.CombineExpressionsWithLiteralsTraverser;
-import de.monticore.expressions.expressionsbasis._ast.ASTExpression;
-import de.monticore.expressions.lambdaexpressions._ast.ASTLambdaExpression;
-import de.monticore.expressions.lambdaexpressions._symboltable.LambdaExpressionsSTCompleteTypes2;
-import de.monticore.ocl.oclexpressions.symboltable.OCLExpressionsSymbolTableCompleter;
-import de.monticore.ocl.setexpressions.symboltable.SetExpressionsSymbolTableCompleter;
-import de.monticore.prettyprint.IndentPrinter;
 import de.monticore.runtime.junit.AbstractMCTest;
 import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
-import de.monticore.symbols.oosymbols.OOSymbolsMill;
-import de.monticore.symbols.oosymbols._symboltable.FieldSymbol;
-import de.monticore.symbols.oosymbols._symboltable.IOOSymbolsGlobalScope;
-import de.monticore.types.check.SymTypeExpression;
-import de.monticore.types.check.SymTypeExpressionFactory;
-import de.monticore.types.mcbasictypes._ast.ASTMCType;
-import de.monticore.types3.SymTypeRelations;
-import de.monticore.types3.TypeCheck3;
-import de.monticore.types3.util.CombineExpressionsWithLiteralsTypeTraverserFactory;
-import de.monticore.types3.util.DefsTypesForTests;
-import de.monticore.types3.util.OOWithinScopeBasicSymbolsResolver;
-import de.monticore.types3.util.OOWithinTypeBasicSymbolsResolver;
-import de.monticore.visitor.ITraverser;
+import de.monticore.tests.expressionsandstatements.ExpressionsAndStatementsUtil;
+import de.monticore.tests.expressionsandstatements._ast.ASTBehaviorInput;
+import de.monticore.tests.expressionsandstatements.codegen.javagen.ExpressionsAndStatementsJavaGenerator;
+import de.se_rwth.commons.logging.LogStub;
 import jdk.jshell.JShell;
 import jdk.jshell.Snippet;
 import jdk.jshell.SnippetEvent;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
-import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.CodeSource;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static de.monticore.codegen.javagen.SymTypeExpression2JavaConverter.convert2JavaType;
 import static de.monticore.runtime.junit.MCAssertions.assertNoFindings;
+import static de.monticore.types3.SymTypeRelations.normalize;
+import static de.monticore.types3.TypeCheck3.typeOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public abstract class AbstractJavaGenTest extends AbstractMCTest {
 
+  // enable for experimenting:
+  // it will print the generated code before evaluation
+  protected static final boolean printGeneratedCode = true;
+
   protected JShell jshell;
+
+  CodeGenerator generator;
 
   @BeforeEach
   void beforeEach() {
-    CombineExpressionsWithLiteralsMill.reset();
-    CombineExpressionsWithLiteralsMill.init();
-    BasicSymbolsMill.initializePrimitives();
-    DefsTypesForTests.set_thePrimitives();
-
-    SymTypeRelations.init();
-    OOWithinScopeBasicSymbolsResolver.init();
-    OOWithinTypeBasicSymbolsResolver.init();
-    CombineExpressionsWithLiteralsTypeTraverserFactory.initTypeCheck3();
-    // Class2MC
-    IOOSymbolsGlobalScope globalScope = OOSymbolsMill.globalScope();
-    OOClass2MCResolver resolver = new OOClass2MCResolver();
-    globalScope.addAdaptedOOTypeSymbolResolver(resolver);
-    globalScope.addAdaptedTypeSymbolResolver(resolver);
-
+    LogStub.initPlusLog();
+    ExpressionsAndStatementsUtil.init();
+    generator = new ExpressionsAndStatementsJavaGenerator();
     jshell = JShell.create();
-  }
-
-  protected CodeGenerator createCodeGenerator() {
-    return new CombineExpressionWithLiteralsCodeGenerator(new IndentPrinter());
-  }
-
-  /**
-   * Evaluates an expression.
-   * First, generates Java code.
-   * Then, generates required functional interfaces.
-   * Ultimately, evaluates the Java code using the JShell API.
-   *
-   * @param javaStr The java expression.
-   * @return A List of {@see jdk.shell.SnippetEvent}s.
-   */
-  protected List<SnippetEvent> evalJava(String javaStr) {
     String[] classpaths = System.getProperty("java.class.path")
         .split(File.pathSeparator);
     for (String classpath : classpaths) {
       getJShell().addToClasspath(classpath);
     }
-    return getJShell().eval(javaStr + ";");
   }
 
-  protected void checkValue(String exprStr, String expectedValue) {
-    String javaStr = generateJavaFromExpression(exprStr);
-    List<SnippetEvent> events = evalJava(javaStr);
+  @AfterEach
+  void cleanUp() {
+    jshell.close();
+    jshell = null;
+  }
+
+  protected void addClassPathEntry(Class<?> clazz) {
+    try {
+      CodeSource codeSource = clazz
+          .getProtectionDomain()
+          .getCodeSource();
+      if (codeSource == null) {
+        return;
+      }
+      Path classPath = Paths.get(codeSource.getLocation().toURI());
+      BasicSymbolsMill.globalScope().getSymbolPath().addEntry(classPath);
+    }
+    catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  protected JShell getJShell() {
+    return jshell;
+  }
+
+  protected void checkValue(String behaviorModelStr, Object expectedValue) {
+    // setup
+    ASTBehaviorInput ast = ExpressionsAndStatementsUtil
+        .getPreparedAST(behaviorModelStr);
+    String javaReturnType = ast.isPresentExpression() ?
+        convert2JavaType(normalize(typeOf(ast.getExpression()))) :
+        "void";
+    assertNoFindings();
+    String javaStatementsStr = generateCode(ast);
+    assertNoFindings();
+    String javaMethodStr =
+        javaReturnType
+            + " systemUnderTest() {" + System.lineSeparator()
+            + javaStatementsStr
+            + "}";
+    if (printGeneratedCode) {
+      System.out.println("***** Generated Code *****");
+      System.out.println(javaMethodStr);
+      System.out.println("**************************");
+    }
+
+    // method definition
+    List<SnippetEvent> events = getJShell().eval(javaMethodStr);
     assertEquals(1, events.size(),
         "More/Less than exactly one events where triggered by the evaluation."
     );
@@ -101,153 +108,40 @@ public abstract class AbstractJavaGenTest extends AbstractMCTest {
     assertEquals(Snippet.Status.VALID, event.status(),
         "JShell failed:" + System.lineSeparator()
             + printDiags(event.snippet()) + System.lineSeparator()
-            + "Expression:" + System.lineSeparator() + javaStr
+            + "Model:" + System.lineSeparator() + javaMethodStr
     );
-    assertEquals(expectedValue, event.value(),
+
+    // method call
+    events = getJShell().eval("systemUnderTest()");
+    assertEquals(1, events.size(),
+        "More/Less than exactly one events where triggered by the evaluation."
+    );
+    event = events.getFirst();
+    assertEquals(Snippet.Status.VALID, event.status(),
+        "JShell failed:" + System.lineSeparator()
+            + printDiags(event.snippet()) + System.lineSeparator()
+            + "Model:" + System.lineSeparator() + javaMethodStr
+    );
+    String value = event.value();
+
+    // edge case to support Strings
+    if (expectedValue instanceof String expectedStr) {
+      expectedValue = "\"" + expectedStr + "\"";
+    }
+    assertEquals(expectedValue.toString(), value,
         "The evaluated Java code does not match the expected output."
-            + " Expression:" + System.lineSeparator() + javaStr
+            + " Model:" + System.lineSeparator() + javaMethodStr
     );
   }
 
-  protected String generateJavaFromExpression(String exprStr) {
-    ASTExpression astExpr = parseExpr(exprStr);
-
-    generateScopes(astExpr);
-    SymTypeExpression type = TypeCheck3.typeOf(astExpr);
-    assertNoFindings();
-    assertFalse(type.isObscureType());
-
-    CodeGenerator generator = createCodeGenerator();
-    generator.generateCode(astExpr);
-    String javaExpr = generator.getPrinter().getContent();
-    assertNoFindings();
-
-    return javaExpr;
-  }
-
-  protected String generateJavaFromType(String typeStr) {
-    ASTMCType astType = parseMCType(typeStr);
-
-    generateScopes(astType);
-    SymTypeExpression type = TypeCheck3.symTypeFromAST(astType);
-    assertNoFindings();
-    assertFalse(type.isObscureType());
-
-    CodeGenerator generator = createCodeGenerator();
-    generator.generateCode(astType);
-    return generator.getPrinter().getContent();
-  }
-
-  protected void generateScopes(ASTExpression expr) {
-    // create a root
-    ASTFoo rootNode = CombineExpressionsWithLiteralsMill.fooBuilder()
-        .setExpression(expr)
-        .build();
-    ICombineExpressionsWithLiteralsArtifactScope rootScope =
-        CombineExpressionsWithLiteralsMill.scopesGenitorDelegator()
-            .createFromAST(rootNode);
-    rootScope.setName("fooRoot");
-    // complete the symbol table
-    expr.accept(getSymbolTableCompleter());
-  }
-
-  protected void generateScopes(ASTMCType mcType) {
-    // create an expression to contain the type
-    // currently (MC 7.8) lambda expressions are the only expressions
-    // which can directly contain MCTypes
-    ASTLambdaExpression lambda = CombineExpressionsWithLiteralsMill
-        .lambdaExpressionBuilder()
-        .setLambdaParameters(
-            CombineExpressionsWithLiteralsMill.lambdaParametersBuilder()
-                .setLambdaParametersList(List.of(
-                    CombineExpressionsWithLiteralsMill.lambdaParameterBuilder()
-                        .setName("parameter")
-                        .setMCType(mcType)
-                        .build()
-                ))
-                .build()
-        )
-        .setLambdaBody(
-            CombineExpressionsWithLiteralsMill.lambdaExpressionBodyBuilder()
-                .setExpression(
-                    CombineExpressionsWithLiteralsMill.literalExpressionBuilder()
-                        .setLiteral(
-                            CombineExpressionsWithLiteralsMill
-                                .natLiteralBuilder()
-                                .setDigits("8243721")
-                                .build()
-                        )
-                        .build()
-                )
-                .setType(SymTypeExpressionFactory.createPrimitive("int"))
-                .build()
-        )
-        .build();
-    // create a root
-    ASTFoo rootNode = CombineExpressionsWithLiteralsMill.fooBuilder()
-        .setExpression(lambda)
-        .build();
-    ICombineExpressionsWithLiteralsArtifactScope rootScope =
-        CombineExpressionsWithLiteralsMill.scopesGenitorDelegator()
-            .createFromAST(rootNode);
-    rootScope.setName("fooRoot");
-  }
-
-  // Parse a String expression of the according language
-  protected Optional<ASTExpression> parseStringExpr(String exprStr) {
-    try {
-      return CombineExpressionsWithLiteralsMill.parser()
-          .parse_StringExpression(exprStr);
-    }
-    catch (IOException e) {
-      return fail(e);
-    }
-  }
-
-  // Parse a String type identifier of the according language
-  protected Optional<ASTMCType> parseStringMCType(String mcTypeStr) {
-    try {
-      return CombineExpressionsWithLiteralsMill.parser()
-          .parse_StringMCType(mcTypeStr);
-    }
-    catch (IOException e) {
-      return fail(e);
-    }
-  }
-
-  protected ASTExpression parseExpr(String exprStr) {
-    Optional<ASTExpression> astExpression = parseStringExpr(exprStr);
-    assertNoFindings();
-    assertTrue(astExpression.isPresent());
-    return astExpression.get();
-  }
-
-  protected ASTMCType parseMCType(String typeStr) {
-    Optional<ASTMCType> mcType = parseStringMCType(typeStr);
-    assertNoFindings();
-    assertTrue(mcType.isPresent());
-    return mcType.get();
-  }
-
-  protected ITraverser getSymbolTableCompleter() {
-    CombineExpressionsWithLiteralsTraverser combinedScopesCompleter =
-        CombineExpressionsWithLiteralsMill.traverser();
-    combinedScopesCompleter.add4LambdaExpressions(
-        new LambdaExpressionsSTCompleteTypes2()
-    );
-    OCLExpressionsSymbolTableCompleter oclExprCompleter =
-        new OCLExpressionsSymbolTableCompleter();
-    combinedScopesCompleter.add4OCLExpressions(oclExprCompleter);
-    combinedScopesCompleter.setOCLExpressionsHandler(oclExprCompleter);
-    SetExpressionsSymbolTableCompleter setExprCompleter =
-        new SetExpressionsSymbolTableCompleter();
-    combinedScopesCompleter.add4SetExpressions(setExprCompleter);
-    combinedScopesCompleter.setSetExpressionsHandler(setExprCompleter);
-    return combinedScopesCompleter;
-  }
-
-  protected JShell getJShell() {
-    return jshell;
+  /**
+   * produces Java code given a model.
+   *
+   * @param node the node to generate code from
+   * @return the generated Java code
+   */
+  protected String generateCode(ASTNode node) {
+    return generator.generateCode(node);
   }
 
   protected String printDiags(Snippet snippet) {
@@ -259,5 +153,4 @@ public abstract class AbstractJavaGenTest extends AbstractMCTest {
         )
         .collect(Collectors.joining(System.lineSeparator()));
   }
-
 }
--- monticore-grammar/src/test/java/de/monticore/interpreter/InterpreterExpressionsTest.java
+++ monticore-grammar/src/test/java/de/monticore/interpreter/InterpreterExpressionsTest.java
@@ -1,147 +1,44 @@
 // (c) https://github.com/MontiCore/monticore
 package de.monticore.interpreter;
 
+import de.monticore.tests.expressionsandstatements.ExpressionsTestModels;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
-public class InterpreterExpressionsTest extends InterpreterTestAbstract {
+public class InterpreterExpressionsTest extends AbstractInterpreterTest {
 
-  @ParameterizedTest(name = "[{index}] {0}")
-  @MethodSource("expressionCases")
+  @ParameterizedTest
+  @MethodSource("de.monticore.tests.expressionsandstatements.ExpressionsTestModels#getExpressionsCases")
   void testExpressions(String exprStr, Object expectedValue) {
+    assumeSupported(exprStr, expectedValue);
     assertEquals(expectedValue, interpretAndCast(exprStr));
   }
 
   // checks if the interpreter also works while logging
-  @ParameterizedTest(name = "[{index}] {0}")
-  @MethodSource("expressionCases")
+  @ParameterizedTest
+  @MethodSource("de.monticore.tests.expressionsandstatements.ExpressionsTestModels#getExpressionsCases")
   void testExpressionsWithLog(String exprStr, Object expectedValue) {
+    assumeSupported(exprStr, expectedValue);
     interpreter = initializeInterpreterWithLog();
     assertEquals(expectedValue, interpretAndCast(exprStr));
   }
 
-  static Stream<Arguments> expressionCases() {
-    return Stream.of(
-        Arguments.of("1 + 3", 4),
-        Arguments.of("1 + 2.5", 3.5),
-        Arguments.of("2.5 + 1", 3.5),
-        Arguments.of("\"a\" + \"b\"", "ab"),
-        Arguments.of("\"x\" + 1", "x1"),
-        Arguments.of("1 + \"x\"", "1x"),
-        Arguments.of("\"sum=\" + (1 + 2)", "sum=3"),
-        Arguments.of("7 - 2", 5),
-        Arguments.of("2 - 7", -5),
-        Arguments.of("7 - 2.5", 4.5),
-        Arguments.of("2.5 - 7", -4.5),
-        Arguments.of("3 * 4", 12),
-        Arguments.of("1.5 * 4", 6.0),
-        Arguments.of("4 * 1.5", 6.0),
-        Arguments.of("7 / 2", 3),
-        Arguments.of("2 / 7", 0),
-        Arguments.of("7 / 2.0", 3.5),
-        Arguments.of("2.0 / 7", 2.0 / 7.0),
-        Arguments.of("7 % 3", 1),
-        Arguments.of("3 % 7", 3),
-        Arguments.of("7.5 % 2", 1.5),
-        Arguments.of("2 % 7.5", 2.0),
-        Arguments.of("+7", 7),
-        Arguments.of("-7", -7),
-        Arguments.of("+2.5", 2.5),
-        Arguments.of("-2.5", -2.5),
-        Arguments.of("1 < 3", true),
-        Arguments.of("3 < 1", false),
-        Arguments.of("1 <= 1", true),
-        Arguments.of("2 <= 1", false),
-        Arguments.of("3 > 1", true),
-        Arguments.of("1 > 3", false),
-        Arguments.of("3 >= 3", true),
-        Arguments.of("1 >= 3", false),
-        Arguments.of("1 < 1.5", true),
-        Arguments.of("1.5 < 1", false),
-        Arguments.of("2.5 <= 2.5", true),
-        Arguments.of("2.5 <= 2", false),
-        Arguments.of("3.5 > 3", true),
-        Arguments.of("3 > 3.5", false),
-        Arguments.of("3.5 >= 3.5", true),
-        Arguments.of("3 >= 3.5", false),
-        Arguments.of("1 == 1", true),
-        Arguments.of("1 == 2", false),
-        Arguments.of("1 != 2", true),
-        Arguments.of("2 != 2", false),
-        Arguments.of("1 == 1.0", true),
-        Arguments.of("1.0 == 1", true),
-        Arguments.of("1.5 == 1.5", true),
-        Arguments.of("1.5 == 1", false),
-        Arguments.of("1 != 1.0", false),
-        Arguments.of("3.0 != 3", false),
-        Arguments.of("1.5 != 1", true),
-        Arguments.of("true == true", true),
-        Arguments.of("true == false", false),
-        Arguments.of("false == true", false),
-        Arguments.of("true != false", true),
-        Arguments.of("false != false", false),
-        Arguments.of("true && true", true),
-        Arguments.of("true && false", false),
-        Arguments.of("false && true", false),
-        Arguments.of("false && false", false),
-        Arguments.of("true || true", true),
-        Arguments.of("true || false", true),
-        Arguments.of("false || true", true),
-        Arguments.of("false || false", false),
-        Arguments.of("!true", false),
-        Arguments.of("!false", true),
-        Arguments.of("!(1 < 2)", false),
-        Arguments.of("!(2 < 1)", true),
-        Arguments.of("~0", -1),
-        Arguments.of("~1", -2),
-        Arguments.of("~7", -8),
-        Arguments.of("~(1 + 2)", -4),
-        Arguments.of("1 << 3", 8),
-        Arguments.of("16 >> 2", 4),
-        Arguments.of("16 >>> 2", 4),
-        Arguments.of("6 & 3", 2),
-        Arguments.of("6 ^ 3", 5),
-        Arguments.of("6 | 3", 7),
-        Arguments.of("Optional.of(2) ?: 9", 2),
-        Arguments.of("((Optional<int>)Optional.empty()) ?: 9", 9),
-        Arguments.of("Optional.of(2) ?< 3", true),
-        Arguments.of("((Optional<int>)Optional.empty()) ?< 1", false),
-        Arguments.of("Optional.of(2) ?> 3", false),
-        Arguments.of("Optional.of(2) ?<= 3", true),
-        Arguments.of("Optional.of(2) ?<= 1", false),
-        Arguments.of("Optional.of(2) ?>= 2", true),
-        Arguments.of("Optional.of(2) ?== 2", true),
-        Arguments.of("Optional.of(2) ?!= 2", false),
-        Arguments.of("Optional.of(2) ?~~ Optional.of(2)", true),
-        Arguments.of("Optional.of(2) ?!~ Optional.of(3)", true),
-        Arguments.of("(String)\"Hello\"", "Hello"),
-        Arguments.of("(() -> 1)()", 1),
-        Arguments.of("(() -> () -> 2)()()", 2),
-        Arguments.of("((long a) -> a + 1)(41L)", 42),
-        Arguments.of("((long a) -> (byte b) -> a + b)(4L)((byte)28)", 32),
-        Arguments.of("((long a, byte b) -> a + b)(4L,(byte)28)", 32),
-        Arguments.of("(() -> () -> (int a) -> () -> () -> a)()()(42)()()", 42),
-        Arguments.of("((byte b) -> (char c) -> b + c)((byte)25)('a') == 'z'", true),
-        Arguments.of("[1, 4..7, 33]",
-            List.of(1, 4, 5, 6, 7, 33)),
-        Arguments.of("[x + x | x in [1, 4..7, 33]]",
-            List.of(2, 8, 10, 12, 14, 66)),
-        Arguments.of("[x + x | x in [y + y | y in [1,2,3]]]",
-            List.of(4, 8, 12)),
-        Arguments.of(
-            "[z + x | int x in [y + y | y in [1,2,3]], "
-                + "int z in [1 + x, 2 + x]]",
-            List.of(5, 6, 9, 10, 13, 14)),
-        Arguments.of("{1, 4..6, 4}", Set.of(1, 4, 5, 6)),
-        Arguments.of("{x * x | x in {1, 2, 3}}", Set.of(1, 4, 9)),
-        Arguments.of("{x | x in {1, 2, 3, 4}, x % 2 == 0}", Set.of(2, 4))
+  protected void assumeSupported(String exprStr, Object expectedValue) {
+    // Tuples not supported yet
+    assumeFalse(exprStr.contains("(0, 3)"));
+    assumeFalse(exprStr.contains("((0, 0), 3)"));
+    // OCLExpressions are not supported yet
+    assumeFalse(
+        ExpressionsTestModels.getOCLExpressionsCases()
+            .anyMatch(c -> c.get()[0].equals(exprStr))
+    );
+    // UglyExpressions are not supported yet
+    assumeFalse(
+        ExpressionsTestModels.getTypeCastingCases()
+            .anyMatch(c -> c.get()[0].equals(exprStr))
     );
   }
 
