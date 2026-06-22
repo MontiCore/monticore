e44dd7cbbe557707600e4e376e219a15273aed86
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2026-06-22 17:24:53 +0200

    Interpreter/CodeGen – major test overhaul

--- monticore-grammar/src/test/java/de/monticore/interpreter/InterpreterClass2MCTest.java
+++ monticore-grammar/src/test/java/de/monticore/interpreter/InterpreterClass2MCTest.java
@@ -1,130 +1,32 @@
 // (c) https://github.com/MontiCore/monticore
 package de.monticore.interpreter;
 
+import de.monticore.tests.expressionsandstatements.Class2MCTestModels;
+import de.monticore.tests.expressionsandstatements.rte.AClass;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class InterpreterClass2MCTest extends InterpreterTestAbstract {
+public class InterpreterClass2MCTest extends AbstractInterpreterTest {
 
-  @ParameterizedTest(name = "[{index}] {0}")
-  @MethodSource("nativeJavaCases")
-  void testExpressionsNativeJava(String exprStr, Object expectedValue) {
-    String modelStr = """
-        import de.monticore.interpreter.util.AClass;
-        AClass aClass = AClass();
-        """
-        + exprStr;
+  @BeforeEach
+  public void setupAClass() {
+    addClassPathEntry(AClass.class);
+  }
+
+  @ParameterizedTest
+  @MethodSource("de.monticore.tests.expressionsandstatements.Class2MCTestModels#getNativeJavaCases")
+  void testNativeJava(String modelStr, Object expectedValue) {
     assertEquals(expectedValue, interpretAndCast(modelStr));
   }
 
-  static Stream<Arguments> nativeJavaCases() {
-    return Stream.of(
-        Arguments.of("aClass.var_boolean", false),
-        Arguments.of("aClass.var_byte", 0),
-        Arguments.of("aClass.var_short", 0),
-        Arguments.of("aClass.var_char", 0),
-        Arguments.of("aClass.var_int", 0),
-        Arguments.of("aClass.var_long", 0),
-        Arguments.of("aClass.var_float", 0.0),
-        Arguments.of("aClass.var_double", 0.0),
-        Arguments.of("aClass.var_Boolean", false),
-        Arguments.of("aClass.var_Byte", 0),
-        Arguments.of("aClass.var_Short", 0),
-        Arguments.of("aClass.var_Character", 0),
-        Arguments.of("aClass.var_Integer", 0),
-        Arguments.of("aClass.var_Long", 0),
-        Arguments.of("aClass.var_Float", 0.0),
-        Arguments.of("aClass.var_Double", 0.0),
-
-        Arguments.of("AClass.var_s_boolean", false),
-        Arguments.of("AClass.var_s_byte", 0),
-        Arguments.of("AClass.var_s_short", 0),
-        Arguments.of("AClass.var_s_char", 0),
-        Arguments.of("AClass.var_s_int", 0),
-        Arguments.of("AClass.var_s_long", 0),
-        Arguments.of("AClass.var_s_float", 0.0),
-        Arguments.of("AClass.var_s_double", 0.0),
-        Arguments.of("AClass.var_s_Boolean", false),
-        Arguments.of("AClass.var_s_Byte", 0),
-        Arguments.of("AClass.var_s_Short", 0),
-        Arguments.of("AClass.var_s_Character", 0),
-        Arguments.of("AClass.var_s_Integer", 0),
-        Arguments.of("AClass.var_s_Long", 0),
-        Arguments.of("AClass.var_s_Float", 0.0),
-        Arguments.of("AClass.var_s_Double", 0.0),
-
-        Arguments.of("aClass.get_var_boolean()", false),
-        Arguments.of("aClass.get_var_byte()", 0),
-        Arguments.of("aClass.get_var_short()", 0),
-        Arguments.of("aClass.get_var_char()", 0),
-        Arguments.of("aClass.get_var_int()", 0),
-        Arguments.of("aClass.get_var_long()", 0),
-        Arguments.of("aClass.get_var_float()", 0.0),
-        Arguments.of("aClass.get_var_double()", 0.0),
-        Arguments.of("aClass.get_var_Boolean()", false),
-        Arguments.of("aClass.get_var_Byte()", 0),
-        Arguments.of("aClass.get_var_Short()", 0),
-        Arguments.of("aClass.get_var_Character()", 0),
-        Arguments.of("aClass.get_var_Integer()", 0),
-        Arguments.of("aClass.get_var_Long()", 0),
-        Arguments.of("aClass.get_var_Float()", 0.0),
-        Arguments.of("aClass.get_var_Double()", 0.0),
-
-        Arguments.of("AClass.get_var_s_boolean()", false),
-        Arguments.of("AClass.get_var_s_byte()", 0),
-        Arguments.of("AClass.get_var_s_short()", 0),
-        Arguments.of("AClass.get_var_s_char()", 0),
-        Arguments.of("AClass.get_var_s_int()", 0),
-        Arguments.of("AClass.get_var_s_long()", 0),
-        Arguments.of("AClass.get_var_s_float()", 0.0),
-        Arguments.of("AClass.get_var_s_double()", 0.0),
-        Arguments.of("AClass.get_var_s_Boolean()", false),
-        Arguments.of("AClass.get_var_s_Byte()", 0),
-        Arguments.of("AClass.get_var_s_Short()", 0),
-        Arguments.of("AClass.get_var_s_Character()", 0),
-        Arguments.of("AClass.get_var_s_Integer()", 0),
-        Arguments.of("AClass.get_var_s_Long()", 0),
-        Arguments.of("AClass.get_var_s_Float()", 0.0),
-        Arguments.of("AClass.get_var_s_Double()", 0.0),
-
-        Arguments.of("aClass.set_var_boolean(true); aClass.var_boolean", true),
-        Arguments.of("aClass.set_var_byte((byte)1); aClass.var_byte", 1),
-        Arguments.of("aClass.set_var_short((short)2); aClass.var_short", 2),
-        Arguments.of("aClass.set_var_char('R'); aClass.var_char", (int) 'R'),
-        Arguments.of("aClass.set_var_int(3); aClass.var_int", 3),
-        Arguments.of("aClass.set_var_long(4L); aClass.var_long", 4),
-        Arguments.of("aClass.set_var_float(5.0f); aClass.var_float", 5.0),
-        Arguments.of("aClass.set_var_double(6.0); aClass.var_double", 6.0),
-        Arguments.of("aClass.set_var_Boolean(true); aClass.var_Boolean", true),
-        Arguments.of("aClass.set_var_Byte((byte)7); aClass.var_Byte", 7),
-        Arguments.of("aClass.set_var_Short((short)8); aClass.var_Short", 8),
-        Arguments.of("aClass.set_var_Character('E'); aClass.var_Character", (int) 'E'),
-        Arguments.of("aClass.set_var_Integer(9); aClass.var_Integer", 9),
-        Arguments.of("aClass.set_var_Long(10L); aClass.var_Long", 10),
-        Arguments.of("aClass.set_var_Float(11.0f); aClass.var_Float", 11.0),
-        Arguments.of("aClass.set_var_Double(12.0); aClass.var_Double", 12.0),
-
-        Arguments.of("AClass.set_var_s_boolean(true); AClass.var_s_boolean", true),
-        Arguments.of("AClass.set_var_s_byte((byte)13); AClass.var_s_byte", 13),
-        Arguments.of("AClass.set_var_s_short((short)14); AClass.var_s_short", 14),
-        Arguments.of("AClass.set_var_s_char('P'); AClass.var_s_char", (int) 'P'),
-        Arguments.of("AClass.set_var_s_int(15); AClass.var_s_int", 15),
-        Arguments.of("AClass.set_var_s_long(16L); AClass.var_s_long", 16),
-        Arguments.of("AClass.set_var_s_float(17.0f); AClass.var_s_float", 17.0),
-        Arguments.of("AClass.set_var_s_double(18.0); AClass.var_s_double", 18.0),
-        Arguments.of("AClass.set_var_s_Boolean(true); AClass.var_s_Boolean", true),
-        Arguments.of("AClass.set_var_s_Byte((byte)19); AClass.var_s_Byte", 19),
-        Arguments.of("AClass.set_var_s_Short((short)20); AClass.var_s_Short", 20),
-        Arguments.of("AClass.set_var_s_Character('L'); AClass.var_s_Character", (int) 'L'),
-        Arguments.of("AClass.set_var_s_Integer(21); AClass.var_s_Integer", 21),
-        Arguments.of("AClass.set_var_s_Long(22L); AClass.var_s_Long", 22),
-        Arguments.of("AClass.set_var_s_Float(23.0f); AClass.var_s_Float", 23.0),
-        Arguments.of("AClass.set_var_s_Double(24.0); AClass.var_s_Double", 24.0)
-    );
+  @ParameterizedTest(name = "[{index}] {0}")
+  @MethodSource("de.monticore.tests.expressionsandstatements.Class2MCTestModels#getAClassCases")
+  void testNativeJavaAClass(String tail, Object expectedValue) {
+    String modelStr = Class2MCTestModels.getAClassPrefix() + tail;
+    assertEquals(expectedValue, interpretAndCast(modelStr));
   }
+
 }
--- monticore-grammar/src/test/java/de/monticore/interpreter/InterpreterStatementsTest.java
+++ monticore-grammar/src/test/java/de/monticore/interpreter/InterpreterStatementsTest.java
@@ -2,19 +2,25 @@
 package de.monticore.interpreter;
 
 import de.monticore.values.MCValue;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
-public class InterpreterStatementsTest extends InterpreterTestAbstract {
+public class InterpreterStatementsTest extends AbstractInterpreterTest {
+
+  @ParameterizedTest(name = "[{index}] {0}")
+  @MethodSource("de.monticore.tests.expressionsandstatements.StatementsTestModels#getStatementCases")
+  void testStatements(String modelStr, Object expectedValue) {
+    // for-loops are not supported yet
+    assumeFalse(modelStr.contains("for "));
+
+    assertEquals(expectedValue, interpretAndCast(modelStr));
+  }
 
   @Test
   void testAssertStatementFailure() {
@@ -32,88 +38,4 @@ public class InterpreterStatementsTest extends InterpreterTestAbstract {
     assertEquals("message", error.getMessage());
   }
 
-  @Disabled
-  @ParameterizedTest(name = "[{index}] {0}")
-  @MethodSource("statementCases")
-  void testStatements(String modelStr, Object expectedValue) {
-    assertEquals(expectedValue, interpretAndCast(modelStr));
-  }
-
-  static Stream<Arguments> statementCases() {
-    return Stream.of(
-        Arguments.of("int x = 1, y = 2; x + y", 3),
-        Arguments.of("int a, b = 4; a = 2; a + b", 6),
-        Arguments.of("int x; if (true) {x = 2;} else {x = 3;}; x", 2),
-        Arguments.of("int x; if (false) {x = 2;} else {x = 3;}; x", 3),
-        Arguments.of("int x = 1; if (true) {x = 2;} x", 2),
-        Arguments.of("int x = 1; if (false) {x = 2;} x", 1),
-        Arguments.of("int x = 1; while (x < 0) {x++;} x", 10),
-        Arguments.of("int x = 1; while (false) {x++;} x", 1),
-        Arguments.of("int x = 0; while (true) {x++; break; x++;} x", 1),
-        Arguments.of("int x = 1; do {x++;} while (x < 10) x", 10),
-        Arguments.of("int x = 1; do {x++;} while (false) x", 2),
-        Arguments.of("int x = 0; do {x++; break; x++;} while (true); x", 1),
-        Arguments.of("int x = 0; for (int i = 1; i <= 4; i++) x++; sum", 5),
-        Arguments.of("int x = 0; for (;x < 4;) x++; x", 4),
-        Arguments.of("int x = 1; for (int i = 0; i < 0; i++) x++; x", 1),
-        Arguments.of("""
-            int x = 0;
-            for (int i = 0; i < 2; i++)
-              for (int j = 0; j < 3; j++)
-                x++;
-            x
-            """, 6),
-        Arguments.of("int x = 7; assert x == 7; x", 7),
-        Arguments.of("int x = 1; ; ; ; x", 1)
-    );
-  }
-
-  @ParameterizedTest(name = "[{index}] {0}")
-  @MethodSource("assignmentCases")
-  void testAssignmentExpressions(String modelStr, Object expectedValue) {
-    assertEquals(expectedValue, interpretAndCast(modelStr));
-  }
-
-  static Stream<Arguments> assignmentCases() {
-    return Stream.of(
-        Arguments.of("int x = 1; x = 2; x", 2),
-        Arguments.of("int x = 1; x += 2; x", 3),
-        Arguments.of("int x = 2; x -= 2; x", 0),
-        Arguments.of("int x = 3; x *= 2; x", 6),
-        Arguments.of("int x = 8; x /= 2; x", 4),
-        Arguments.of("int x = 9; x %= 4; x", 1),
-        Arguments.of("int x = 1; x <<= 3; x", 8),
-        Arguments.of("int x = 4; x >>= 2; x", 1),
-        Arguments.of("int x = -1; x >>>= 1; x", -1 >>> 1),
-        Arguments.of("int x = 6; x &= 3; x", 2),
-        Arguments.of("int x = 6; x ^= 3; x", 5),
-        Arguments.of("int x = 6; x |= 3; x", 7),
-
-        Arguments.of("double x = 1.5; x += 2.0; x", 3.5),
-        Arguments.of("double x = 5.0; x -= 2.5; x", 2.5),
-        Arguments.of("double x = 1.5; x *= 4.0; x", 6.0),
-        Arguments.of("double x = 9.0; x /= 1.5; x", 6.0),
-        Arguments.of("double x = 7.5; x %= 2.0; x", 1.5),
-
-        Arguments.of("int x = 1; ++x", 2),
-        Arguments.of("int x = 1; x++", 1),
-        Arguments.of("int x = 1; x++; x", 2),
-        Arguments.of("int x = 2; --x", 1),
-        Arguments.of("int x = 2; x--", 2),
-        Arguments.of("int x = 2; x--; x", 1),
-
-        Arguments.of("double x = 1.5; ++x", 2.5),
-        Arguments.of("double x = 1.5; x++", 1.5),
-        Arguments.of("double x = 1.5; x++; x", 2.5),
-        Arguments.of("double x = 2.5; --x", 1.5),
-        Arguments.of("double x = 2.5; x--", 2.5),
-        Arguments.of("double x = 2.5; x--; x", 1.5),
-
-        // more complex assignment
-        Arguments.of(
-            "int -> int s = (int n) -> n > 0 ? n + s(n - 1) : 0; s(5)"
-            , 15
-        )
-    );
-  }
 }
