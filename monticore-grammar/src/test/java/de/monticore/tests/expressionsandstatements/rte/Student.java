234d6324ffbd27697c2250ab988227f832dceb61
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2026-06-27 13:33:05 +0200

    Class2MC Person/Student – add equals

--- monticore-grammar/src/test/java/de/monticore/tests/expressionsandstatements/rte/Student.java
+++ monticore-grammar/src/test/java/de/monticore/tests/expressionsandstatements/rte/Student.java
@@ -1,6 +1,8 @@
 // (c) https://github.com/MontiCore/monticore
 package de.monticore.tests.expressionsandstatements.rte;
 
+import java.util.Objects;
+
 /**
  * simple class to test inheritance behavior
  */
@@ -15,5 +17,25 @@ public class Student extends Person {
 
   public int getStudentID() {
     return studentID;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    if (!super.equals(o)) {
+      return false;
+    }
+    Student student = (Student) o;
+    return studentID == student.studentID;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), studentID);
   }
 }
