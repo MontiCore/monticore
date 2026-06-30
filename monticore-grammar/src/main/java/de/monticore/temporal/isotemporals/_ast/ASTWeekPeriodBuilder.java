778035c86a18951535669b9b1c48d05c44d29c1f
Marc Schmidt <schmidt@se-rwth.de>
2026-06-30 11:14:00 +0200

    Minor

--- monticore-grammar/src/main/java/de/monticore/temporal/isotemporals/_ast/ASTWeekPeriodBuilder.java
+++ monticore-grammar/src/main/java/de/monticore/temporal/isotemporals/_ast/ASTWeekPeriodBuilder.java
@@ -1,9 +1,7 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.temporal.isotemporals._ast;
 
-import de.monticore.temporal.isotemporals.ISOTemporalsMill;
 import de.monticore.temporal.isotemporals.parsing.ISOTemporals2ndParser;
-import de.monticore.temporal.parsing.isotemporals4parsing.ISOTemporals4ParsingMill;
 
 public class ASTWeekPeriodBuilder extends ASTWeekPeriodBuilderTOP {
   
