b0e739e88580203a79356b8e9dd5e91a4ab9e0d4
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2026-07-03 15:27:02 +0200

    assure that MCValueObject cannot be used for numbers

--- monticore-runtime/src/main/java/de/monticore/values/MCValueObject.java
+++ monticore-runtime/src/main/java/de/monticore/values/MCValueObject.java
@@ -15,6 +15,10 @@ public class MCValueObject implements MCValue {
   protected Object value;
 
   public MCValueObject(Object value) {
+    assert !(value instanceof Number) :
+        "MCValueInt/MCValueDouble should be used";
+    assert !(value instanceof Boolean) :
+        "MCValueBoolean should be used";
     this.value = value;
   }
 
@@ -59,110 +63,6 @@ public class MCValueObject implements MCValue {
   // further type checks and conversions
 
   @Override
-  public boolean isBoolean() {
-    return value instanceof Boolean;
-  }
-
-  @Override
-  public boolean asBoolean() {
-    if (isBoolean()) {
-      return this.<Boolean> unsafeCast();
-    }
-    throw createIllegalConversionException("boolean");
-  }
-
-  @Override
-  public boolean isByte() {
-    return value instanceof Byte;
-  }
-
-  @Override
-  public byte asByte() {
-    if (isByte()) {
-      return this.<Byte> unsafeCast();
-    }
-    throw createIllegalConversionException("byte");
-  }
-
-  @Override
-  public boolean isChar() {
-    return value instanceof Character;
-  }
-
-  @Override
-  public char asChar() {
-    if (isChar()) {
-      return this.<Character> unsafeCast();
-    }
-    throw createIllegalConversionException("char");
-  }
-
-  @Override
-  public boolean isShort() {
-    return value instanceof Short;
-  }
-
-  @Override
-  public short asShort() {
-    if (isShort()) {
-      return this.<Short> unsafeCast();
-    }
-    throw createIllegalConversionException("short");
-  }
-
-  @Override
-  public boolean isInt() {
-    return value instanceof Integer;
-  }
-
-  @Override
-  public int asInt() {
-    if (isInt()) {
-      return this.<Integer> unsafeCast();
-    }
-    throw createIllegalConversionException("int");
-  }
-
-  @Override
-  public boolean isLong() {
-    return value instanceof Long;
-  }
-
-  @Override
-  public long asLong() {
-    if (isLong()) {
-      return this.<Long> unsafeCast();
-    }
-    throw createIllegalConversionException("long");
-  }
-
-  @Override
-  public boolean isFloat() {
-    return value instanceof Float;
-  }
-
-  @Override
-  public float asFloat() {
-    if (isFloat()) {
-      return this.<Float> unsafeCast();
-    }
-    throw createIllegalConversionException("float");
-  }
-
-  @Override
-  public boolean isDouble() {
-    return value instanceof Double;
-  }
-
-  @Override
-  public double asDouble() {
-    if (isDouble()) {
-      return this.<Double> unsafeCast();
-    }
-    throw createIllegalConversionException("double");
-  }
-
-  @Override
   public boolean isString() {
     return value instanceof String;
   }
@@ -183,16 +83,6 @@ public class MCValueObject implements MCValue {
   @Override
   public String printValue() {
     return String.valueOf(value);
-  }
-
-  // helper
-
-  protected IllegalStateException createIllegalConversionException(String type) {
-    return new IllegalStateException("0xF1381 internal error: "
-        + "Tried to convert MIValue to " + type + ", even though it is not;"
-        + " Type: " + printType()
-        + " Value: " + printValue()
-    );
   }
 
 }
