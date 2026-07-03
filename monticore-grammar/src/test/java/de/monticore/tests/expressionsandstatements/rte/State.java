fd0d235c8973e41485f59be5e6af15060467b826
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2026-07-03 14:02:52 +0200

    add enums for testing purposes

---
+++ monticore-grammar/src/test/java/de/monticore/tests/expressionsandstatements/rte/State.java
@@ -0,0 +1,10 @@
+// (c) https://github.com/MontiCore/monticore
+package de.monticore.tests.expressionsandstatements.rte;
+
+/**
+ * simple enum to test behavior
+ */
+public enum State {
+  ON,
+  OFF
+}
