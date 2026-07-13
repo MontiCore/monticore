a5d518ada6eed6020a3c1914ba3420985a19a095
SE-FDr <88034434+SE-FDr@users.noreply.github.com>
2026-07-13 14:43:33 +0200

    Add labels to MCSignalBreak

--- monticore-grammar/src/main/java/de/monticore/interpreter/signals/MCSignalBreak.java
+++ monticore-grammar/src/main/java/de/monticore/interpreter/signals/MCSignalBreak.java
@@ -1,17 +1,38 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.interpreter.signals;
 
+import java.util.Optional;
+
 /**
  * Represents that a {@code break} statement has been executed.
  */
 public class MCSignalBreak
     extends MCSignalFlowControl {
 
+  // Null-able for efficiency
+  protected final String label;
+
+  public MCSignalBreak() {
+    this.label = null;
+  }
+
+  public MCSignalBreak(String label) {
+    this.label = label;
+  }
+
   /**
    * signals the current thread.
    */
   public static void signal() {
     throw new MCSignalBreak();
+  }
+
+  public static void signal(String label) {
+    throw new MCSignalBreak(label);
+  }
+
+  public Optional<String> getLabel() {
+    return Optional.ofNullable(label);
   }
 
 }
