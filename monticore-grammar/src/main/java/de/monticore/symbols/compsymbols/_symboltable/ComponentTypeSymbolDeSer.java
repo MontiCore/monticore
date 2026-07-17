31f8069b2de3d65aad3459bba9c9712be5175659
Christoph Lütticke <christoph.luetticke@rwth-aachen.de>
2026-07-17 14:48:29 +0200

    refactor

--- monticore-grammar/src/main/java/de/monticore/symbols/compsymbols/_symboltable/ComponentTypeSymbolDeSer.java
+++ monticore-grammar/src/main/java/de/monticore/symbols/compsymbols/_symboltable/ComponentTypeSymbolDeSer.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
 import de.monticore.symbols.basicsymbols._symboltable.VariableSymbol;
 import de.monticore.symbols.compsymbols.CompSymbolsMill;
+import de.monticore.symboltable.modifiers.AccessModifier;
 import de.monticore.symboltable.serialization.ISymbolDeSer;
 import de.monticore.symboltable.serialization.JsonDeSers;
 import de.monticore.symboltable.serialization.JsonPrinter;
@@ -160,7 +161,7 @@ public class ComponentTypeSymbolDeSer extends ComponentTypeSymbolDeSerTOP {
     return ArrayListMultimap.create();
   }
 
-  private void fillEffectChain(ComponentTypeSymbol symbol, JsonObject symbolJson) {
+  protected void fillEffectChain(ComponentTypeSymbol symbol, JsonObject symbolJson) {
     if (symbolJson == null) {
       return;
     }
@@ -170,7 +171,7 @@ public class ComponentTypeSymbolDeSer extends ComponentTypeSymbolDeSerTOP {
     JsonObject chain = symbolJson.getObjectMember(EFFECT_CHAIN);
     Multimap<PortSymbol, PortSymbol> effectMap = symbol.getEffectChains();
     for (Entry<String, JsonElement> entry : chain.getMembers().entrySet()) {
-      List<PortSymbol> inPorts = symbol.getSpannedScope().resolvePortMany(entry.getKey());
+      List<PortSymbol> inPorts = symbol.getSpannedScope().resolvePortLocallyMany(true, entry.getKey(), AccessModifier.ALL_INCLUSION, (PortSymbol p) -> true);
       List<PortSymbol> outPorts = entry.getValue().getAsJsonArray().getValues().stream()
               .map(outPortName -> symbol.getSpannedScope().resolvePortMany(outPortName.toString()))
               .flatMap(Collection::stream).toList();
