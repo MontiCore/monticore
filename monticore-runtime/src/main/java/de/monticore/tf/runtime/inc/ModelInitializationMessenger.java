66d5dfdf45dc0c615f042e504824424245790182
Janik Rapp <rapp@se-rwth.de>
2026-07-23 12:54:47 +0200

    reverse ModelInitializationMessenger notification order to restore previous behavior

--- monticore-runtime/src/main/java/de/monticore/tf/runtime/inc/ModelInitializationMessenger.java
+++ monticore-runtime/src/main/java/de/monticore/tf/runtime/inc/ModelInitializationMessenger.java
@@ -48,15 +48,15 @@ public class ModelInitializationMessenger<E extends ITraverser> {
       
       @Override
       public void visit(ASTNode node) {
+        ASTNode parent = parentStack.isEmpty() ? null : parentStack.peek();
+        accessor.notifyNodeAttach(node, parent);
+        
         parentStack.push(node);
       }
       
       @Override
       public void endVisit(ASTNode node) {
         parentStack.pop();
-        
-        ASTNode parent = parentStack.isEmpty() ? null : parentStack.peek();
-        accessor.notifyNodeAttach(node, parent);
       }
     });
   }
