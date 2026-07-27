16323d6ac0e885f77330b8a3618ce79cc8b03bf8
Hendrik7889 <44064629+Hendrik7889@users.noreply.github.com>
2026-07-27 12:44:37 +0200

    adapt tests

--- monticore-generator/src/test/java/de/monticore/codegen/cd2java/mill/CDMillDecoratorTest.java
+++ monticore-generator/src/test/java/de/monticore/codegen/cd2java/mill/CDMillDecoratorTest.java
@@ -181,7 +181,10 @@ public class CDMillDecoratorTest extends DecoratorTestCase {
     Optional<ASTCDPackage> p = decoratedCompilationUnit.getCDDefinition().getCDPackagesList().stream()
             .filter(pp -> "de.monticore.codegen.symboltable.automaton".equals(pp.getName())).findAny();
     assertTrue (p.isPresent());
-    assertEquals(1, p.get().getCDElementList().size());
+    //we expect two classes in the mill package: StatechartMill and StatechartMillState
+    assertEquals(2, p.get().getCDElementList().size());
+    assertEquals("AutomatonMill", ((ASTCDClass) p.get().getCDElementList().get(0)).getName());
+    assertEquals("AutomatonMillState", ((ASTCDClass) p.get().getCDElementList().get(1)).getName());
   
     assertTrue(Log.getFindings().isEmpty());
   }
