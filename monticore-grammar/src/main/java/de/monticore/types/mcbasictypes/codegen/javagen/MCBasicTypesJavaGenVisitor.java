a81217f9b1fc3f5f5ac1a7e069b4b76fec2d423a
Tom Bursch <bursch@se-rwth.de>
2026-07-30 10:19:31 +0200

    Fix

--- monticore-grammar/src/main/java/de/monticore/types/mcbasictypes/codegen/javagen/MCBasicTypesJavaGenVisitor.java
+++ monticore-grammar/src/main/java/de/monticore/types/mcbasictypes/codegen/javagen/MCBasicTypesJavaGenVisitor.java
@@ -46,7 +46,7 @@ public class MCBasicTypesJavaGenVisitor extends MCBasicTypesInheritanceHandler i
           SymTypeExpression2JavaConverter.getJavaTypePrint(
               SymTypeRelations.normalize(TypeCheck3.symTypeFromAST(node)))
       );
-    rootMCBasicType = null;
+      rootMCBasicType = null;
     }
   }
 
